5e8e40a3385a1c78306566337581e611b14b8b84
haerong22 <haerong22@naver.com>
2020-11-22 15:17:03 +0900

    init

---
+++ src/main/java/com/example/eatgo/EatgoApplication.java
@@ -0,0 +1,13 @@
+package com.example.eatgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatgoApplication.class, args);
+    }
+
+}
