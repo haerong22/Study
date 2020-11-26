2df8b99bfb68ed7de12b9e214e3dbece35e26f09
haerong22 <haerong22@naver.com>
2020-11-26 18:19:53 +0900

    사용자 추가 api

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,8 +1,6 @@
 package com.example.restfulwebservice.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,10 @@ public class UserController {
     public User retrieveUser(@PathVariable int id) {
         return userDaoService.findOne(id);
 
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        User savedUser = userDaoService.save(user);
     }
 }
