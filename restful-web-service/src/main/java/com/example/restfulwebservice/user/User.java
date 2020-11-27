86887dcc32cc1617e81056190ec7866ede970464
haerong22 <haerong22@naver.com>
2020-11-27 16:21:09 +0900

    유효성 검사

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
@@ -11,6 +13,10 @@ import java.util.Date;
 @NoArgsConstructor
 public class User {
     private Integer id;
+
+    @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
     private String name;
+
+    @Past
     private Date joinDate;
 }
