247ab25b6c27a1aee4f7f60a9deba3ff391cf6a2
haerong22 <haerong22@naver.com>
2020-11-27 17:55:27 +0900

    json filter

---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/AdminController.java
@@ -0,0 +1,57 @@
+package com.example.restfulwebservice.user;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.context.MessageSource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.Locale;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+    private final UserDaoService userDaoService;
+
+    public AdminController(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public MappingJacksonValue retrieveAllUsers() {
+        List<User> users = userDaoService.findAll();
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "ssn");
+
+        FilterProvider filters =  new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping  = new MappingJacksonValue(users);
+        mapping.setFilters(filters);
+        return mapping;
+    }
+
+    @GetMapping("/users/{id}")
+    public MappingJacksonValue retrieveUser(@PathVariable int id) {
+        User user = userDaoService.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "ssn");
+
+        FilterProvider filters =  new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping  = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+        return mapping;
+    }
+}
