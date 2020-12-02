4b2d0ebca2c923fc6493bc59233caa35050fe39f
kim <haerong22@naver.com>
2020-12-03 03:58:23 +0900

    exports module study

---
+++ nodejs/main.js
@@ -0,0 +1,7 @@
+let m = require('./math-tools');
+
+console.log(m.PI);
+console.log(m.add(1,2));
+console.log(m.subtract(1,2));
+console.log(m.multiply(1,2));
+console.log(m.divide(1,2));
---
+++ nodejs/math-tools.js
@@ -0,0 +1,9 @@
+let calculator = {
+  PI : 3.14,
+  add : (a, b) => a + b,
+  subtract : (a, b) => a - b,
+  multiply : (a, b) => a * b,
+  divide : (a, b) => a / b
+}
+
+module.exports = calculator;
