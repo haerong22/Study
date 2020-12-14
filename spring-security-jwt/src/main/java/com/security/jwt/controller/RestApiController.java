4a59fe9c1be5c21b4b1e00f812c5244b0a8ee79b
haerong22 <haerong22@naver.com>
2020-12-14 21:03:04 +0900

    filter

--- spring-security-jwt/src/main/java/com/security/jwt/controller/RestApiController.java
+++ spring-security-jwt/src/main/java/com/security/jwt/controller/RestApiController.java
@@ -1,13 +1,19 @@
 package com.security.jwt.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestApiController {
 
-    @GetMapping("home")
+    @GetMapping("/home")
     public String home() {
         return "<h1>home</h1>";
+    }
+
+    @PostMapping("/token")
+    public String token() {
+        return "<h1>token</h1>";
     }
 }
--- spring-security-jwt/src/main/java/com/security/jwt/filter/MyFilter1.java
+++ spring-security-jwt/src/main/java/com/security/jwt/filter/MyFilter1.java
@@ -1,7 +1,10 @@
 package com.security.jwt.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter1 implements Filter {
 
@@ -9,7 +12,23 @@ public class MyFilter1 implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
 
-        System.out.println("필터 1");
-        filterChain.doFilter(servletRequest, servletResponse);
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+        // 토큰이 넘어오면
+        if(req.getMethod().equals("POST")) {
+            System.out.println("POST 요청됨");
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println(headerAuth);
+
+            if (headerAuth != null && headerAuth.equals("cos")) {
+                filterChain.doFilter(req, res);
+            } else {
+                PrintWriter out = res.getWriter();
+                out.println("인증 안됨");
+            }
+        } else {
+            filterChain.doFilter(req, res);
+        }
     }
 }
