8e76cbaf226307a93d630a0b3a6cdcbfbb36e224
haerong22 <haerong22@naver.com>
2020-12-20 20:25:55 +0900

    key

--- react_webgame/main/NumberBaseball.jsx
+++ react_webgame/main/NumberBaseball.jsx
@@ -35,13 +35,23 @@ class NumberBaseball extends Component {
                 </form>
                 <div>시도: {this.state.tries.length}</div>
                 <ul>
-                    {['사과', '바나나', '포도', '귤', '수박'].map((v) => {
+                    {[
+                        { fruit: '사과', description: '빨개'}, 
+                        { fruit: '바나나', description: '길어'},
+                        { fruit: '포도', description: '맛있다'},
+                        { fruit: '귤', description: '시다'},
+                        { fruit: '수박', description: '달다'},    
+                    ].map((v) => {
                         return (
-                            <li>{v}</li>
+                            <li key={v.fruit}><b>{v.fruit}</b> - {v.description}</li>
                         )
-                    })
+                    })}
 
-                    }
+                    {/* <li><b>사과</b> - 빨개</li>
+                    <li><b>바나나</b> - 길어</li>
+                    <li><b>포도</b> - 맛있다</li>
+                    <li><b>귤</b> - 시다</li>
+                    <li><b>수박</b> - 달다</li> */}
                 </ul>
             </>
         )
