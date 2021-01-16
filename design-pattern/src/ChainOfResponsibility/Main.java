1c023fb3585b7c754cda430e1403ac592f23f5e8
kim <haerong22@naver.com>
2021-01-16 18:33:56 +0900

    Chain of Responsibility Pattern

---
+++ design-pattern/src/ChainOfResponsibility/Main.java
@@ -0,0 +1,15 @@
+package ChainOfResponsibility;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator plus = new PlusCalculator();
+        Calculator sub = new SubCalculator();
+
+        plus.setNextCalculator(sub);
+        Request request1 = new Request(1, 2, "+");
+        Request request2 = new Request(10, 2, "-");
+
+        plus.process(request1);
+        plus.process(request2);
+    }
+}
