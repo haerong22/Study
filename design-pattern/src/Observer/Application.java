3b7ff5babe1a1c37c872cf41efd052889ab0cc13
kim <haerong22@naver.com>
2021-01-17 22:52:35 +0900

    Observer Pattern

---
+++ design-pattern/src/Observer/Application.java
@@ -0,0 +1,17 @@
+package Observer;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Button button = new Button();
+
+        button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(Button button) {
+                System.out.println(button + "is Clicked");
+            }
+        });
+        button.onClick();
+    }
+}
+
