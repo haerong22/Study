17eb1989f997a66de90fcec5607bfdc01e6b35d3
kim <haerong22@naver.com>
2021-01-24 02:44:44 +0900

    jpashop : entity (embedded type)

--- jpashop/src/main/java/com/example/jpashop/domain/Address.java
+++ jpashop/src/main/java/com/example/jpashop/domain/Address.java
@@ -11,4 +11,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
