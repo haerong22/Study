ce39455fa98609733e089fc457115d9dce1943dd
kim <haerong22@naver.com>
2021-01-24 18:55:10 +0900

    jpashop : item entity

--- jpashop/src/main/java/com/example/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/com/example/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.example.jpashop.domain.item;
 
 import com.example.jpashop.domain.Category;
+import com.example.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비지니스 로직
+
+    /**
+     *  stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     *  stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock0");
+        }
+        this.stockQuantity = restStock;
+    }
 }
