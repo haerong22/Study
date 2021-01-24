ba9af4d9386f9c36041e7c4872ec77404c0a2a33
kim <haerong22@naver.com>
2021-01-24 19:17:13 +0900

    jpashop : item service

---
+++ jpashop/src/main/java/com/example/jpashop/service/ItemService.java
@@ -0,0 +1,30 @@
+package com.example.jpashop.service;
+
+import com.example.jpashop.domain.item.Item;
+import com.example.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItem() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
