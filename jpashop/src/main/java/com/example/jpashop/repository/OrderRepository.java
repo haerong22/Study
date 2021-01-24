36586bb8950ec2a2e4b165a5d6cf5559c209403f
kim <haerong22@naver.com>
2021-01-24 20:40:45 +0900

    jpashop : order repository

---
+++ jpashop/src/main/java/com/example/jpashop/repository/OrderRepository.java
@@ -0,0 +1,25 @@
+package com.example.jpashop.repository;
+
+import com.example.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch) {}
+}
