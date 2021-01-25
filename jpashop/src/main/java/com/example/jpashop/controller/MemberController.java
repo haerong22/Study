49f797b428023c66e393b57cf66a42074b099ff6
kim <haerong22@naver.com>
2021-01-25 17:46:33 +0900

    jpashop : list view, controller

--- jpashop/src/main/java/com/example/jpashop/controller/MemberController.java
+++ jpashop/src/main/java/com/example/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -40,5 +41,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
