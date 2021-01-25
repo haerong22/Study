ef10e9fdc5ecd0cb34cc8f32ca76a89dc1184205
kim <haerong22@naver.com>
2021-01-25 21:19:38 +0900

    jpashop : orderForm view, controller

---
+++ jpashop/src/main/java/com/example/jpashop/controller/OrderController.java
@@ -0,0 +1,41 @@
+package com.example.jpashop.controller;
+
+import com.example.jpashop.domain.Member;
+import com.example.jpashop.domain.item.Item;
+import com.example.jpashop.service.ItemService;
+import com.example.jpashop.service.MemberService;
+import com.example.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItem();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(Long memberId, Long itemId, int count){
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
