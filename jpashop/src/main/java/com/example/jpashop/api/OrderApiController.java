484b99f670840919214412a35142bcc7a83b8c06
kim <haerong22@naver.com>
2021-01-27 16:32:25 +0900

    jpashop : 컬렉션 조회 v1 (entity 직접응답)

---
+++ jpashop/src/main/java/com/example/jpashop/api/OrderApiController.java
@@ -0,0 +1,30 @@
+package com.example.jpashop.api;
+
+import com.example.jpashop.domain.Order;
+import com.example.jpashop.domain.OrderItem;
+import com.example.jpashop.repository.OrderRepository;
+import com.example.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+}
