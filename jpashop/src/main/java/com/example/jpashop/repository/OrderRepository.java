4b63ef0094bae702c0a7b64941ad8317ac5e1720
kim <haerong22@naver.com>
2021-01-28 17:12:42 +0900

    jpashop : querydsl

--- jpashop/src/main/java/com/example/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/com/example/jpashop/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.example.jpashop.repository;
 
-import com.example.jpashop.domain.Member;
+import com.example.jpashop.domain.*;
 import com.example.jpashop.domain.Order;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -86,14 +88,31 @@ public class OrderRepository {
     }
 
     public List<Order> findAll(OrderSearch orderSearch) {
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
 
-        return em.createQuery("select o from Order o join o.member m" +
-                " where o.status = :status" +
-                " and m.name like :name", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("name", orderSearch.getMemberName())
-                .setMaxResults(1000) // 최대 1000건건
-               .getResultList();
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if (!StringUtils.hasText(memberName)) {
+            return null;
+        }
+        return QMember.member.name.like(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return QOrder.order.status.eq(statusCond);
     }
 
     public List<Order> findAllWithMemberDelivery(int offset, int limit) {
--- jpashop/src/main/java/com/example/jpashop/service/OrderService.java
+++ jpashop/src/main/java/com/example/jpashop/service/OrderService.java
@@ -60,6 +60,6 @@ public class OrderService {
 
     // 검색
     public List<Order> findOrders(OrderSearch orderSearch) {
-        return orderRepository.findAllByString(orderSearch);
+        return orderRepository.findAll(orderSearch);
     }
 }
