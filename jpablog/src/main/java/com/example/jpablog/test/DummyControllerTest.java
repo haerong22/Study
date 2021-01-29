51ff7f902cd7154c4d7a629f7beedc27ea68a5a3
kim <haerong22@naver.com>
2021-01-29 18:23:30 +0900

    jpablog : read test

--- jpablog/src/main/java/com/example/jpablog/test/DummyControllerTest.java
+++ jpablog/src/main/java/com/example/jpablog/test/DummyControllerTest.java
@@ -4,9 +4,9 @@ import com.example.jpablog.model.RoleType;
 import com.example.jpablog.model.User;
 import com.example.jpablog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.function.Supplier;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,10 +14,16 @@ public class DummyControllerTest {
 
     private final UserRepository userRepository;
 
+    @GetMapping("/dummy/user/{id}")
+    public User detail(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("유저 없음. id : " + id));
+    }
+
     @PostMapping("/dummy/join")
     public String join(@RequestBody User user) {
         user.setRole(RoleType.USER);
         userRepository.save(user);
         return "회원가입 완료";
     }
+
 }
