7d7494ffcfa8eeaa7b1cd939b7d7e28644c305d8
kim <haerong22@naver.com>
2021-01-29 18:54:14 +0900

    jpablog : paging

--- jpablog/src/main/java/com/example/jpablog/test/DummyControllerTest.java
+++ jpablog/src/main/java/com/example/jpablog/test/DummyControllerTest.java
@@ -4,8 +4,13 @@ import com.example.jpablog.model.RoleType;
 import com.example.jpablog.model.User;
 import com.example.jpablog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 @RestController
@@ -13,6 +18,20 @@ import java.util.function.Supplier;
 public class DummyControllerTest {
 
     private final UserRepository userRepository;
+
+    @GetMapping("/dummy/user")
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/dummy/user/search")
+    public List<User> pageList(
+            @PageableDefault(size = 2,sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        System.out.println(pageable);
+        Page<User> pagingUsers = userRepository.findAll(pageable);
+        List<User> users = pagingUsers.getContent();
+        return users;
+    }
 
     @GetMapping("/dummy/user/{id}")
     public User detail(@PathVariable Long id) {
