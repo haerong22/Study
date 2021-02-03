32d061a3eb862ab7e74ae373c6c3398e2e3ddbcc
kim <haerong22@naver.com>
2021-02-03 15:46:01 +0900

    react : useState

--- react-springboot/my-app/src/App.js
+++ react-springboot/my-app/src/App.js
@@ -1,51 +1,32 @@
 import { useState } from 'react';
 import './App.css';
-import Sub from './Sub';
-
-let a = 10;
-const b = 20;
 
 function App() {
-  const mystyle = {
-    color: 'red',
-  };
-
-  let list = [1, 2, 3];
-
-  const users = [
-    { id: 1, name: 'kim', phone: '1234' },
-    { id: 2, name: 'lee', phone: '1234' },
-    { id: 3, name: 'choi', phone: '1234' },
+  let sample = [
+    { id: 1, name: '홍길동' },
+    { id: 2, name: '임꺽정' },
+    { id: 3, name: '장보고' },
+    { id: 4, name: '세종대왕' },
   ];
-  const update = {
-    id: 2,
-    name: 'park',
-  };
-  const newUser = users.map((u) =>
-    u.id === update.id ? { ...u, ...update } : u,
-  );
-  console.log(newUser);
+  const [users, setUsers] = useState(sample);
+  const [num, setNum] = useState(5);
 
-  const [number, setNumber] = useState(1);
-  const add = () => {
-    setNumber(number + 1);
-    console.log('add', number);
+  const download = () => {
+    setUsers([...sample, { id: num, name: '조자룡' }]);
+    setNum(num + 1);
   };
 
+  console.log('rendering');
   return (
     <>
-      <div style={mystyle}>안녕{a === 10 ? '10입니다.' : '10이 아닙니다'}</div>
-      <h1 className="box-style">제목{b === 20 && '20입니다.'}</h1>
       <div>
-        {list.map((n) => (
-          <h1 key={n}>n</h1>
+        {users.map((u) => (
+          <h1>
+            {u.id}, {u.name}
+          </h1>
         ))}
       </div>
-      <div>
-        <h1>숫자 : {number} </h1>
-        <button onClick={add}>더하기</button>
-      </div>
-      <Sub />
+      <button onClick={download}>다운로드</button>
     </>
   );
 }
