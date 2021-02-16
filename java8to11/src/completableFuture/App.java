a05558a2f53726de494c866c76bf6bf8be3b42f2
haerong22 <haerong22@naver.com>
2021-02-16 17:24:33 +0900

    java8 : completableFuture(Callable, Future)

--- java8to11/src/completableFuture/App.java
+++ java8to11/src/completableFuture/App.java
@@ -1,9 +1,8 @@
 package completableFuture;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class App {
 
@@ -11,16 +10,75 @@ public class App {
         return () -> System.out.println(message + ": " + Thread.currentThread().getName());
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
         /**
-         * Executors 
+         * Callable, Future
          */
 
-        // newSingleThreadScheduledExecutor
-        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-        executorService.schedule(getRunnable("Hello"), 3, TimeUnit.SECONDS); // 3초 후 실행
-        executorService.scheduleAtFixedRate(getRunnable("World"), 1, 2, TimeUnit.SECONDS); // 1초 후 2초마다 실행
+        // invokeAll, invokeAny
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        Callable<String> hello = () -> {
+            Thread.sleep(2000L);
+            return "Hello";
+        };
+
+        Callable<String> world = () -> {
+            Thread.sleep(3000L);
+            return "world";
+        };
+
+        Callable<String> java = () -> {
+            Thread.sleep(1000L);
+            return "java";
+        };
+
+        // invokeAny
+        String future = executorService.invokeAny(Arrays.asList(hello, world, java)); // 가장 먼저 도착한 값
+        System.out.println(future);
+
+        // invokeAll
+        List<Future<String>> futures = executorService.invokeAll(Arrays.asList(hello, world, java)); // 다 끝날때 까지 대기
+        for (Future<String> f : futures) {
+            System.out.println(f.get());
+        }
+
+        executorService.shutdown();
+
+
+//      // Callable - get, cancel
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//
+//        Callable<String> hello = () -> {
+//            Thread.sleep(2000L);
+//            return "Hello";
+//        };
+//
+//        Future<String> helloFuture = executorService.submit(hello);
+//
+//        System.out.println(helloFuture.isDone());
+//        System.out.println("Start!");
+//
+////        System.out.println(helloFuture.get()); // 결과 까지 대기 ( blocking call )
+//        helloFuture.cancel(false); // true : 현재 진행중인 작업을 interrupt 후 종료,
+//                                                     // false : 대기 후 종료( 대기 했다 하더라도 get 은 불가능,
+//                                                     // cancel 이 되면 isDone 은 무조건 true 가 된다.
+//        System.out.println(helloFuture.isDone());
+//        System.out.println("End!");
+//        executorService.shutdown();
+
+
+
+        /**
+         * Executors
+         */
+
+//        // newSingleThreadScheduledExecutor
+//        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+//        executorService.schedule(getRunnable("Hello"), 3, TimeUnit.SECONDS); // 3초 후 실행
+//        executorService.scheduleAtFixedRate(getRunnable("World"), 1, 2, TimeUnit.SECONDS); // 1초 후 2초마다 실행
 
 
 //        // newFixedThreadPool
