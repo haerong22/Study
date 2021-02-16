89c93843699e233b3afe00cd1430f65aa176732b
haerong22 <haerong22@naver.com>
2021-02-16 16:20:16 +0900

    java8 : completableFuture(executors)

--- java8to11/src/completableFuture/App.java
+++ java8to11/src/completableFuture/App.java
@@ -1,26 +1,68 @@
 package completableFuture;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class App {
+
+    private static Runnable getRunnable(String message) {
+        return () -> System.out.println(message + ": " + Thread.currentThread().getName());
+    }
+
     public static void main(String[] args) throws InterruptedException {
 
         /**
-         * Thread 사용
+         * Executors 
          */
 
-        // join
-        Thread thread = new Thread(() -> {
-            System.out.println("Thread: " + Thread.currentThread().getName());
-            try {
-                Thread.sleep(3000L);
-            } catch (InterruptedException e) {
-                throw new IllegalStateException(e);
-            }
-        });
-        thread.start();
+        // newSingleThreadScheduledExecutor
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.schedule(getRunnable("Hello"), 3, TimeUnit.SECONDS); // 3초 후 실행
+        executorService.scheduleAtFixedRate(getRunnable("World"), 1, 2, TimeUnit.SECONDS); // 1초 후 2초마다 실행
 
-        System.out.println("Hello: " + Thread.currentThread().getName());
-        thread.join();
-        System.out.println(thread + " is finished");
+
+//        // newFixedThreadPool
+//        ExecutorService executorService = Executors.newFixedThreadPool(5);
+//        executorService.submit(getRunnable("Hello"));
+//        executorService.submit(getRunnable("World"));
+//        executorService.submit(getRunnable("The"));
+//        executorService.submit(getRunnable("Java"));
+//        executorService.submit(getRunnable("Thread"));
+//
+//        executorService.shutdown();
+
+
+//        // newSingleThreadExecutor
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        executorService.submit(() -> {
+//            System.out.println("Thread: " + Thread.currentThread().getName());
+//        });
+//
+//        executorService.shutdown(); // shutdown() : graceful,  shutdownNow() : 즉시 종료
+
+
+
+
+        /**
+         * Thread
+         */
+
+//        // join
+//        Thread thread = new Thread(() -> {
+//            System.out.println("Thread: " + Thread.currentThread().getName());
+//            try {
+//                Thread.sleep(3000L);
+//            } catch (InterruptedException e) {
+//                throw new IllegalStateException(e);
+//            }
+//        });
+//        thread.start();
+//
+//        System.out.println("Hello: " + Thread.currentThread().getName());
+//        thread.join();
+//        System.out.println(thread + " is finished");
 
 
 //        // interrupt
@@ -40,6 +82,7 @@ public class App {
 //        Thread.sleep(3000L);
 //        thread.interrupt();
 
+
 //        // sleep
 //        Thread thread = new Thread(() -> {
 //            try {
@@ -52,12 +95,14 @@ public class App {
 //        thread.start();
 //        System.out.println("Hello: " + Thread.currentThread().getName());
 
+
 //        // 람다 사용
 //        Thread thread = new Thread(() -> {
 //            System.out.println("Thread: " + Thread.currentThread().getName());
 //        });
 //        thread.start();
 //        System.out.println("Hello: " + Thread.currentThread().getName());
+
 
 //        // 익명 클래스 사용
 //        Thread thread = new Thread(new Runnable() {
@@ -68,6 +113,7 @@ public class App {
 //        });
 //        thread.start();
 //        System.out.println("Hello: " + Thread.currentThread().getName());
+
 
 //        // Thread 상속해서 사용
 //        MyThread myThread = new MyThread();
