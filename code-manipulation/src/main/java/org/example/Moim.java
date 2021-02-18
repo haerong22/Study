6533f5fd475cdf7951c62b0b574c64c867f4e626
haerong22 <haerong22@naver.com>
2021-02-18 19:07:45 +0900

    code manipulation : 코드 커버리지 측정하기(jacoco)

---
+++ code-manipulation/src/main/java/org/example/Moim.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Moim {
+
+    int maxNumberOfAttendees;
+
+    int numberOfEnrollment;
+
+    public boolean isEnrollmentFull() {
+        if (maxNumberOfAttendees == 0) {
+            return false;
+        }
+
+        if (numberOfEnrollment < maxNumberOfAttendees) {
+            return false;
+        }
+
+        return true;
+    }
+}
