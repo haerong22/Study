35b0b745af55168e2efac644a9e52145acc31072
haerong22 <haerong22@naver.com>
2021-02-25 23:44:16 +0900

    application test : junit5 - junit4 migration

---
+++ application-test/app-test/src/test/java/com/example/apptest/StudyJUnit4Test.java
@@ -0,0 +1,17 @@
+package com.example.apptest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudyJUnit4Test {
+
+    @Before
+    public void before() {
+        System.out.println("before");
+    }
+
+    @Test
+    public void junit4_test() {
+        System.out.println("JUnit4_test");
+    }
+}
