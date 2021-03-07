b0f96c966b7d92b756ba55e7e2da9f37d24fcb60
haerong22 <haerong22@naver.com>
2021-03-07 19:08:17 +0900

    react web game : 로또추첨기 - 클래스 컴포넌트

--- react_webgame/main/Lotto.jsx
+++ react_webgame/main/Lotto.jsx
@@ -27,7 +27,7 @@ class Lotto extends Component {
 
   timeouts = [];
 
-  componentDidMount() {
+  runTimeouts = () => {
     const { winNumbers } = this.state;
     for (let i = 0; i < this.state.winNumbers.length - 1; i++) {
       this.timeouts[i] = setTimeout(() => {
@@ -44,6 +44,10 @@ class Lotto extends Component {
         redo: true,
       });
     }, 7000);
+  };
+
+  componentDidMount() {
+    this.runTimeouts();
   }
 
   componentWillUnmount() {
@@ -51,6 +55,22 @@ class Lotto extends Component {
       clearTimeout(v);
     });
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.winBalls.length === 0) {
+      this.runTimeouts();
+    }
+  }
+
+  onClickRedo = () => {
+    this.setState({
+      winNumbers: getWinNumbers(),
+      winBalls: [],
+      bonus: null,
+      redo: false,
+    });
+    this.timeouts = [];
+  };
 
   render() {
     const { winBalls, bonus, redo } = this.state;
