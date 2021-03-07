765691e6fa53bffeaf6ce81d1757597cffeab5f9
haerong22 <haerong22@naver.com>
2021-03-07 16:00:47 +0900

    java oop : class, object, instance

---
+++ JavaOOP/src/object/BookDTO.java
@@ -0,0 +1,18 @@
+package object;
+
+public class BookDTO {
+    public String title;
+    public int price;
+    public String company;
+    public int page;
+
+    public BookDTO() {
+    }
+
+    public BookDTO(String title, int price, String company, int page) {
+        this.title = title;
+        this.price = price;
+        this.company = company;
+        this.page = page;
+    }
+}
