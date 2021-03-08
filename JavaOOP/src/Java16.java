e09fcbeccd6577afebc9935744de4c9bb6c17811
haerong22 <haerong22@naver.com>
2021-03-08 17:05:50 +0900

    java oop : 여러 API 사용

--- JavaOOP/src/Java16.java
+++ JavaOOP/src/Java16.java
@@ -1,2 +1,12 @@
+import object.OverLoad;
+
 public class Java16 {
+    public static void main(String[] args) {
+
+        OverLoad ov = new OverLoad();
+
+        ov.hap(20, 50); // hap_int_int(20, 50)
+        ov.hap(23.4f, 56); // hap_float_int(23.4f, 56)
+        ov.hap(56.7f, 78.9f); // hap_float_float(56.7f, 78.9f)
+    }
 }
