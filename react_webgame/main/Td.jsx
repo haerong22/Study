8b836a642dde76a7c6520b98e64e7b95998768f2
haerong22 <haerong22@naver.com>
2021-03-11 20:05:24 +0900

    react web game : 틱택토 구현

--- react_webgame/main/Td.jsx
+++ react_webgame/main/Td.jsx
@@ -1,12 +1,14 @@
 import React, { useCallback } from "react";
-import { CLICK_CELL, CHANGE_TURN } from "./TicTacToe";
+import { CLICK_CELL } from "./TicTacToe";
 
 const Td = ({ rowIndex, cellIndex, dispatch, cellData }) => {
   const onClickTd = useCallback(() => {
     console.log(rowIndex, cellIndex);
+    if(cellData) {
+      return;
+    }
     dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex });
-    dispatch({ type: CHANGE_TURN });
-  });
+  }, [cellData]);
 
   return <td onClick={onClickTd}>{cellData}</td>;
 };
