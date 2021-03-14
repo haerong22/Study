045f2ab5fc4108b7fb9ec267d593eed55c6de46d
haerong22 <haerong22@naver.com>
2021-03-14 17:58:24 +0900

    java oop : java api

--- JavaOOP/src/Java31.java
+++ JavaOOP/src/Java31.java
@@ -1,3 +1,13 @@
 public class Java31 {
+    public static void main(String[] args) {
+        String str = "apple"; // java.lang.String str = new java.lang.String("apple");
 
+        // String 클래스의 다양한 메소드들
+        System.out.println(str.toUpperCase());
+        System.out.println(str.charAt(3));
+        System.out.println(str.length());
+        System.out.println(str.indexOf("pl"));
+        System.out.println(str.indexOf("px"));
+        System.out.println(str.replaceAll("p", "x"));
+    }
 }
