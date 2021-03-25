fc5b2013e3336654bb1e72b154b7e12af8774bab
haerong22 <haerong22@naver.com>
2021-03-25 22:01:11 +0900

    react web game : react router

--- react_webgame/main/Games.jsx
+++ react_webgame/main/Games.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, HashRouter, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Link, Route } from "react-router-dom";
 import NumberBaseBall from "./games/NumberBaseball";
 import RSP from "./games/RSP";
 import Lotto from "./games/Lotto"
@@ -8,6 +8,11 @@ const Games = () => {
 
   return (
     <BrowserRouter>
+      <div>
+        <Link to="/number-baseball">숫자야구</Link>$nbsp
+        <Link to="/rock-scissors-paper">가위바위보</Link>
+        <Link to="/lotto-generator">로또추첨기</Link>
+      </div>
       <div>
         <Route path="/number-baseball" component={NumberBaseBall} />
         <Route path="/rock-scissors-paper" component={RSP} />
