45e1e78527e8434a1a6364776f83821ab2419f8c
haerong22 <haerong22@naver.com>
2021-03-28 23:39:39 +0900

    spring mvc : servlet 생성

---
+++ spring-mvc/servlet/src/main/java/com/example/servlet/basic/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.example.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("req = " + req);
+        System.out.println("resp = " + resp);
+
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+
+        // response header 정보 추가
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("utf-8");
+
+        // response body
+        resp.getWriter().write("hello " + username);
+    }
+}
