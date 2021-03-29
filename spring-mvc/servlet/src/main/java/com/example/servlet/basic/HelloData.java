a79e5de0a7957e34c4f67fe62034de65def1e566
haerong22 <haerong22@naver.com>
2021-03-29 23:16:28 +0900

    spring mvc : servlet - http message body(json)

---
+++ spring-mvc/servlet/src/main/java/com/example/servlet/basic/HelloData.java
@@ -0,0 +1,11 @@
+package com.example.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+}
