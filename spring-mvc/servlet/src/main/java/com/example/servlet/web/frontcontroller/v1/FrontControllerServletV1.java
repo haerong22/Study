c4f24fe465e275fa94bc2ead5b03b516acb65a4d
haerong22 <haerong22@naver.com>
2021-04-03 00:01:50 +0900

    spring mvc : mvc 프레임워크 만들기 - front controller 도입

---
+++ spring-mvc/servlet/src/main/java/com/example/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -0,0 +1,44 @@
+package com.example.servlet.web.frontcontroller.v1;
+
+import com.example.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
+import com.example.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
+import com.example.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
+public class FrontControllerServletV1 extends HttpServlet {
+
+    private final Map<String, ControllerV1> controllerMap = new HashMap<>();
+
+    // 요청 URI 와 controller 매핑
+    public FrontControllerServletV1() {
+        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+        controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+        controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV1.service");
+
+        String requestURI = request.getRequestURI(); // 요청 URI
+
+        ControllerV1 controller = controllerMap.get(requestURI);
+
+        // 해당 요청의 controller 가 없으면 404 리턴
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        controller.process(request, response);
+    }
+}
