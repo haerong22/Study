72127d8ccc646ca1517ce9802ed36d8c9fc40c40
haerong22 <haerong22@naver.com>
2021-04-03 21:03:51 +0900

    spring mvc : mvc 프레임워크 만들기 - 유연한 컨트롤러

---
+++ spring-mvc/servlet/src/main/java/com/example/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,79 @@
+package com.example.servlet.web.frontcontroller.v5;
+
+import com.example.servlet.web.frontcontroller.ModelView;
+import com.example.servlet.web.frontcontroller.MyView;
+import com.example.servlet.web.frontcontroller.v3.ControllerV3;
+import com.example.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import com.example.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import com.example.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import com.example.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public FrontControllerServletV5() {
+        initHandlerMappingMap();
+        initHandlerAdapters();
+    }
+
+    private void initHandlerMappingMap() {
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+    }
+
+    private void initHandlerAdapters() {
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Object handler = getHandler(request); // handler 가져오기
+
+        if (handler == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        MyHandlerAdapter adapter = getHandlerAdapter(handler); // handler adapter 가져오기
+
+        ModelView mv = adapter.handle(request, response, handler);
+
+        String viewName = mv.getViewName(); // 논리 이름 new-form
+        MyView view = viewResolver(viewName); // 물리 뷰 경로
+
+        view.render(mv.getModel(), request, response);
+    }
+
+    private Object getHandler(HttpServletRequest request) {
+        String requestURI = request.getRequestURI(); // 요청 URI
+        return handlerMappingMap.get(requestURI);
+    }
+
+    private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        for (MyHandlerAdapter adapter : handlerAdapters) {
+            if (adapter.supports(handler)) {
+                return adapter;
+            }
+        }
+        throw new IllegalArgumentException("handler adapter 를 찾을 수 없습니다. handler=" + handler);
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+}
---
+++ spring-mvc/servlet/src/main/java/com/example/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -0,0 +1,15 @@
+package com.example.servlet.web.frontcontroller.v5;
+
+import com.example.servlet.web.frontcontroller.ModelView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+
+    boolean supports(Object handler);
+
+    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
+}
