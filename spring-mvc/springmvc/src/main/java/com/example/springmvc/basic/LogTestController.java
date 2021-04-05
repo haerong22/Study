5ba57148a48786dae310cfeb5c31b2d43a319161
haerong22 <haerong22@naver.com>
2021-04-05 15:59:16 +0900

    sprnig mvc : logging

---
+++ spring-mvc/springmvc/src/main/java/com/example/springmvc/basic/LogTestController.java
@@ -0,0 +1,27 @@
+package com.example.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+//    private final Logger log = LoggerFactory.getLogger(getClass()); // LogTestController.class
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        log.trace("trace log = {}", name);
+        log.debug("debug log = {}", name);
+        log.info("info log = {}", name);
+        log.warn("warn log = {}", name);
+        log.error("error log = {}", name);
+
+        return "ok";
+    }
+}
