3159a95ee7478fe7a37bf05b57e44abbf172addf
haerong22 <haerong22@naver.com>
2021-04-06 20:18:17 +0900

    spring mvc : request mapping

---
+++ spring-mvc/springmvc/src/main/java/com/example/springmvc/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,43 @@
+package com.example.springmvc.basic.requestmapping;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")
+public class MappingClassController {
+    /**
+     * GET /mapping/users
+     */
+    @GetMapping
+    public String users() {
+        return "get users";
+    }
+    /**
+     * POST /mapping/users
+     */
+    @PostMapping
+    public String addUser() {
+        return "post user";
+    }
+    /**
+     * GET /mapping/users/{userId}
+     */
+    @GetMapping("/{userId}")
+    public String findUser(@PathVariable String userId) {
+        return "get userId=" + userId;
+    }
+    /**
+     * PATCH /mapping/users/{userId}
+     */
+    @PatchMapping("/{userId}")
+    public String updateUser(@PathVariable String userId) {
+        return "update userId=" + userId;
+    }
+    /**
+     * DELETE /mapping/users/{userId}
+     */
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable String userId) {
+        return "delete userId=" + userId;
+    }
+}
