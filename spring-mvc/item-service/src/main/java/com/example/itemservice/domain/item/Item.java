e74c29ad20057798514e1e7ffb12cdd266418aba
haerong22 <haerong22@naver.com>
2021-04-09 22:30:25 +0900

    spring mvc : item domain

---
+++ spring-mvc/item-service/src/main/java/com/example/itemservice/domain/item/Item.java
@@ -0,0 +1,22 @@
+package com.example.itemservice.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
