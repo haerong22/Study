5f92e02fc8d9709e1e4ac98f80553c27aefc90cf
haerong22 <haerong22@naver.com>
2021-04-11 00:34:54 +0900

    spring mvc : item detail

--- spring-mvc/item-service/src/main/java/com/example/itemservice/web/basic/BasicItemController.java
+++ spring-mvc/item-service/src/main/java/com/example/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,6 +25,13 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
 
     // 테스트용 데이터
