a70c87361bdd551f526a4870d16f1dd3d86acf51
haerong22 <haerong22@naver.com>
2021-04-10 15:25:36 +0900

    spring mvc : item list

---
+++ spring-mvc/item-service/src/main/java/com/example/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,35 @@
+package com.example.itemservice.web.basic;
+
+import com.example.itemservice.domain.item.Item;
+import com.example.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    // 테스트용 데이터
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
