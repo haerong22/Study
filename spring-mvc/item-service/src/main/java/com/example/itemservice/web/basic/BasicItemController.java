a82b113799ab6509431235004bcc907f1162449c
haerong22 <haerong22@naver.com>
2021-04-11 18:26:43 +0900

    spring mvc : item update

--- spring-mvc/item-service/src/main/java/com/example/itemservice/web/basic/BasicItemController.java
+++ spring-mvc/item-service/src/main/java/com/example/itemservice/web/basic/BasicItemController.java
@@ -72,6 +72,19 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
     // 테스트용 데이터
     @PostConstruct
     public void init() {
