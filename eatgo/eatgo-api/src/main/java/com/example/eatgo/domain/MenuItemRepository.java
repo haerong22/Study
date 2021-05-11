1a73fd98502b53e141e94d6470a14b985d5d88ad
haerong22 <haerong22@naver.com>
2021-05-11 16:12:44 +0900

    delete eatgo

--- eatgo/eatgo-api/src/main/java/com/example/eatgo/domain/MenuItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.eatgo.domain;
-
-import java.util.List;
-
-public interface MenuItemRepository {
-    List<MenuItem> findAllByRestaurantId(Long restaurantId);
-}
--- eatgo/eatgo-api/src/main/java/com/example/eatgo/domain/RestaurantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.eatgo.domain;
-
-import java.util.List;
-
-public interface RestaurantRepository {
-    List<Restaurant> findAll();
-
-    Restaurant findById(Long id);
-}
