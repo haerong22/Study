25523700d62cf860fc8c46c2252de6d017e60b28
haerong22 <haerong22@naver.com>
2021-07-05 20:44:58 +0900

    each

--- thymeleaf/src/main/java/com/example/thymeleaf/basic/BasicController.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/basic/BasicController.java
@@ -90,6 +90,20 @@ public class BasicController {
         return "basic/attribute";
     }
 
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        model.addAttribute("users", list);
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
