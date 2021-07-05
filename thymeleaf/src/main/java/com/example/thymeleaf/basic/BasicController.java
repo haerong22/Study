f5c11356d1dc080b03ca37aba8a9ce3e6779cca8
haerong22 <haerong22@naver.com>
2021-07-05 20:32:30 +0900

    attribute

--- thymeleaf/src/main/java/com/example/thymeleaf/basic/BasicController.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/basic/BasicController.java
@@ -85,6 +85,11 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
