16428071942d1337279d561ed1a530f6e27b6735
haerong22 <haerong22@naver.com>
2021-07-05 20:54:10 +0900

    condition

--- thymeleaf/src/main/java/com/example/thymeleaf/basic/BasicController.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/basic/BasicController.java
@@ -96,6 +96,12 @@ public class BasicController {
         return "basic/each";
     }
 
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
     private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("userA", 10));
