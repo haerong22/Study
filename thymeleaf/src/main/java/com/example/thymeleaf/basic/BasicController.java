b7ef59a6e6adae706cd28d294211d0df5f406518
haerong22 <haerong22@naver.com>
2021-07-05 20:05:40 +0900

    url link & literal

--- thymeleaf/src/main/java/com/example/thymeleaf/basic/BasicController.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/basic/BasicController.java
@@ -64,6 +64,19 @@ public class BasicController {
         return "basic/date";
     }
 
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+
+        return "basic/link";
+    }
+
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
 
     @Component("helloBean")
     static class HelloBean {
