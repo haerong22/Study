15983e8eeb5d6bad99937248af2ceac1135e083a
haerong22 <haerong22@naver.com>
2021-07-12 20:38:10 +0900

    springboot_message : message

--- springboot_message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ springboot_message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,9 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -9,5 +12,16 @@ public class ItemServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
+
+	/**
+	 * 직접 등록
+	 */
+//	@Bean
+//	public MessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasenames("messages", "errors"); // messages.properties, errors.properties
+//		messageSource.setDefaultEncoding("utf-8");
+//		return messageSource;
+//	}
 
 }
