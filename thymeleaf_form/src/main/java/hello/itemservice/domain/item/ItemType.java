ccc730a6d217869b7822cd5fc4c6ce7475db2e81
haerong22 <haerong22@naver.com>
2021-07-12 19:15:54 +0900

    thymeleaf_form : radio box

--- thymeleaf_form/src/main/java/hello/itemservice/domain/item/ItemType.java
+++ thymeleaf_form/src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -10,4 +10,8 @@ public enum ItemType {
     ItemType(String description) {
         this.description = description;
     }
+
+    public String getDescription() {
+        return description;
+    }
 }
--- thymeleaf_form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ thymeleaf_form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -2,6 +2,7 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -28,6 +29,11 @@ public class FormItemController {
         regions.put("BUSAN", "부산");
         regions.put("JEJU", "제주");
         return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes() {
+        return ItemType.values();
     }
 
     @GetMapping
