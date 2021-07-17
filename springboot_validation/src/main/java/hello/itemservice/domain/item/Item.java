6c0f5f7e88133a1b32ca7b86a6228c42c071d7ed
haerong22 <haerong22@naver.com>
2021-07-17 11:03:24 +0900

    springboot_validation : bean validation

--- springboot_validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ springboot_validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,26 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
