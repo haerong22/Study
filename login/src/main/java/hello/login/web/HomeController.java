40b6b1211a3eb45e6005db80d1bfd20c53c6bc0d
haerong22 <haerong22@naver.com>
2021-07-20 22:02:29 +0900

    login : http session(spring)

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -59,7 +60,7 @@ public class HomeController {
     }
 
     // http session 사용
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLogin3(HttpServletRequest request, Model model) {
 
         // 세션 조회
@@ -69,6 +70,19 @@ public class HomeController {
         }
 
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    // spring
+    @GetMapping("/")
+    public String homeLogin3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+                                   Model model) {
 
         if (loginMember == null) {
             return "home";
