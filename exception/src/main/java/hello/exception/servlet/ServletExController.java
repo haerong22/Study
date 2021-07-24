8586c31e2f676797a9676a5a63fdae417787c83d
haerong22 <haerong22@naver.com>
2021-07-25 00:28:50 +0900

    exception : exception page(springboot)

--- exception/src/main/java/hello/exception/servlet/ServletExController.java
+++ exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -21,6 +21,11 @@ public class ServletExController {
         response.sendError(404, "404 오류!");
     }
 
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400 오류!");
+    }
+
     @GetMapping("/error-500")
     public void error500(HttpServletResponse response) throws IOException {
         response.sendError(500);
