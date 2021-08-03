8665d427d894c1f4f284d806668eba1853ef14a5
haerong22 <haerong22@naver.com>
2021-08-03 18:37:17 +0900

    tdd : picking system - init

---
+++ tdd_practice/pickingTDD/src/main/java/com/example/pickingtdd/PickingTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.pickingtdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickingTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickingTddApplication.class, args);
+	}
+
+}
---
+++ tdd_practice/pickingTDD/src/test/java/com/example/pickingtdd/PickingTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pickingtdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickingTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
