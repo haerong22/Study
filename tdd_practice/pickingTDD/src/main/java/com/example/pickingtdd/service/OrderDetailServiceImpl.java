396111104f35c28ea320ebfdce3129472176cec2
haerong22 <haerong22@naver.com>
2021-08-04 22:56:41 +0900

    tdd : picking system - order detail vaildation

--- tdd_practice/pickingTDD/src/main/java/com/example/pickingtdd/service/OrderDetailServiceImpl.java
+++ tdd_practice/pickingTDD/src/main/java/com/example/pickingtdd/service/OrderDetailServiceImpl.java
@@ -6,7 +6,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderDetailServiceImpl implements OrderDetailService {
     @Override
-    public OrderDetail createOrderDetail(OrderDetail orderDetail) {
-        return orderDetail;
+    public OrderDetail createOrderDetail(OrderDetail orderDetail) throws Exception {
+        if (orderDetailValidation(orderDetail)) {
+            // do Something
+            return orderDetail;
+        } else {
+            throw new Exception("orderDetail validation fail");
+        }
+    }
+
+    private boolean orderDetailValidation(OrderDetail orderDetail) {
+        if (orderDetail.getOrderId() == null) {
+            return false;
+        }
+        if (orderDetail.getSku() == null) {
+            return false;
+        }
+        if (orderDetail.getAmount() < 1) {
+            return false;
+        }
+        return true;
     }
 }
--- tdd_practice/pickingTDD/src/test/java/com/example/pickingtdd/service/OrderDetailServiceTests.java
+++ tdd_practice/pickingTDD/src/test/java/com/example/pickingtdd/service/OrderDetailServiceTests.java
@@ -1,6 +1,7 @@
 package com.example.pickingtdd.service;
 
 import com.example.pickingtdd.entity.OrderDetail;
+import com.example.pickingtdd.entity.OrderStateEnum;
 import com.example.pickingtdd.entity.Sku;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -46,4 +47,24 @@ public class OrderDetailServiceTests {
         assertEquals(10, orderDetail.getAmount());
     }
 
+    @Test
+    void orderDetailValidation_success() {
+        OrderDetail orderDetail = new OrderDetail();
+        try {
+            orderDetail = orderDetailService.createOrderDetail(orderDetailSuccess);
+        } catch (Exception e) {
+            fail("should not throw exception");
+        }
+
+        assertEquals(1L, orderDetail.getOrderDetailId());
+    }
+
+    @Test
+    void orderValidation_fail() {
+        Exception e = assertThrows(Exception.class, () -> {
+            orderDetailService.createOrderDetail(orderDetailFail);
+        });
+
+        assertEquals("orderDetail validation fail", e.getMessage());
+    }
 }
