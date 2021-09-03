d7dec53511ee2f6bd54b70553d1a9ac2720d1363
haerong22 <haerong22@naver.com>
2021-09-03 12:13:16 +0900

    http encryption : aes

--- http-encryption/src/main/java/com/example/httpencryption/controller/TestController.java
+++ http-encryption/src/main/java/com/example/httpencryption/controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.httpencryption.controller;
 
 import com.example.httpencryption.dto.TestDto;
 import org.apache.commons.io.IOUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.nio.charset.StandardCharsets;
 @RestController
 public class TestController {
 
+    @CrossOrigin
     @PostMapping("/")
     public TestDto hello(HttpServletRequest request, @RequestBody TestDto dto) throws IOException {
 
