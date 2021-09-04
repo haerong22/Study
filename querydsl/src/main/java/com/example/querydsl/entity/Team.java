079fbe29cfd9b08bc554adb85e9d5451588ec0d1
haerong22 <haerong22@naver.com>
2021-09-05 01:19:39 +0900

    querydsl : example test

---
+++ querydsl/src/main/java/com/example/querydsl/entity/Team.java
@@ -0,0 +1,28 @@
+package com.example.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
