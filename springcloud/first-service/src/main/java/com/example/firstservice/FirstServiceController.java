8b0ecf8b288d41bbe37a832613daecab9a5148d7
haerong22 <haerong22@naver.com>
2021-09-17 01:50:01 +0900

    spring cloud : spring cloud gateway - custom filter, global filter

--- springcloud/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ springcloud/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -21,4 +21,9 @@ public class FirstServiceController {
         log.info(header);
         return "Hello World in First Service.";
     }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, there. This is a message from first Service.";
+    }
 }
