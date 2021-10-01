3a0c12727bd6d459b21607d47a2aa83f35ac974c
haerong22 <haerong22@naver.com>
2021-10-02 01:50:21 +0900

    spring core : 할인정책 변경(OCP, DIP 위반)

---
+++ spring-core/basic/src/main/java/com/example/basic/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package com.example.basic.discount;
+
+import com.example.basic.member.Grade;
+import com.example.basic.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private final int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
--- spring-core/basic/src/main/java/com/example/basic/order/OrderServiceImpl.java
+++ spring-core/basic/src/main/java/com/example/basic/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.basic.order;
 
 import com.example.basic.discount.DiscountPolicy;
 import com.example.basic.discount.FixDiscountPolicy;
+import com.example.basic.discount.RateDiscountPolicy;
 import com.example.basic.member.Member;
 import com.example.basic.member.MemberRepository;
 import com.example.basic.member.MemoryMemberRepository;
@@ -9,7 +10,8 @@ import com.example.basic.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
