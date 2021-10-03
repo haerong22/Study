64818c2b71cfb706ef0ff6e3e84a12442772eceb
haerong22 <haerong22@naver.com>
2021-10-03 18:43:52 +0900

    zoom : project setup

---
+++ zoom/src/server.js
@@ -0,0 +1,13 @@
+import express, { application } from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
