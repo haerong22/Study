5af6ebfeee69eb53d6c347e41a92dfeaf9579ea1
haerong22 <haerong22@naver.com>
2021-10-04 02:25:30 +0900

    spring core : xml config

---
+++ spring-core/basic/src/test/java/com/example/basic/xml/XmlAppContext.java
@@ -0,0 +1,18 @@
+package com.example.basic.xml;
+
+import com.example.basic.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
