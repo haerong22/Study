1c58883fcdd11a238c2a5de36f18c2607ee24f0b
haerong22 <haerong22@naver.com>
2021-10-16 20:54:34 +0900

    zoom : socket.io - admin-ui

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
-import SocketIo from "socket.io";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import express, { application } from "express";
 
 const app = express();
@@ -14,7 +15,16 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIo(httpServer);
+const wsServer = new Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true,
+    },
+});
+
+instrument(wsServer, {
+    auth: false,
+});
 
 function publicRooms() {
     const {sockets: {adapter: {sids, rooms}}} = wsServer;
