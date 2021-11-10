24836b84a8b0a8cc44a9222d80ee271149844a19
haerong22 <haerong22@naver.com>
2021-11-11 02:40:20 +0900

    spring core advanced : log trace v4 - template method

---
+++ spring-core/advanced/src/main/java/com/example/advanced/app/v4/OrderServiceV4.java
@@ -0,0 +1,26 @@
+package com.example.advanced.app.v4;
+
+import com.example.advanced.trace.logtrace.LogTrace;
+import com.example.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV4 {
+
+    private final OrderRepositoryV4 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId) {
+
+        AbstractTemplate<Void> template = new AbstractTemplate<Void>(trace) {
+            @Override
+            protected Void call() {
+                orderRepository.save(itemId);
+                return null;
+            }
+        };
+        template.execute("OrderService.orderItem()");
+    }
+}
---
+++ spring-core/advanced/src/main/java/com/example/advanced/trace/template/AbstractTemplate.java
@@ -0,0 +1,30 @@
+package com.example.advanced.trace.template;
+
+import com.example.advanced.trace.TraceStatus;
+import com.example.advanced.trace.logtrace.LogTrace;
+
+public abstract class AbstractTemplate<T> {
+
+    private final LogTrace trace;
+
+    public AbstractTemplate(LogTrace trace) {
+        this.trace = trace;
+    }
+
+    public T execute(String message) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin(message);
+
+            T result = call();
+
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    protected abstract T call();
+}
