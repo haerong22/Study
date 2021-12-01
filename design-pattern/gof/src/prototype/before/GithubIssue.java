b5592a6d89330bcc3b7920c332a5cdbd93e2e649
haerong22 <haerong22@naver.com>
2021-12-02 01:58:09 +0900

    design patterns : prototype

---
+++ design-pattern/gof/src/prototype/before/GithubIssue.java
@@ -0,0 +1,41 @@
+package prototype.before;
+
+public class GithubIssue {
+
+    private int id;
+
+    private String title;
+
+    private GithubRepository repository;
+
+    public GithubIssue(GithubRepository repository) {
+        this.repository = repository;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public GithubRepository getRepository() {
+        return repository;
+    }
+
+    public String getUrl() {
+        return String.format("https://github.com/%s/%s/issues/%d",
+                repository.getUser(),
+                repository.getName(),
+                this.getId());
+    }
+}
---
+++ design-pattern/gof/src/prototype/before/GithubRepository.java
@@ -0,0 +1,24 @@
+package prototype.before;
+
+public class GithubRepository {
+
+    private String user;
+
+    private String name;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
