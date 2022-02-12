9535fffa7a6cf801b0ffcc34cc99035ddc74819c
haerong22 <haerong22@naver.com>
2022-02-12 23:11:03 +0900

    refactoring : init

---
+++ refactoring/src/test/java/com/example/refactoring/RefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.refactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
