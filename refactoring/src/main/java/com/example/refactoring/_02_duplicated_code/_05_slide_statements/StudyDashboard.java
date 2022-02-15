5eb3995dc889c09b6ae945b7b8e9db72b522aa39
haerong22 <haerong22@naver.com>
2022-02-16 01:24:38 +0900

    refactoring : duplicated code - slide statements

---
+++ refactoring/src/main/java/com/example/refactoring/_02_duplicated_code/_05_slide_statements/StudyDashboard.java
@@ -0,0 +1,51 @@
+package com.example.refactoring._02_duplicated_code._05_slide_statements;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 관련있는 코드끼리 묶어서 정리
+ */
+public class StudyDashboard {
+
+    private void printParticipants(int eventId) throws IOException {
+        // Get github issue to check homework
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("haerong22/Study");
+        GHIssue issue = repository.getIssue(eventId);
+
+        // Get participants
+        Set<String> participants = new HashSet<>(); // participants 를 사용하는 코드 위에 선언
+        issue.getComments().forEach(c -> participants.add(c.getUserName()));
+
+        // Print participants
+        participants.forEach(System.out::println);
+    }
+
+    private void printReviewers() throws IOException {
+        // Get github issue to check homework
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("haerong22/Study");
+        GHIssue issue = repository.getIssue(1);
+
+        // Get reviewers
+        Set<String> reviewers = new HashSet<>();
+        issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
+
+        // Print reviewers
+        reviewers.forEach(System.out::println);
+    }
+
+    public static void main(String[] args) throws IOException {
+        StudyDashboard studyDashboard = new StudyDashboard();
+        studyDashboard.printReviewers();
+        studyDashboard.printParticipants(1);
+
+    }
+
+}
