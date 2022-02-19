53d89db12cd0c2cb7bd6987bba645c3232d17a53
haerong22 <haerong22@naver.com>
2022-02-19 22:38:31 +0900

    refactoring : long function - introduce parameter object

---
+++ refactoring/src/main/java/com/example/refactoring/_03_long_function/_08_introduce_parameter_object/Participant.java
@@ -0,0 +1,22 @@
+package com.example.refactoring._03_long_function._08_introduce_parameter_object;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public record Participant(String username, Map<Integer, Boolean> homework) {
+    public Participant(String username) {
+        this(username, new HashMap<>());
+    }
+
+    public double getRate(double total) {
+        long count = this.homework.values().stream()
+                .filter(v -> v == true)
+                .count();
+        return count * 100 / total;
+    }
+
+    public void setHomeworkDone(int index) {
+        this.homework.put(index, true);
+    }
+
+}
