0a2b13f4a70e66305defd638d82bde0bbf4c929d
haerong22 <haerong22@naver.com>
2022-02-22 23:34:44 +0900

    refactoring : long function - decompose conditional

---
+++ refactoring/src/main/java/com/example/refactoring/_03_long_function/_11_decompose_conditional/StudyDashboard.java
@@ -0,0 +1,88 @@
+package com.example.refactoring._03_long_function._11_decompose_conditional;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class StudyDashboard {
+
+    private final int totalNumberOfEvents;
+
+    public StudyDashboard(int totalNumberOfEvents) {
+        this.totalNumberOfEvents = totalNumberOfEvents;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        StudyDashboard studyDashboard = new StudyDashboard(15);
+        studyDashboard.print();
+    }
+
+    /**
+     * 조건문을 의도에 따라 추출하여 코드를 간단하게 줄여 메소드의 의도를 쉽게 파악할 수 있다.
+     */
+    private Participant findParticipant(String username, List<Participant> participants) {
+        return isNewParticipant(username, participants) ?
+                createNewParticipant(username, participants) : findExistingParticipant(username, participants);
+    }
+
+    private Participant findExistingParticipant(String username, List<Participant> participants) {
+        return participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
+    }
+
+    private Participant createNewParticipant(String username, List<Participant> participants) {
+        Participant participant;
+        participant = new Participant(username);
+        participants.add(participant);
+        return participant;
+    }
+
+    private boolean isNewParticipant(String username, List<Participant> participants) {
+        return participants.stream().noneMatch(p -> p.username().equals(username));
+    }
+
+    private void print() throws IOException, InterruptedException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        List<Participant> participants = new CopyOnWriteArrayList<>();
+
+        ExecutorService service = Executors.newFixedThreadPool(8);
+        CountDownLatch latch = new CountDownLatch(totalNumberOfEvents);
+
+        for (int index = 1 ; index <= totalNumberOfEvents ; index++) {
+            int eventId = index;
+            service.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        GHIssue issue = repository.getIssue(eventId);
+                        List<GHIssueComment> comments = issue.getComments();
+
+                        for (GHIssueComment comment : comments) {
+                            Participant participant = findParticipant(comment.getUserName(), participants);
+                            participant.setHomeworkDone(eventId);
+                        }
+
+                        latch.countDown();
+                    } catch (IOException e) {
+                        throw new IllegalArgumentException(e);
+                    }
+                }
+            });
+        }
+
+        latch.await();
+        service.shutdown();
+
+        new StudyPrinter(this.totalNumberOfEvents, participants).execute();
+    }
+
+}
+
