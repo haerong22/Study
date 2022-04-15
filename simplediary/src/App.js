035cd6cd7e77482c76165060e9b01aefb7a6ffa8
haerong22 <haerong22@naver.com>
2022-04-15 15:21:19 +0900

    simple diary : init data

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,36 +1,34 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "kim",
-//     content: "hihi",
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 2,
-//     author: "lee",
-//     content: "hihi",
-//     emotion: 2,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: "park",
-//     content: "hihi",
-//     emotion: 4,
-//     created_date: new Date().getTime(),
-//   },
-// ];
 
 const App = () => {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
