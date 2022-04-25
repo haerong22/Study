ee84f9116fad61dfb7b783826df5c550e5078209
haerong22 <haerong22@naver.com>
2022-04-26 00:24:36 +0900

    nft ex : burn

---
+++ blockchain/nft-ex/js/jscript.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // page 이동
+    $("#page_mynft_detail").on("click", function () {
+      window.location.href = "./mynft_detail.html";
+    });
+  
+    $("#page_mynft").on("click", function () {
+      window.location.href = "./mynft.html";
+    });
+});
