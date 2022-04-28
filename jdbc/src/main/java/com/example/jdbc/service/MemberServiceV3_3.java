829d720670e160c1e2f9d2ff8c044437e4cf10d7
haerong22 <haerong22@naver.com>
2022-04-29 00:44:30 +0900

    closed #2 jdbc: transaction aop

---
+++ jdbc/src/main/java/com/example/jdbc/service/MemberServiceV3_3.java
@@ -0,0 +1,44 @@
+package com.example.jdbc.service;
+
+import com.example.jdbc.domain.Member;
+import com.example.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - @Transactional AOP
+ */
+@Slf4j
+public class MemberServiceV3_3 {
+
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+
+        validation(toMember);
+
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
