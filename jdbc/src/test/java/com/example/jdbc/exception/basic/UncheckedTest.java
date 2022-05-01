c88287982c6b29b9521e4f2057bb72ba78fad400
haerong22 <haerong22@naver.com>
2022-05-01 17:42:56 +0900

    closed #6 jdbc: unchecked exception ex

---
+++ jdbc/src/test/java/com/example/jdbc/exception/basic/UncheckedTest.java
@@ -0,0 +1,67 @@
+package com.example.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+public class UncheckedTest {
+
+    @Test
+    void unchecked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void unchecked_throw() {
+        Service service = new Service();
+        assertThatThrownBy(service::callThrow)
+                .isInstanceOf(MyUncheckedException.class);
+    }
+
+    /**
+     * RuntimeException 을 상속받은 예외는 언체크 예외가 된다.
+     */
+    static class MyUncheckedException extends RuntimeException {
+
+        public MyUncheckedException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * Unchecked 예외는
+     * 예외를 잡거나, 던지지 않아도 된다.
+     * 예외를 잡지 않으면 자동으로 밖으로 던진다.
+     */
+    static class Service {
+        Repository repository = new Repository();
+
+        /**
+         * 필요한 경우 예외를 잡어서 처리하면 된다.
+         */
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyUncheckedException e) {
+                // 예외처리 로직
+                log.info("예외처리, message={}", e.getMessage(), e);
+            }
+        }
+
+        /**
+         * 체크 예외와 다르게 throws 예외 선언을 하지 않아도 된다.
+         */
+        public void callThrow() {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+        public void call() {
+            throw new MyUncheckedException("ex");
+        }
+    }
+}
