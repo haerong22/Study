a149d51f110dad38290791d9fd78f6a136ee6479
haerong22 <haerong22@naver.com>
2022-05-03 00:43:02 +0900

    #9 custom data access exception

---
+++ jdbc/src/main/java/com/example/jdbc/repository/ex/MyDuplicateKeyException.java
@@ -0,0 +1,19 @@
+package com.example.jdbc.repository.ex;
+
+public class MyDuplicateKeyException extends MyDbException {
+
+    public MyDuplicateKeyException() {
+    }
+
+    public MyDuplicateKeyException(String message) {
+        super(message);
+    }
+
+    public MyDuplicateKeyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyDuplicateKeyException(Throwable cause) {
+        super(cause);
+    }
+}
