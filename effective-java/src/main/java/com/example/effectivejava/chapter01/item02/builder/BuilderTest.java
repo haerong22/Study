668c2d69c3897d9c41bece4dba90bbd2f160ff0e
haerong22 <haerong22@naver.com>
2022-05-11 02:07:35 +0900

    #10 effective java: item 2

---
+++ effective-java/src/main/java/com/example/effectivejava/chapter01/item02/builder/BuilderTest.java
@@ -0,0 +1,10 @@
+package com.example.effectivejava.chapter01.item02.builder;
+
+public class BuilderTest {
+
+    public static void main(String[] args) {
+        new NutritionFacts.Builder(10, 10)
+                .calories(10)
+                .build();
+    }
+}
---
+++ effective-java/src/main/java/com/example/effectivejava/chapter01/item02/hierarchicalbuilder/PizzaTest.java
@@ -0,0 +1,19 @@
+package com.example.effectivejava.chapter01.item02.hierarchicalbuilder;
+
+import static com.example.effectivejava.chapter01.item02.hierarchicalbuilder.NyPizza.Size.SMALL;
+import static com.example.effectivejava.chapter01.item02.hierarchicalbuilder.Pizza.Topping.*;
+
+// 계층적 빌더 사용 (21쪽)
+public class PizzaTest {
+    public static void main(String[] args) {
+        NyPizza pizza = new NyPizza.Builder(SMALL)
+                .addTopping(SAUSAGE)
+                .addTopping(ONION).build();
+
+        Calzone calzone = new Calzone.Builder()
+                .addTopping(HAM).sauceInside().build();
+        
+        System.out.println(pizza);
+        System.out.println(calzone);
+    }
+}
