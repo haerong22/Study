190e3a0b60b7784a73a3cdf441ddb856d16ce80d
haerong22 <haerong22@naver.com>
2022-05-16 00:05:28 +0900

    chatting: websocket(stomp), redis pub/sub

---
+++ blog/chatting/server/src/main/java/com/example/chatting/redis/RedisSubscriber.java
@@ -0,0 +1,30 @@
+package com.example.chatting.redis;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class RedisSubscriber implements MessageListener {
+
+    private final ObjectMapper objectMapper;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        try {
+            String pubMessage = redisTemplate.getStringSerializer().deserialize(message.getBody());
+            messagingTemplate.convertAndSend("/sub/message", pubMessage == null ? "" : pubMessage);
+        } catch (Exception e) {
+            log.error("Subscriber Error", e);
+        }
+    }
+}
