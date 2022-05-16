b4d9a0a296d2a3c02440cfeaaf3641ec2f211427
haerong22 <haerong22@naver.com>
2022-05-17 02:26:19 +0900

    #10 effective java: item 4

---
+++ effective-java/src/main/java/com/example/effectivejava/chapter01/item04/DefaultUtilityClass.java
@@ -0,0 +1,9 @@
+package com.example.effectivejava.chapter01.item04;
+
+public class DefaultUtilityClass extends AbstractUtilityClass {
+
+    public static void main(String[] args) {
+        DefaultUtilityClass utilityClass = new DefaultUtilityClass();
+        utilityClass.hello();
+    }
+}
---
+++ effective-java/src/main/java/com/example/effectivejava/chapter01/item04/UtilityClass.java
@@ -0,0 +1,24 @@
+package com.example.effectivejava.chapter01.item04;
+
+public class UtilityClass {
+
+    /**
+     * 이 클래스는 인스턴스를 만들 수 없습니다.
+     */
+    private UtilityClass() {
+        throw new AssertionError();
+    }
+
+    public static String hello() {
+        return "hello";
+    }
+
+    public static void main(String[] args) {
+        String hello = UtilityClass.hello();
+
+        UtilityClass utilityClass = new UtilityClass();
+        utilityClass.hello();
+
+
+    }
+}
