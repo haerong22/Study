8188d0fc152f1f7c5995faeaf46bc17b7a1bad5e
haerong22 <haerong22@naver.com>
2022-05-24 07:50:57 +0900

    #10 effective java: item 6

---
+++ effective-java/src/main/java/com/example/effectivejava/chapter01/item06/Deprecation.java
@@ -0,0 +1,18 @@
+package com.example.effectivejava.chapter01.item06;
+
+public class Deprecation {
+
+    /**
+     * @deprecated in favor of
+     * {@link #Deprecation(String)}
+     */
+    @Deprecated(forRemoval = false, since = "1.2")
+    public Deprecation() {
+    }
+
+    private String name;
+
+    public Deprecation(String name) {
+        this.name = name;
+    }
+}
