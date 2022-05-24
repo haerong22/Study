fe6feed168ac5ccaab0e35a268a79fd2f6e68c64
haerong22 <haerong22@naver.com>
2022-05-25 02:11:12 +0900

    #10 effective java: item 7

---
+++ effective-java/src/main/java/com/example/effectivejava/chapter01/item07/listener/User.java
@@ -0,0 +1,8 @@
+package com.example.effectivejava.chapter01.item07.listener;
+
+public class User {
+
+    public void receive(String message) {
+        System.out.println(message);
+    }
+}
---
+++ effective-java/src/test/java/com/example/effectivejava/chapter01/item07/listener/ChatRoomTest.java
@@ -0,0 +1,32 @@
+package com.example.effectivejava.chapter01.item07.listener;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChatRoomTest {
+
+    @Test
+    void charRoom() throws InterruptedException {
+        ChatRoom chatRoom = new ChatRoom();
+        User user1 = new User();
+        User user2 = new User();
+
+        chatRoom.addUser(user1);
+        chatRoom.addUser(user2);
+
+        chatRoom.sendMessage("hello");
+
+        user1 = null;
+
+        System.gc();
+
+        Thread.sleep(5000L);
+
+        List<WeakReference<User>> users = chatRoom.getUsers();
+        assertEquals(1, users.size());
+    }
+}
