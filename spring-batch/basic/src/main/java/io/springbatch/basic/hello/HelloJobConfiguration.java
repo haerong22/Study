635828eb4a7c29c8e39b48e1b8db14ce38df6992
haerong22 <haerong22@naver.com>
2022-05-30 00:06:42 +0900

    spring batch: hello spring batch

---
+++ spring-batch/basic/src/main/java/io/springbatch/basic/hello/HelloJobConfiguration.java
@@ -0,0 +1,61 @@
+package io.springbatch.basic.hello;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloJobConfiguration {
+
+    private final JobBuilderFactory  jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloJob() {
+        return jobBuilderFactory.get("helloJob")
+                .start(helloStep1())
+                .next(helloStep2())
+                .build();
+    }
+
+    @Bean
+    public Step helloStep1() {
+        return stepBuilderFactory.get("helloStep1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+                        System.out.println(" ======================= ");
+                        System.out.println(" >> Hello Spring Batch!! ");
+                        System.out.println(" ======================= ");
+
+                        return RepeatStatus.FINISHED; // tasklet 은 무한반복하므로 FINISHED 또는 null 일 경우 종료
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step helloStep2() {
+        return stepBuilderFactory.get("helloStep2")
+                .tasklet((contribution, chunkContext) -> {
+
+                    System.out.println(" ======================= ");
+                    System.out.println(" >> Step2 was execute!!! ");
+                    System.out.println(" ======================= ");
+
+                    return RepeatStatus.FINISHED; // tasklet 은 무한반복하므로 FINISHED 또는 null 일 경우 종료
+                })
+                .build();
+    }
+
+}
