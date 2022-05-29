16af1f17d450f8982fce22b07d1f27de8944e15c
haerong22 <haerong22@naver.com>
2022-05-29 13:33:05 +0900

    spring batch basic: init

---
+++ spring-batch/basic/src/main/java/io/springbatch/basic/BasicApplication.java
@@ -0,0 +1,15 @@
+package io.springbatch.basic;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class BasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicApplication.class, args);
+    }
+
+}
---
+++ spring-batch/basic/src/test/java/io/springbatch/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbatch.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
