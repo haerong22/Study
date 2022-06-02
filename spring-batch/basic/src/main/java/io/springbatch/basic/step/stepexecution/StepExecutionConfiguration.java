7ce9a013f2ded96f0311c869f3a26ecf5e6b7c73
haerong22 <haerong22@naver.com>
2022-06-03 01:41:51 +0900

    #11 spring batch: StepExecution

--- spring-batch/basic/src/main/java/io/springbatch/basic/step/stepexecution/StepExecutionConfiguration.java
+++ spring-batch/basic/src/main/java/io/springbatch/basic/step/stepexecution/StepExecutionConfiguration.java
@@ -1,4 +1,4 @@
-package io.springbatch.basic.step;
+package io.springbatch.basic.step.stepexecution;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
@@ -9,9 +9,9 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class StepConfiguration {
+public class StepExecutionConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -21,9 +21,21 @@ public class StepConfiguration {
         return jobBuilderFactory.get("job")
                 .start(step1())
                 .next(step2())
+                .next(step3())
                 .build();
     }
 
+    /*
+        StepExecution
+
+        - Step 에 대한 한번의 시도
+        - Step 실행시 생성(성공, 실패)
+        - 이전단계 Step 이 실패하면 다음 StepExecution 은 생성되지 않음
+        - Step 이 실패하면 Job 이 실패
+        - Job 을 재실행하면 실패한 Step 만 실행
+        - BATCH_STEP_EXECUTION 테이블과 매핑
+        - JobExecution : StepExecution = 1:N
+    */
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
@@ -34,22 +46,24 @@ public class StepConfiguration {
                 .build();
     }
 
-    /*
-        Step 의 기본 구현체
-        - TaskletStep : 기본 구현체
-        - PartitionStep : 멀티 스레드 방식으로 Step 을 여러 개로 분리 실행
-        - JobStep : Step 내에서 Job 실행
-        - FlowStep : Step 내에서 Flow 실행
-
-        1. TaskletStep 생성 (stepName, tasklet 저장)
-        2. SimpleJob 생성 (tasklet 가지고 있음)
-        3. SimpleJobLauncher 가 SimpleJob 실행 (StepHandler 가 실행)
-        4. 내부 TaskletStep 실행
-     */
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(new CustomTasklet())
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step2 was executed");
+//                    throw new RuntimeException("step2 has failed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step3 was executed");
+                    return RepeatStatus.FINISHED;
+                })
                 .build();
     }
 }
