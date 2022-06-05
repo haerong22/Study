1bf109d360cd23f194a37807ba966c1b429141f2
haerong22 <haerong22@naver.com>
2022-06-05 18:49:29 +0900

    #11 spring batch: ExecutionContext

---
+++ spring-batch/basic/src/main/java/io/springbatch/basic/executioncontext/ExecutionContextTasklet4.java
@@ -0,0 +1,21 @@
+package io.springbatch.basic.executioncontext;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExecutionContextTasklet4 implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        System.out.println("step4 was executed");
+
+        Object name = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().get("name");
+        System.out.println("name = " + name);
+
+        return RepeatStatus.FINISHED;
+    }
+}
