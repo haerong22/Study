6d071b17fb317b303423e755673040111c8d35bb
haerong22 <haerong22@naver.com>
2022-06-18 18:55:40 +0900

    #13 spring db: JdbcTemplate - basic

--- spring-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ spring-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -9,7 +9,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 
-@Import(MemoryConfig.class)
+//@Import(MemoryConfig.class)
+@Import(JdbcTemplateV1Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
