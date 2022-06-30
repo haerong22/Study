e11cdb9664901f88bb15f94603bb60c191c7d06e
haerong22 <haerong22@naver.com>
2022-07-01 00:36:32 +0900

    #11 spring batch: SimpleFlow ex

--- spring-batch/basic/src/main/java/io/springbatch/basic/flow/SimpleFlowConfiguration.java
+++ spring-batch/basic/src/main/java/io/springbatch/basic/flow/SimpleFlowConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 @RequiredArgsConstructor
 public class SimpleFlowConfiguration {
 
