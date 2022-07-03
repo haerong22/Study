4663a90b90b17d11279b3a760e85e84a18693dc8
haerong22 <haerong22@naver.com>
2022-07-03 19:37:55 +0900

    #13 spring db: jpa - basic

--- spring-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ spring-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -17,7 +17,8 @@ import javax.sql.DataSource;
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
 //@Import(JdbcTemplateV3Config.class)
-@Import(MyBatisConfig.class)
+//@Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
--- spring-db/src/main/java/hello/itemservice/domain/Item.java
+++ spring-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,16 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 20)
     private String itemName;
     private Integer price;
     private Integer quantity;
