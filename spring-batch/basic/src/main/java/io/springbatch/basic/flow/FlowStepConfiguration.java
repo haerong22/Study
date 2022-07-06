4a3fe7c987ee3041f76454ab8139af7c3b1eb483
haerong22 <haerong22@naver.com>
2022-07-07 01:41:37 +0900

    #11 spring batch: FlowStep

---
+++ spring-batch/basic/src/main/java/io/springbatch/basic/flow/FlowStepConfiguration.java
@@ -0,0 +1,87 @@
+package io.springbatch.basic.flow;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class FlowStepConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    /*
+        FlowStep
+        - Step 내에 Flow 를 할당하여 실행시키는 도메인 객체
+        - FlowStep 의 BatchStatus 와 ExitStatus 는 Flow 의 최종 상태값에 따라 결정된다.
+
+        API
+        - StepBuilderFactory > StepBuilder > FlowStepBuilder > FlowStep
+
+     */
+    @Bean
+    public Job batchJob() {
+        return jobBuilderFactory.get("batchJob")
+                .incrementer(new RunIdIncrementer())
+                .start(flowStep())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step flowStep() {
+        return stepBuilderFactory.get("flowStep")
+                .flow(flow())
+                .build();
+    }
+
+    public Flow flow() {
+
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+
+        flowBuilder
+                .start(step1())
+                .end();
+
+        return flowBuilder.build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step1 was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step2 was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step3 was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
