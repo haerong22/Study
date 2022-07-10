297b9615847694b43865a8818ba1d9ebe731b5ec
haerong22 <haerong22@naver.com>
2022-07-10 18:06:39 +0900

    #13 spring db: spring data jpa - basic

--- spring-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ spring-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -18,7 +18,8 @@ import javax.sql.DataSource;
 //@Import(JdbcTemplateV2Config.class)
 //@Import(JdbcTemplateV3Config.class)
 //@Import(MyBatisConfig.class)
-@Import(JpaConfig.class)
+//@Import(JpaConfig.class)
+@Import(SpringDataJpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
