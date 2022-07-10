3b430bdf3dc7a98597978f339415bf9f62f4eb17
haerong22 <haerong22@naver.com>
2022-07-10 20:47:59 +0900

    #11 spring batch: @JobScope, @StepScope

--- spring-batch/basic/src/main/java/io/springbatch/basic/flow/ScopeConfiguration2.java
+++ spring-batch/basic/src/main/java/io/springbatch/basic/flow/ScopeConfiguration2.java
@@ -14,37 +14,30 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class ScopeConfiguration {
+public class ScopeConfiguration2 {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     /*
-        1. Scope
-            - 스프링 컨테이너 에서 빈이 관리되는 범위
-            - singleton, prototype, request, session, application 있으며 기본은 singleton
+        1. Proxy 객체 생성
+            - @JobScope, @StepScope 어노테이션이 붙은 빈 선언은 내부적으로 빈의 Proxy 객체가 생성
+                - @JobScope
+                    - @Scope(value="job", proxyMode=ScopedProxyMode.TARGET_CLASS)
+                - @StepScope
+                    - @Scope(value="step", proxyMode=ScopedProxyMode.TARGET_CLASS)
 
-        2. 스프링 배치 스코프
-            @JobScope, @StepScope
-            - Job 과 Step 의 빈 생성과 실행에 관여하는 스코프
-            - 프록시 모드를 기본값으로 하는 스코프 - @Scope(value = "job", proxyMode = ScopedProxyMode.TARGET_CLASS)
-            - 해당 스코프가 선언되면 빈의 생성이 어플리케이션 구동시점이 아닌 빈의 실행시점에 이루어진다.
-                - @Values 를 주입해서 빈의 실행 시점에 값을 참조할 수 있으며 일종의 Lazy Binding 이 가능해진다.
-                - @Value("#{jobParameters[파라미터명]}"),
-                  @Value("#{jobExecutionContext[파라미터명]}"),
-                  @Value("#{stepExecutionContext[파라미터명]}")
-            - 프록시 모드로 빈이 선언되기 때문에 어플리케이션 구동시점에는 빈의 프록시 객체가 생성되어 실행 시점에 실제 빈을 호출.
-            - 병렬처리 시 각 스레드마다 생성된 스코프 빈이 할당되기 때문에 스레드에 안전하게 실행 가능
+            - Job 실행 시 Proxy 객체가 실제 빈을 호출해서 해당 메소드를 실행 시키는 구조
 
-        3. @JopScope
-            - Step 선언문에 정의
-            - @Value: jobParameter, jobExecutionContext 만 사용가능
+        2. JobScope, StepScope
+            - Proxy 객체의 실제 대상이 되는 Bean 을 등록, 해제하는 역할
+            - 실제 빈을 저장하고 있는 JobContext, StepContext 를 가지고 있다.
 
-        4. @StepScope
-            - Tasklet 이나 ItemReader, ItemWriter, ItemProcessor 선언문에 정의
-            - @Value: jobParameter, jobExecutionContext, stepExecutionContext 사용가능
+        3. JobContext, StepContext
+            - 스프링 컨테이너에서 생성된 빈을 저장하는 컨텍스트 역할
+            - Job 의 실행 시점에서 프록시 객체가 실제 빈을 참조할 때 사용
      */
     @Bean
     public Job batchJob() {
