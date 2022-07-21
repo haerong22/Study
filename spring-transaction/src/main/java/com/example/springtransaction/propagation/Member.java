9a97fcd8fd46af122f77c0fd5895dc86bc43e265
haerong22 <haerong22@naver.com>
2022-07-22 02:50:11 +0900

    #13 spring transaction : propagation ex init

---
+++ spring-transaction/src/main/java/com/example/springtransaction/propagation/Member.java
@@ -0,0 +1,24 @@
+package com.example.springtransaction.propagation;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+
+    public Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+}
