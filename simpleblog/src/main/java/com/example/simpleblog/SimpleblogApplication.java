9037722220e92287a22a26d02a396043f04b51d6
haerong22 <haerong22@naver.com>
2022-07-23 03:42:44 +0900

    #14 simple blog : init

---
+++ simpleblog/src/main/java/com/example/simpleblog/SimpleblogApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleblogApplication.class, args);
+    }
+
+}
---
+++ simpleblog/src/test/java/com/example/simpleblog/SimpleblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
