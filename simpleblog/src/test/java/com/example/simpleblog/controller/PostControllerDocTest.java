6750a3f7f249e490c262c3272fcfbeaf0322bc10
haerong22 <haerong22@naver.com>
2022-07-30 06:14:17 +0900

    #14 simple blog : spring rest docs custom

--- simpleblog/src/test/java/com/example/simpleblog/controller/PostControllerDocTest.java
+++ simpleblog/src/test/java/com/example/simpleblog/controller/PostControllerDocTest.java
@@ -2,44 +2,47 @@ package com.example.simpleblog.controller;
 
 import com.example.simpleblog.domain.Post;
 import com.example.simpleblog.repository.PostRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.example.simpleblog.request.PostCreate;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.simpleblog.com", uriPort = 443)
 @ExtendWith(RestDocumentationExtension.class)
 public class PostControllerDocTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private PostRepository postRepository;
 
-    @BeforeEach
-    public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .build();
-    }
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     @DisplayName("글 단건 조회 테스트")
-    void test() throws Exception {
+    void get_post() throws Exception {
         // given
         Post post = Post.builder()
                 .title("제목")
@@ -54,6 +57,43 @@ public class PostControllerDocTest {
                 )
                 .andExpect(status().isOk())
                 .andDo(print())
-                .andDo(document("index"));
+                .andDo(document("post-inquiry",
+                        pathParameters(
+                                parameterWithName("postId").description("게시글 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("게시글 ID"),
+                                fieldWithPath("title").description("제목"),
+                                fieldWithPath("content").description("내용")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("글 등록 테스트")
+    void add_post() throws Exception {
+        // given
+        PostCreate request = PostCreate.builder()
+                .title("글 제목입니다.")
+                .content("글 내용입니다.")
+                .build();
+
+        String json = objectMapper.writeValueAsString(request);
+
+        // expected
+        this.mockMvc.perform(post("/posts/")
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("post-create",
+                        requestFields(
+                                fieldWithPath("title").description("제목")
+                                        .attributes(key("constraint").value("좋은 제목 입력해주세요.")),
+                                fieldWithPath("content").description("내용").optional()
+                        )
+                ));
     }
 }
