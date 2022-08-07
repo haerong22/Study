ead357016dec49d0dc75a64d7ca80343f0981a97
haerong22 <haerong22@naver.com>
2022-08-08 03:30:38 +0900

    #16 board : data rest test disabled

--- board/src/test/java/com/example/board/controller/DataRestTest.java
+++ board/src/test/java/com/example/board/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.example.board.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled("Spring Data REST 통합테스트는 불필요하므로 제외")
 @DisplayName("Data REST - API 테스트")
 @AutoConfigureMockMvc
 @Transactional
