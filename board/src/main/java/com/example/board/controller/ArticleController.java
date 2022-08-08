7ac0d164f5b9928beecc81bf9b53a6e3efc839b6
haerong22 <haerong22@naver.com>
2022-08-09 00:32:01 +0900

    #16 board : view - endpoint test

---
+++ board/src/main/java/com/example/board/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package com.example.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
---
+++ board/src/test/java/com/example/board/controller/ArticleControllerTest.java
@@ -0,0 +1,82 @@
+package com.example.board.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mockMvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] 게시글 리스트(게시판) 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+        // given
+
+        // when & then
+        mockMvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"))
+        ;
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+        // given
+
+        // when & then
+        mockMvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"))
+        ;
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
+        // given
+
+        // when & then
+        mockMvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search"))
+        ;
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
+        // given
+
+        // when & then
+        mockMvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search-hashtag"))
+        ;
+    }
+}
