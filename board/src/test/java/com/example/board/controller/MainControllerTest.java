f955f9da20cc15f5a480bf4515ab66489f3e5161
haerong22 <haerong22@naver.com>
2022-08-13 03:31:42 +0900

    #16 board : main controller, test

---
+++ board/src/test/java/com/example/board/controller/MainControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.board.controller;
+
+import com.example.board.config.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Import(SecurityConfig.class)
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+
+    private final MockMvc mockMvc;
+
+    public MainControllerTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
+        // given
+
+        // when & then
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("forward:/articles"))
+                .andExpect(forwardedUrl("/articles"))
+                .andDo(print())
+        ;
+    }
+}
