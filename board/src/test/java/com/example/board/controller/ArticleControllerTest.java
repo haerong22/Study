05a75ee55bdb3e918f671d87a8e6d3405efc70eb
haerong22 <haerong22@naver.com>
2022-08-14 04:17:09 +0900

    #16 board : article controller impl

--- board/src/test/java/com/example/board/controller/ArticleControllerTest.java
+++ board/src/test/java/com/example/board/controller/ArticleControllerTest.java
@@ -1,15 +1,25 @@
 package com.example.board.controller;
 
 import com.example.board.config.SecurityConfig;
+import com.example.board.dto.ArticleWithCommentsDto;
+import com.example.board.dto.UserAccountDto;
+import com.example.board.service.ArticleService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -20,6 +30,9 @@ class ArticleControllerTest {
 
     private final MockMvc mockMvc;
 
+    @MockBean
+    private ArticleService articleService;
+
     public ArticleControllerTest(@Autowired MockMvc mockMvc) {
         this.mockMvc = mockMvc;
     }
@@ -28,6 +41,8 @@ class ArticleControllerTest {
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
         // given
+        given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class)))
+                .willReturn(Page.empty());
 
         // when & then
         mockMvc.perform(get("/articles"))
@@ -36,21 +51,28 @@ class ArticleControllerTest {
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"))
         ;
+
+        then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
     }
 
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         // given
+        Long articleId = 1L;
+
+        given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentDto());
 
         // when & then
-        mockMvc.perform(get("/articles/1"))
+        mockMvc.perform(get("/articles/" + articleId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"))
         ;
+
+        then(articleService).should().getArticle(articleId);
     }
 
     @Disabled("구현 중")
@@ -79,5 +101,35 @@ class ArticleControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search-hashtag"))
         ;
+    }
+
+    private ArticleWithCommentsDto createArticleWithCommentDto() {
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                "#java",
+                LocalDateTime.now(),
+                "bobby",
+                LocalDateTime.now(),
+                "bobby"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+                1L,
+                "bobby",
+                "1234",
+                "bobby@email.com",
+                "bobby",
+                "memo",
+                LocalDateTime.now(),
+                "bobby",
+                LocalDateTime.now(),
+                "bobby"
+        );
     }
 }
