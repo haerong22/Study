ada77599473cb6cc66c7f2e20af54f6784c3426e
haerong22 <haerong22@naver.com>
2022-08-14 17:56:10 +0900

    #17 react-todo-app : init

---
+++ react/todo-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할 일 목록</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
