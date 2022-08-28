19ff2b1c1c963dd99cb6735ac11369754c57adc7
haerong22 <haerong22@naver.com>
2022-08-28 19:42:23 +0900

    #17 react-todo-app : edit todoData

--- react/todo-app/src/components/List.js
+++ react/todo-app/src/components/List.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const List = ({
   id,
@@ -10,7 +10,8 @@ const List = ({
   snapshot,
   handleClick,
 }) => {
-  console.log("List component");
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(title);
 
   const handleCompleteChange = (id) => {
     let newTodoData = todoData.map((data) => {
@@ -22,34 +23,94 @@ const List = ({
 
     setTodoData(newTodoData);
   };
-  return (
-    <div
-      key={id}
-      {...provided.draggableProps}
-      ref={provided.innerRef}
-      {...provided.dragHandleProps}
-      className={`${
-        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600  border rounded`}
-    >
-      <div className="items-center">
-        <input
-          type={"checkbox"}
-          defaultChecked={false}
-          onChange={() => handleCompleteChange(id)}
-        />{" "}
-        <span className={completed ? "line-through" : undefined}>{title}</span>
+
+  const handleEditChange = (e) => {
+    setEditedTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let newTodoData = todoData.map((data) => {
+      if (data.id === id) {
+        data.title = editedTitle;
+      }
+      return data;
+    });
+
+    setTodoData(newTodoData);
+    setIsEditing(false);
+  };
+
+  if (isEditing) {
+    return (
+      <div
+        className={`bg-gray-100 flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600  border rounded`}
+      >
+        <div className="items-center">
+          <form onSubmit={handleSubmit}>
+            <input
+              value={editedTitle}
+              onChange={handleEditChange}
+              className="w-full px-3 py-2 mr-4 text-gray-500 rounded"
+            />
+          </form>
+        </div>
+        <div className="items-center">
+          <button
+            className="px-4 py-2 float-right"
+            onClick={() => setIsEditing(false)}
+          >
+            x
+          </button>
+          <button
+            onClick={handleSubmit}
+            className="px-4 py-2 float-right"
+            type="submit"
+          >
+            save
+          </button>
+        </div>
       </div>
-      <div className="items-center">
-        <button
-          className="px-4 py-2 float-right"
-          onClick={() => handleClick(id)}
-        >
-          x
-        </button>
+    );
+  } else {
+    return (
+      <div
+        key={id}
+        {...provided.draggableProps}
+        ref={provided.innerRef}
+        {...provided.dragHandleProps}
+        className={`${
+          snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+        } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600  border rounded`}
+      >
+        <div className="items-center">
+          <input
+            type={"checkbox"}
+            defaultChecked={false}
+            onChange={() => handleCompleteChange(id)}
+          />{" "}
+          <span className={completed ? "line-through" : undefined}>
+            {title}
+          </span>
+        </div>
+        <div className="items-center">
+          <button
+            className="px-4 py-2 float-right"
+            onClick={() => handleClick(id)}
+          >
+            x
+          </button>
+          <button
+            className="px-4 py-2 float-right"
+            onClick={() => setIsEditing(true)}
+          >
+            edit
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default React.memo(List);
