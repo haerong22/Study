f0570d855aa1ad1ad20c83090933b31cb3524faa
haerong22 <haerong22@naver.com>
2022-08-30 02:31:07 +0900

    #18 oop : grade calculator

---
+++ oop/oop-practice/oop-practice/src/main/java/org/example/grade/Course.java
@@ -0,0 +1,49 @@
+package org.example.grade;
+
+public class Course {
+
+    private final String subject; // 과목명
+    private final int credit; // 학점
+    private final String grade; // 성적
+
+    public Course(String subject, int credit, String grade) {
+        this.subject = subject;
+        this.credit = credit;
+        this.grade = grade;
+    }
+
+    public double multiplyCreditAndCourseGrade() {
+        return credit * getGradeToNumber();
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public double getGradeToNumber() {
+        double grade = 0;
+
+        switch (this.grade) {
+            case "A+":
+                grade = 4.5;
+                break;
+            case "A":
+                grade = 4.0;
+                break;
+            case "B+":
+                grade = 3.5;
+                break;
+            case "B":
+                grade = 3.0;
+                break;
+            case "C+":
+                grade = 2.5;
+                break;
+            case "C":
+                grade = 2.0;
+                break;
+        }
+        return grade;
+    }
+
+}
---
+++ oop/oop-practice/oop-practice/src/test/java/org/example/grade/CourseTest.java
@@ -0,0 +1,16 @@
+package org.example.grade;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class CourseTest {
+
+    @DisplayName("과목(코스)를 생성한다.")
+    @Test
+    void createTest() {
+        assertThatCode(() -> new Course("OOP", 3, "A+"))
+                .doesNotThrowAnyException();
+    }
+}
