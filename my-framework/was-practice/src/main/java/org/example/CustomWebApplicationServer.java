0934c0f2a771bf372e2cfe2e7014291932ff352b
haerong22 <haerong22@naver.com>
2022-08-31 02:59:18 +0900

    #19 was : custom web application server init

---
+++ my-framework/was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class CustomWebApplicationServer {
+
+    private final int port;
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
+
+    public CustomWebApplicationServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            logger.info("[CustomWebApplicationServer] started {} port.", port);
+
+            Socket clientSocket;
+            logger.info("[CustomWebApplicationServer] waiting for client.");
+
+            while ((clientSocket = serverSocket.accept()) != null) {
+                logger.info("[CustomWebApplicationServer] client connected!");
+            }
+        }
+    }
+}
