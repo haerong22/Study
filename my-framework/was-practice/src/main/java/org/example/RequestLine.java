d3e4dc074cdeab59dfb3abfa95a1c155320421ee
haerong22 <haerong22@naver.com>
2022-09-01 03:28:40 +0900

    #19 was : parsing inputStream

---
+++ my-framework/was-practice/src/main/java/org/example/RequestLine.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.util.Objects;
+
+public class RequestLine {
+    private final String method;
+    private final String urlPath;
+
+    private QueryStrings queryStrings;
+
+    public RequestLine(String method, String urlPath, String queryString) {
+        this.method = method;
+        this.urlPath = urlPath;
+        this.queryStrings = new QueryStrings(queryString);
+    }
+
+    public RequestLine(String requestLine) {
+        String[] tokens = requestLine.split(" ");
+        this.method = tokens[0];
+
+        String[] urlPathTokens = tokens[1].split("\\?");
+        this.urlPath = urlPathTokens[0];
+
+        if (urlPathTokens.length == 2) {
+            this.queryStrings = new QueryStrings(urlPathTokens[1]);
+        }
+    }
+
+    public boolean isGetRequest() {
+        return "GET".equals(this.method);
+    }
+
+    public boolean matchPath(String requestPath) {
+        return this.urlPath.equals(requestPath);
+    }
+
+    public QueryStrings getQueryStrings() {
+        return this.queryStrings;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RequestLine)) return false;
+        RequestLine that = (RequestLine) o;
+        return Objects.equals(method, that.method) && Objects.equals(urlPath, that.urlPath) && Objects.equals(queryStrings, that.queryStrings);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(method, urlPath, queryStrings);
+    }
+}
---
+++ my-framework/was-practice/src/test/java/org/example/QueryStringTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class QueryStringTest {
+
+    @Test
+    void createTest() {
+        QueryString queryString = new QueryString("operand1", "11");
+
+        Assertions.assertThat(queryString).isNotNull();
+    }
+}
