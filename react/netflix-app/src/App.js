198858f98e08734313db03d6dd79a8f1012646d4
haerong22 <haerong22@naver.com>
2022-09-10 19:22:37 +0900

    #20 react-netflix: movies row

--- react/netflix-app/src/App.js
+++ react/netflix-app/src/App.js
@@ -1,12 +1,33 @@
+import requests from "./api/requests";
 import "./App.css";
 import Banner from "./components/Banner";
 import Nav from "./components/Nav";
+import Row from "./components/Row";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <Nav />
       <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="TRANDING NOW" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="TOP RATED" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="ACTION MOVIES"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="COMEDY MOVIES"
+        id="=CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
     </div>
   );
 };
---
+++ react/netflix-app/src/components/Row.js
@@ -0,0 +1,47 @@
+import axios from "../api/axios";
+import React, { useEffect, useState } from "react";
+import "./Row.css";
+
+const Row = ({ title, id, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
+
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+
+    setMovies(request.data.results);
+  };
+
+  return (
+    <section className="row">
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className="arrow">{"<"}</span>
+        </div>
+
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`https://image.tmdb.org/t/p/original${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          ))}
+        </div>
+
+        <div className="slider__arrow-right">
+          <span className="arrow">{">"}</span>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Row;
