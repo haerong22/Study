53c5d34a3b6ef590e1c1877e26635ebf927b4645
haerong22 <haerong22@naver.com>
2022-09-19 03:46:34 +0900

    #21 jdbc: create test

---
+++ my-framework/jdbc-practice/src/main/java/org/example/User.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.Objects;
+
+public class User {
+    private final String userId;
+    private final String password;
+    private final String name;
+    private final String email;
+
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId) && Objects.equals(password, user.password) && Objects.equals(name, user.name) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, password, name, email);
+    }
+}
--- my-framework/jdbc-practice/src/test/java/org/example/UserDaoTest.java
+++ my-framework/jdbc-practice/src/test/java/org/example/UserDaoTest.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.sql.SQLException;
 
 public class UserDaoTest {
 
@@ -14,5 +18,15 @@ public class UserDaoTest {
         DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
     }
 
+    @Test
+    void createTest() throws SQLException {
+        UserDao userDao = new UserDao();
 
+        userDao.create(new User("bobby", "pass", "name", "email"));
+
+        User user = userDao.findByUserId("bobby");
+
+        Assertions.assertThat(user).isEqualTo(new User("bobby", "pass", "name", "email"));
+
+    }
 }
