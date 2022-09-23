89a253d36ed312971e18bfbb167ac58058b87b82
haerong22 <haerong22@naver.com>
2022-09-24 02:14:01 +0900

    #22 mvc: dispatcher servlet

---
+++ my-framework/mvc-practice/src/main/java/org/example/WebApplicationServer.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+public class WebApplicationServer {
+
+    private static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
+    public static void main(String[] args) throws LifecycleException {
+        String webappDirLocation = "webapp/";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        Context ctx = tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        log.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
+
+        File additionWebInfClasses = new File("build/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
+
+    }
+}
---
+++ my-framework/mvc-practice/src/main/java/org/example/mvc/DispatcherServlet.java
@@ -0,0 +1,32 @@
+package org.example.mvc;
+
+import org.example.mvc.controller.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class DispatcherServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+
+    private RequestMappingHandlerMapping requestMappingHandlerMapping;
+
+    @Override
+    public void init() throws ServletException {
+        requestMappingHandlerMapping = new RequestMappingHandlerMapping();
+        requestMappingHandlerMapping.init();
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("[DispatcherServlet] service started.");
+    }
+}
