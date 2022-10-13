b8d7190637ffb27eff3db84da9ee6734d43f4d64
haerong22 <haerong22@naver.com>
2022-10-14 03:18:16 +0900

    #24 simple sns: init

---
+++ src/test/java/com/example/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
