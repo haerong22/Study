a8efd26a780b10506260b631afb031fc1fa65ee5
haerong22 <haerong22@naver.com>
2022-11-22 01:29:33 +0900

    #24 simple sns: 시큐리티 설정 변경

--- simple_sns/src/main/java/com/example/sns/config/AuthenticationConfig.java
+++ simple_sns/src/main/java/com/example/sns/config/AuthenticationConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,11 +17,15 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
-
     private final UserService userService;
 
     @Value("${jwt.secret-key}")
     private String key;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().regexMatchers("^(?!/api/).*");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
