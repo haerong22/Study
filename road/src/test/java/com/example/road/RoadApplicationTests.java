6a1d2828b56f528ead917c06b8cfa2849ed92c09
haerong22 <haerong22@naver.com>
2022-12-09 01:45:22 +0900

    #26 pharmacy: init

---
+++ road/src/test/java/com/example/road/RoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.road;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
