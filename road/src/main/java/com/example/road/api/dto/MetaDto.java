b972aefc794ab530405ef41fd988ea6fa1b7a029
haerong22 <haerong22@naver.com>
2022-12-12 00:43:53 +0900

    #26 pharmacy: kakao api - local address search

---
+++ road/src/main/java/com/example/road/api/dto/MetaDto.java
@@ -0,0 +1,15 @@
+package com.example.road.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MetaDto {
+
+    @JsonProperty("total_count")
+    private Integer totalCount;
+}
---
+++ road/src/main/java/com/example/road/api/service/KakaoUriBuilderService.java
@@ -0,0 +1,25 @@
+package com.example.road.api.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+public class KakaoUriBuilderService {
+
+    private static final String KAKAO_LOCAL_SEARCH_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/address.json";
+
+    public URI builderUriByAddressSearch(String address) {
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(KAKAO_LOCAL_SEARCH_ADDRESS_URL);
+        uriBuilder.queryParam("query", address);
+
+        URI uri = uriBuilder.build().encode().toUri();
+        log.info("[KakaoUriBuilderService builderUriByAddressSearch] address: {}, uri: {}", address, uri);
+
+        return uri;
+    }
+}
