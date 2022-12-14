dcabb4c55af4b4d87dfe4b4a548eeee615cea98f
haerong22 <haerong22@naver.com>
2022-12-14 23:56:16 +0900

    #26 pharmacy: data setup

--- road/src/main/java/com/example/road/pharmacy/service/PharmacyRepositoryService.java
+++ road/src/main/java/com/example/road/pharmacy/service/PharmacyRepositoryService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
@@ -37,5 +38,10 @@ public class PharmacyRepositoryService {
         }
 
         entity.changePharmacyAddress(address);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Pharmacy> findAll() {
+        return pharmacyRepository.findAll();
     }
 }
