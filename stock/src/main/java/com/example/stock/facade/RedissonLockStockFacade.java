ab0a48f770f914006651f7023dcf411b70e39f05
haerong22 <haerong22@naver.com>
2022-12-21 00:50:51 +0900

    #27 stock: race condition solution 3 - redis(redisson)

---
+++ stock/src/main/java/com/example/stock/facade/RedissonLockStockFacade.java
@@ -0,0 +1,37 @@
+package com.example.stock.facade;
+
+import com.example.stock.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class RedissonLockStockFacade {
+
+    private final RedissonClient redissonClient;
+
+    private final StockService stockService;
+
+    public void decrease(Long id, Long quantity) {
+        RLock lock = redissonClient.getLock(id.toString());
+
+        try {
+            boolean available = lock.tryLock(10, 1, TimeUnit.SECONDS);
+
+            if (!available) {
+                System.out.println("get lock fail.");
+                return;
+            }
+
+            stockService.decrease(id, quantity);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
