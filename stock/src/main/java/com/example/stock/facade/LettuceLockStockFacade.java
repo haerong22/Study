148ceb5e3ff858727725ed88e5fb902cdb5bfab2
haerong22 <haerong22@naver.com>
2022-12-21 00:17:05 +0900

    #27 stock: race condition solution 3 - redis(lettuce)

---
+++ stock/src/main/java/com/example/stock/facade/LettuceLockStockFacade.java
@@ -0,0 +1,28 @@
+package com.example.stock.facade;
+
+import com.example.stock.repository.RedisLockRepository;
+import com.example.stock.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class LettuceLockStockFacade {
+
+    private final RedisLockRepository redisLockRepository;
+
+    private final StockService stockService;
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+        while (!redisLockRepository.lock(id)) {
+            Thread.sleep(100);
+        }
+
+        try {
+            stockService.decrease(id, quantity);
+        } finally {
+            redisLockRepository.unlock(id);
+        }
+    }
+}
