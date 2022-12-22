4840835d4e8d42994fddb5ad2f70e15762b89e40
haerong22 <haerong22@naver.com>
2022-12-23 01:13:57 +0900

    #29 pass: init

---
+++ pass-batch/src/test/java/com/example/passbatch/PassBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.passbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
