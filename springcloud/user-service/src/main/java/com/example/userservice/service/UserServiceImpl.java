4e4631e359269b6bb4990ba32b51fa405d057f2c
haerong22 <haerong22@naver.com>
2022-12-28 21:39:18 +0900

    #25 user-service: sleuth + zipkin

--- springcloud/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ springcloud/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -74,11 +74,13 @@ public class UserServiceImpl implements UserService {
         /**
          * circuit breaker
          */
+        log.info("Before call orders microservice");
         CircuitBreaker circuitbreaker = circuitBreakerFactory.create("circuitbreaker");
         List<ResponseOrder> orderList = circuitbreaker.run(
                 () -> orderServiceClient.getOrders(userId),
                 throwable -> new ArrayList<>()
         );
+        log.info("After call orders microservice");
 
         userDto.setOrders(orderList);
         return userDto;
