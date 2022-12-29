dc23c3405bf75b91b3a93b8dbcef37988e802731
haerong22 <haerong22@naver.com>
2022-12-30 00:36:49 +0900

    #29 pass: notification entity

---
+++ pass-batch/src/main/java/com/example/passbatch/repository/notification/NotificationEvent.java
@@ -0,0 +1,5 @@
+package com.example.passbatch.repository.notification;
+
+public enum NotificationEvent {
+    BEFORE_CLASS
+}
---
+++ pass-batch/src/main/java/com/example/passbatch/repository/notification/NotificationRepository.java
@@ -0,0 +1,6 @@
+package com.example.passbatch.repository.notification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<NotificationEntity, Integer> {
+}
