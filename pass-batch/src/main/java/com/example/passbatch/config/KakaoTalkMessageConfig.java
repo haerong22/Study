a445ff6bbe128acef8ce2f028b94ceeaac8ace6d
haerong22 <haerong22@naver.com>
2022-12-30 00:49:37 +0900

    #29 pass: send notification before class job

---
+++ pass-batch/src/main/java/com/example/passbatch/config/KakaoTalkMessageConfig.java
@@ -0,0 +1,16 @@
+package com.example.passbatch.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "kakaotalk")
+public class KakaoTalkMessageConfig {
+    private String host;
+    private String token;
+}
---
+++ pass-batch/src/main/java/com/example/passbatch/job/notification/SendNotificationBeforeClassJobConfig.java
@@ -0,0 +1,103 @@
+package com.example.passbatch.job.notification;
+
+import com.example.passbatch.repository.booking.BookingEntity;
+import com.example.passbatch.repository.notification.NotificationEntity;
+import com.example.passbatch.repository.notification.NotificationEvent;
+import com.example.passbatch.repository.notification.NotificationModelMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.batch.item.support.SynchronizedItemStreamReader;
+import org.springframework.batch.item.support.builder.SynchronizedItemStreamReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class SendNotificationBeforeClassJobConfig {
+
+    private final int CHUNK_SIZE = 10;
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+    private final SendNotificationItemWriter sendNotificationItemWriter;
+
+    @Bean
+    public Job sendNotificationBeforeClassJob() {
+        return this.jobBuilderFactory.get("sendNotificationBeforeClassJob")
+                .start(addNotificationStep())
+                .next(sendNotificationStep())
+                .build();
+    }
+
+    @Bean
+    public Step addNotificationStep() {
+        return this.stepBuilderFactory.get("addNotificationStep")
+                .<BookingEntity, NotificationEntity>chunk(CHUNK_SIZE)
+                .reader(addNotificationItemReader())
+                .processor(addNotificationItemProcessor())
+                .writer(addNotificationItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JpaPagingItemReader<BookingEntity> addNotificationItemReader() {
+        return new JpaPagingItemReaderBuilder<BookingEntity>()
+                .name("addNotificationItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(CHUNK_SIZE)
+                .queryString("select b from BookingEntity b join fetch b.userEntity where b.status = :status and b.startedAt <= :startedAt order by b.bookingSeq")
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<BookingEntity, NotificationEntity> addNotificationItemProcessor() {
+        return bookingEntity ->
+                NotificationModelMapper.INSTANCE.toNotificationEntity(bookingEntity, NotificationEvent.BEFORE_CLASS);
+    }
+
+    @Bean
+    public JpaItemWriter<NotificationEntity> addNotificationItemWriter() {
+        return new JpaItemWriterBuilder<NotificationEntity>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
+    }
+
+    @Bean
+    public Step sendNotificationStep() {
+        return this.stepBuilderFactory.get("sendNotificationStep")
+                .<NotificationEntity, NotificationEntity>chunk(CHUNK_SIZE)
+                .reader(sendNotificationItemReader())
+                .writer(sendNotificationItemWriter)
+                .taskExecutor(new SimpleAsyncTaskExecutor())
+                .build();
+    }
+
+    @Bean
+    public SynchronizedItemStreamReader<NotificationEntity> sendNotificationItemReader() {
+        JpaCursorItemReader<NotificationEntity> itemReader = new JpaCursorItemReaderBuilder<NotificationEntity>()
+                .name("sendNotificationItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .queryString("select n from NotificationEntity n where n.event = :event and n.sent = :sent")
+                .parameterValues(Map.of("event", NotificationEvent.BEFORE_CLASS, "sent", false))
+                .build();
+
+        return new SynchronizedItemStreamReaderBuilder<NotificationEntity>()
+                .delegate(itemReader)
+                .build();
+    }
+}
