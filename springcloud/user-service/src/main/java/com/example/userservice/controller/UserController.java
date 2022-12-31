490427b42fb0a595b7dba8a1fd8e4e09c98a8672
haerong22 <haerong22@naver.com>
2022-12-31 18:49:51 +0900

    #25 spring cloud: prometheus + grafana

--- springcloud/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ springcloud/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
 import com.example.userservice.vo.RequestUser;
 import com.example.userservice.vo.ResponseUser;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -32,6 +33,7 @@ public class UserController {
 
 
     @GetMapping("/health_check")
+    @Timed(value = "users.status", longTask = true)
     public String status() {
         return "It's working in User Service"
                 + ", port(local.server.port)=" + env.getProperty("local.server.port")
@@ -41,6 +43,7 @@ public class UserController {
     }
 
     @GetMapping("/welcome")
+    @Timed(value = "users.welcome", longTask = true)
     public String welcome() {
         return greeting.getMessage() ;
     }
