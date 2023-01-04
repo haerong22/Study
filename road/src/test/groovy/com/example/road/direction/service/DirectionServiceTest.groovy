a04491b14872617a6ae21112f68749cf37bab986
haerong22 <haerong22@naver.com>
2023-01-05 06:53:02 +0900

    #26 pharmacy: build project

--- road/src/test/groovy/com/example/road/direction/service/DirectionServiceTest.groovy
+++ road/src/test/groovy/com/example/road/direction/service/DirectionServiceTest.groovy
@@ -1,6 +1,8 @@
 package com.example.road.direction.service
 
 import com.example.road.api.dto.DocumentDto
+import com.example.road.api.service.KakaoCategorySearchService
+import com.example.road.direction.repository.DirectionRepository
 import com.example.road.pharmacy.dto.PharmacyDto
 import com.example.road.pharmacy.service.PharmacySearchService
 import spock.lang.Specification
@@ -8,8 +10,13 @@ import spock.lang.Specification
 class DirectionServiceTest extends Specification {
 
     private PharmacySearchService pharmacySearchService = Mock();
+    private DirectionRepository directionRepository = Mock();
+    private KakaoCategorySearchService kakaoCategorySearchService = Mock();
+    private Base62Service base62Service = Mock();
 
-    private DirectionService directionService = new DirectionService(pharmacySearchService);
+    private DirectionService directionService = new DirectionService(
+            pharmacySearchService, directionRepository, kakaoCategorySearchService, base62Service
+    );
 
     private List<PharmacyDto> pharmacyList;
 
