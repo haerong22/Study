4992bcfe1be3ab645ed14a426d3438f4084a4bda
haerong22 <haerong22@naver.com>
2023-01-09 00:49:23 +0900

    #31 loan: init

---
+++ loan/src/test/java/com/example/loan/LoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
