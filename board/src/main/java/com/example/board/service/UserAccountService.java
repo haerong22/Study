5899dba9a4ac47c4bb7443179d8702e8e239ad23
haerong22 <haerong22@naver.com>
2023-01-26 01:10:06 +0900

    #16 board: user account service

---
+++ board/src/main/java/com/example/board/service/UserAccountService.java
@@ -0,0 +1,29 @@
+package com.example.board.service;
+
+import com.example.board.domain.UserAccount;
+import com.example.board.dto.UserAccountDto;
+import com.example.board.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserAccountService {
+
+    private final UserAccountRepository userAccountRepository;
+
+    @Transactional(readOnly = true)
+    public Optional<UserAccountDto> searchUser(String username) {
+        return userAccountRepository.findById(username).map(UserAccountDto::from);
+    }
+
+    public UserAccountDto saveUser(String username, String password, String email, String nickname, String memo) {
+        return UserAccountDto.from(
+                userAccountRepository.save(UserAccount.of(username, password, email, nickname, memo, username))
+        );
+    }
+}
