74865252c27471a43cec2d809ea5b4da66c3dd1a
haerong22 <haerong22@naver.com>
2023-01-27 01:40:05 +0900

    #16 board: swagger test

--- board/src/main/java/com/example/board/controller/MainController.java
+++ board/src/main/java/com/example/board/controller/MainController.java
@@ -1,13 +1,42 @@
 package com.example.board.controller;
 
+import com.example.board.dto.response.ArticleCommentResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ *     메인 컨트롤러
+ * </p>
+ * 테스트 중
+ */
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public String root() {
         return "forward:/articles";
+    }
+
+    /**
+     * 댓글 정보를 열람한다.
+     *
+     * @param id 댓글 ID
+     * @return 댓글 응답
+     */
+    @ResponseBody
+    @GetMapping("/test-rest")
+    public ArticleCommentResponse test(Long id) {
+        return ArticleCommentResponse.of(
+                id,
+                "content",
+                LocalDateTime.now(),
+                "email@email.com",
+                "bobby",
+                "bobby"
+        );
     }
 }
