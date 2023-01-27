0e9ca9cbd9e536d13eae0d8454f15aa4cf9b44f4
haerong22 <haerong22@naver.com>
2023-01-28 01:15:23 +0900

    #32 multi module: init

---
+++ multimodule/module-api/src/test/java/com/example/moduleapi/ModuleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moduleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ multimodule/module-common/src/test/java/com/example/modulecommon/ModuleCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modulecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
