698623f2a4520504be78b917cd7f2d0ff6dbf541
haerong22 <haerong22@naver.com>
2023-01-30 01:47:25 +0900

    #30 jpa basic: MappedSuperClass

---
+++ hello-jpa/src/main/java/com/hello/jpa/inheritance/ex01/Member.java
@@ -0,0 +1,42 @@
+package com.hello.jpa.inheritance.ex01;
+
+import javax.persistence.*;
+
+@Entity
+public class Member extends BaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
---
+++ hello-jpa/src/main/java/com/hello/jpa/inheritance/ex01/RoleType.java
@@ -0,0 +1,6 @@
+package com.hello.jpa.inheritance.ex01;
+
+public enum RoleType {
+
+    USER, ADMIN
+}
