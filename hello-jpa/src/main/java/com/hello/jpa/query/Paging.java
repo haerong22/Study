c4650ba936e0915368a8e3f01daa271d2e3baabf
haerong22 <haerong22@naver.com>
2023-02-04 01:05:02 +0900

    #30 jpa basic: jpql - paging

---
+++ hello-jpa/src/main/java/com/hello/jpa/query/Paging.java
@@ -0,0 +1,50 @@
+ package com.hello.jpa.query;
+
+ import com.hello.jpa.query.entity.Member;
+
+ import javax.persistence.EntityManager;
+ import javax.persistence.EntityManagerFactory;
+ import javax.persistence.EntityTransaction;
+ import javax.persistence.Persistence;
+ import java.util.List;
+
+public class Paging {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setName("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
+
+            em.flush();
+            em.clear();
+
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            System.out.println("result.size() = " + result.size());
+
+            result.forEach(System.out::println);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
