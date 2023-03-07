eacde8152194bc71a71b9c5df81a1f371c81a925
haerong22 <haerong22@naver.com>
2023-03-08 01:28:44 +0900

    #35 springboot: boot init

---
+++ springboot/boot/src/main/java/hello/boot/HelloController.java
@@ -0,0 +1,14 @@
+package hello.boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-spring")
+    public String hello() {
+        System.out.println("HelloController.hello");
+        return "hello spring!";
+    }
+}
