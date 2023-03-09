0713974837f1f489cf225fa51ec25e3491d376ca
haerong22 <haerong22@naver.com>
2023-03-10 00:51:29 +0900

    #35 springboot: auto config - init

---
+++ springboot/autoconfig/src/main/java/hello/AutoConfigApplication.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoConfigApplication.class, args);
+    }
+
+}
