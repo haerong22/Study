851ba5c12ba4f06ccb27eb6eab02c9e0d4207bdb
haerong22 <haerong22@naver.com>
2023-03-14 03:36:10 +0900

    #35 springboot: use custom library(auto configuration)

---
+++ springboot/memory/project-v2/src/main/java/hello/ProjectV2Application.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectV2Application.class, args);
+    }
+
+}
