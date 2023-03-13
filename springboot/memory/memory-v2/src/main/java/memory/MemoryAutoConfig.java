8e3e541013c86c929367d1635ed33e63ef662150
haerong22 <haerong22@naver.com>
2023-03-14 03:34:58 +0900

    #35 springboot: custom library(auto configuration)

---
+++ springboot/memory/memory-v2/src/main/java/memory/MemoryAutoConfig.java
@@ -0,0 +1,20 @@
+package memory;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+@ConditionalOnProperty(name = "memory", havingValue = "on")
+public class MemoryAutoConfig {
+
+    @Bean
+    public MemoryController memoryController() {
+        return new MemoryController(memoryFinder());
+    }
+
+    @Bean
+    public MemoryFinder memoryFinder() {
+        return new MemoryFinder();
+    }
+}
---
+++ springboot/memory/memory-v2/src/main/java/memory/MemoryFinder.java
@@ -0,0 +1,21 @@
+package memory;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MemoryFinder {
+
+    public Memory get() {
+        long max = Runtime.getRuntime().maxMemory();
+        long total = Runtime.getRuntime().totalMemory();
+        long free = Runtime.getRuntime().freeMemory();
+        long used = total - free;
+        return new Memory(used, max);
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("init memoryFinder");
+    }
+}
