aba45ed1ceb8315093dff3890bfdcf7300fc25d2
haerong22 <haerong22@naver.com>
2023-03-14 03:12:17 +0900

    #35 springboot: use custom library

---
+++ springboot/memory/project-v1/src/main/java/hello/ProjectV1Application.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectV1Application.class, args);
+    }
+
+}
---
+++ springboot/memory/project-v1/src/main/java/hello/config/MemoryConfig.java
@@ -0,0 +1,20 @@
+package hello.config;
+
+import memory.MemoryController;
+import memory.MemoryFinder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MemoryConfig {
+
+    @Bean
+    public MemoryFinder memoryFinder() {
+        return new MemoryFinder();
+    }
+
+    @Bean
+    public MemoryController memoryController() {
+        return new MemoryController(memoryFinder());
+    }
+}
