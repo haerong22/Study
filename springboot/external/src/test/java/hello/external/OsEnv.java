0a4198ba8f356b67b03d7095644e4c8ee844bdd7
haerong22 <haerong22@naver.com>
2023-03-15 03:48:07 +0900

    #35 springboot: environment variables - os

---
+++ springboot/external/src/test/java/hello/external/OsEnv.java
@@ -0,0 +1,14 @@
+package hello.external;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+@Slf4j
+public class OsEnv {
+
+    public static void main(String[] args) {
+        Map<String, String> envMap = System.getenv();
+        envMap.forEach((k, v) -> log.info("env {}={}", k, v));
+    }
+}
