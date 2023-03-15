023d4ce0d2fd2a1cce46623fe08f84fa582dcb92
haerong22 <haerong22@naver.com>
2023-03-16 01:10:26 +0900

    #35 springboot: application properties

--- springboot/external/src/main/java/hello/EnvironmentCheck.java
+++ springboot/external/src/main/java/hello/EnvironmentCheck.java
@@ -16,10 +16,12 @@ public class EnvironmentCheck {
     @PostConstruct
     public void init() {
         String url = env.getProperty("url");
-        String username = env.getProperty("username");
+        String name = env.getProperty("name");
         String password = env.getProperty("password");
+        String testKey = env.getProperty("testKey");
         log.info("env url={}", url);
-        log.info("env username={}", username);
+        log.info("env name={}", name);
         log.info("env password={}", password);
+        log.info("env testKey={}", testKey);
     }
 }
