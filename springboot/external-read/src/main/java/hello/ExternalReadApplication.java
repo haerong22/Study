031a38085c8d2632a34847686730bf5a69ff631f
haerong22 <haerong22@naver.com>
2023-03-20 00:17:31 +0900

    #35 springboot: external read - init

---
+++ springboot/external-read/src/main/java/hello/ExternalReadApplication.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalReadApplication.class, args);
+    }
+
+}
