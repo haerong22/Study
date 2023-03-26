701c000c43fcdf72f2d4a9b9522ba63f29ec050a
haerong22 <haerong22@naver.com>
2023-03-27 00:59:44 +0900

    #31 loan: create judgement domain

---
+++ loan/src/main/java/com/example/loan/domain/Judgement.java
@@ -0,0 +1,33 @@
+package com.example.loan.domain;
+
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@Where(clause = "is_deleted=false")
+public class Judgement extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long judgementId;
+
+    @Column(columnDefinition = "bigint NOT NULL COMMENT '신청 ID'")
+    private Long applicationId;
+
+    @Column(columnDefinition = "varchar(12) NOT NULL COMMENT '심사자'")
+    private String name;
+
+    @Column(columnDefinition = "decimal(15,2) NOT NULL COMMENT '승인 금액'")
+    private BigDecimal approvalAmount;
+}
