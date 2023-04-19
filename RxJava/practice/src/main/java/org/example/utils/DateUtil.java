eeb0f86c6b19d4e1e7260d55217a603adf7117c4
haerong22 <haerong22@naver.com>
2023-04-20 00:23:02 +0900

    #36 rxjava: utils

---
+++ RxJava/practice/src/main/java/org/example/utils/DateUtil.java
@@ -0,0 +1,11 @@
+package org.example.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DateUtil {
+    public static String getNowDate(){
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                .format(Calendar.getInstance().getTime());
+    }
+}
