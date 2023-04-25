d866cc3d1b44d4d44db58c52fcc06b034c9736d8
haerong22 <haerong22@naver.com>
2023-04-26 01:32:41 +0900

    #36 rxjava: functional descriptor

---
+++ RxJava/practice/src/main/java/org/example/ex06/FunctionalDescriptorFunction.java
@@ -0,0 +1,26 @@
+package org.example.ex06;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * 함수 디스크립터의 Function 예제
+ */
+public class FunctionalDescriptorFunction {
+    public static void main(String[] args) {
+        List<Character> characterList = Arrays.asList('a', 'b', 'c', 'd', 'e');
+        List<Integer> asciiNumbers = map(characterList, character -> (int) character);
+
+        for(int asciiNumber : asciiNumbers)
+            System.out.println(asciiNumber);
+    }
+
+    public static <T, R> List<R> map(List<T> list, Function<T, R> f){
+        List<R> result = new ArrayList<>();
+        for(T t : list)
+            result.add(f.apply(t));
+        return result;
+    }
+}
