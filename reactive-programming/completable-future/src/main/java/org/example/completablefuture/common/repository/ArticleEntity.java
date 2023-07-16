ac03b1be8f282240174981858cce2645b61aad7c
haerong22 <haerong22@naver.com>
2023-07-16 18:49:13 +0900

    #39 reactive programming: completable future - common

---
+++ reactive-programming/completable-future/src/main/java/org/example/completablefuture/common/repository/ArticleEntity.java
@@ -0,0 +1,11 @@
+package org.example.completablefuture.common.repository;
+
+import lombok.Data;
+
+@Data
+public class ArticleEntity {
+    private final String id;
+    private final String title;
+    private final String content;
+    private final String userId;
+}
---
+++ reactive-programming/completable-future/src/main/java/org/example/completablefuture/common/repository/UserEntity.java
@@ -0,0 +1,11 @@
+package org.example.completablefuture.common.repository;
+
+import lombok.Data;
+
+@Data
+public class UserEntity {
+    private final String id;
+    private final String name;
+    private final int age;
+    private final String profileImageId;
+}
