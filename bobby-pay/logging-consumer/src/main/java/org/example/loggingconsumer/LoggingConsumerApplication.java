fb2eab1bd040b20d7ec2a007aaea53c8e09e26b8
haerong22 <haerong22@naver.com>
2023-08-03 20:54:46 +0900

    #38 hexagonal: common - logging consumer(kafka)

---
+++ bobby-pay/logging-consumer/src/main/java/org/example/loggingconsumer/LoggingConsumerApplication.java
@@ -0,0 +1,12 @@
+package org.example.loggingconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingConsumerApplication.class, args);
+    }
+}
