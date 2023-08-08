11cc59f4fb5b8fcbd39849fcb9ed1cf1a5b281f8
haerong22 <haerong22@naver.com>
2023-08-08 19:47:48 +0900

    #38 hexagonal: remittance service - domain

---
+++ bobby-pay/remittance-service/src/main/java/org/example/remittance/adapter/out/persistence/SpringDataRemittanceRequestRepository.java
@@ -0,0 +1,6 @@
+package org.example.remittance.adapter.out.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpringDataRemittanceRequestRepository extends JpaRepository<RemittanceRequestJpaEntity, Long> {
+}
