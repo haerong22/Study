47d810b00af6d6ddacee7b233f2be0915ab4964b
haerong22 <haerong22@naver.com>
2023-11-07 13:08:13 +0900

    #40 feat: fail event

--- coupon-system/api/src/main/java/com/example/api/service/ApplyService.java
+++ coupon-system/api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,7 +1,7 @@
 package com.example.api.service;
 
-import com.example.api.domain.Coupon;
 import com.example.api.producer.CouponCreateProducer;
+import com.example.api.repository.AppliedUserRepository;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -12,14 +12,22 @@ public class ApplyService {
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
     private final CouponCreateProducer couponCreateProducer;
+    private final AppliedUserRepository appliedUserRepository;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer, AppliedUserRepository appliedUserRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(Long userId) {
+        Long apply = appliedUserRepository.add(userId);
+
+        if (apply != 1) {
+            return;
+        }
+
         long count = couponCountRepository.increment();
 
         if (count > 100) {
--- coupon-system/consumer/src/main/java/com/example/consumer/consumer/CouponCreatedConsumer.java
+++ coupon-system/consumer/src/main/java/com/example/consumer/consumer/CouponCreatedConsumer.java
@@ -1,7 +1,11 @@
 package com.example.consumer.consumer;
 
 import com.example.consumer.domain.Coupon;
+import com.example.consumer.domain.FailedEvent;
 import com.example.consumer.repository.CouponRepository;
+import com.example.consumer.repository.FailedEventRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -9,13 +13,22 @@ import org.springframework.stereotype.Component;
 public class CouponCreatedConsumer {
 
     private final CouponRepository couponRepository;
+    private final FailedEventRepository failedEventRepository;
+    private final Logger log = LoggerFactory.getLogger(CouponCreatedConsumer.class);
 
-    public CouponCreatedConsumer(CouponRepository couponRepository) {
+    public CouponCreatedConsumer(CouponRepository couponRepository, FailedEventRepository failedEventRepository) {
         this.couponRepository = couponRepository;
+        this.failedEventRepository = failedEventRepository;
     }
 
     @KafkaListener(topics = "coupon_create", groupId = "group_1")
     public void listener(Long userId) {
-        couponRepository.save(new Coupon(userId));
+        try {
+            couponRepository.save(new Coupon(userId));
+        } catch (Exception e) {
+            log.error("failed to create coupon:: {}", userId);
+            failedEventRepository.save(new FailedEvent(userId));
+        }
+
     }
 }
