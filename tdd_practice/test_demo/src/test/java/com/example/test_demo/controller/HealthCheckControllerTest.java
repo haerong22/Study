e1c8de7b392dd8e2f635a6d1d65435cd122382bb
haerong22 <haerong22@naver.com>
2023-11-13 20:15:52 +0900

    #41 test code: health check controller test

---
+++ tdd_practice/test_demo/src/test/java/com/example/test_demo/controller/HealthCheckControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.test_demo.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+class HealthCheckControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("health check 응답 200")
+    void health_check_success() throws Exception {
+        mockMvc
+                .perform(get("/health_check.html"))
+                .andExpect(status().isOk());
+    }
+}
