e48d8470511c7fa4d4352fb7d3f36916f68d1ab9
haerong22 <haerong22@naver.com>
2023-11-17 18:19:13 +0900

    #42 cafe kiosk: init

---
+++ tdd_practice/cafekiosk/src/test/java/com/example/cafekiosk/CafekioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafekioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
