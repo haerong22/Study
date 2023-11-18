723ff64036facb09c4bfe6fd7e9682b5700bc536
haerong22 <haerong22@naver.com>
2023-11-18 16:28:53 +0900

    #42 cafe kiosk: persistence layer test

--- tdd_practice/cafekiosk/src/main/java/com/example/cafekiosk/spring/domain/product/Product.java
+++ tdd_practice/cafekiosk/src/main/java/com/example/cafekiosk/spring/domain/product/Product.java
@@ -2,6 +2,7 @@ package com.example.cafekiosk.spring.domain.product;
 
 import com.example.cafekiosk.spring.domain.BaseEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -27,4 +28,12 @@ public class Product extends BaseEntity {
 
     private int price;
 
+    @Builder
+    private Product(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
 }
---
+++ tdd_practice/cafekiosk/src/test/java/com/example/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.example.cafekiosk.spring.domain.product;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static com.example.cafekiosk.spring.domain.product.ProductSellingStatus.*;
+import static com.example.cafekiosk.spring.domain.product.ProductType.BAKERY;
+import static com.example.cafekiosk.spring.domain.product.ProductType.HANDMADE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+@ActiveProfiles("test")
+//@SpringBootTest
+@DataJpaTest
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    @DisplayName("원하는 판매상태를 가진 상품들을 조회한다")
+    void findAllBySellingStatusIn() {
+        // given
+        Product product1 = Product.builder()
+                .productNumber("001")
+                .type(HANDMADE)
+                .sellingStatus(SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+
+        Product product2 = Product.builder()
+                .productNumber("002")
+                .type(HANDMADE)
+                .sellingStatus(HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .build();
+
+        Product product3 = Product.builder()
+                .productNumber("003")
+                .type(BAKERY)
+                .sellingStatus(STOP_SELLING)
+                .name("크루아상")
+                .price(3500)
+                .build();
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        List<Product> result = productRepository.findAllBySellingStatusIn(List.of(SELLING, HOLD));
+
+        // then
+        assertThat(result).hasSize(2)
+                .extracting("productNumber", "name", "sellingStatus")
+                .containsExactlyInAnyOrder(
+                        tuple("001", "아메리카노", SELLING),
+                        tuple("002", "카페라떼", HOLD)
+                );
+
+    }
+}
