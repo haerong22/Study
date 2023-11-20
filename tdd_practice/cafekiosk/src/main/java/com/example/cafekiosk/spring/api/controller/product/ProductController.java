451609c741078607ffd745ae7ffef3e16e9df59d
haerong22 <haerong22@naver.com>
2023-11-20 18:07:41 +0900

    #42 cafe kiosk: 공통 응답 및 유효성 검사 추가

--- tdd_practice/cafekiosk/src/main/java/com/example/cafekiosk/spring/api/controller/product/ProductController.java
+++ tdd_practice/cafekiosk/src/main/java/com/example/cafekiosk/spring/api/controller/product/ProductController.java
@@ -1,13 +1,16 @@
 package com.example.cafekiosk.spring.api.controller.product;
 
+import com.example.cafekiosk.spring.api.ApiResponse;
 import com.example.cafekiosk.spring.api.controller.product.request.ProductCreateRequest;
 import com.example.cafekiosk.spring.api.service.product.ProductService;
 import com.example.cafekiosk.spring.api.service.product.response.ProductResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -17,13 +20,13 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/api/v1/products/new")
-    public void createProduct(ProductCreateRequest request) {
-        productService.createProduct(request);
+    public ApiResponse<ProductResponse> createProduct(@Valid @RequestBody ProductCreateRequest request) {
+        return ApiResponse.ok(productService.createProduct(request));
     }
 
     @GetMapping("/api/v1/products/selling")
-    public List<ProductResponse> getSellingProducts() {
-        return productService.getSellingProducts();
+    public ApiResponse<List<ProductResponse>> getSellingProducts() {
+        return ApiResponse.ok(productService.getSellingProducts());
     }
 
 }
--- tdd_practice/cafekiosk/src/main/java/com/example/cafekiosk/spring/api/controller/product/request/ProductCreateRequest.java
+++ tdd_practice/cafekiosk/src/main/java/com/example/cafekiosk/spring/api/controller/product/request/ProductCreateRequest.java
@@ -5,13 +5,26 @@ import com.example.cafekiosk.spring.domain.product.ProductSellingStatus;
 import com.example.cafekiosk.spring.domain.product.ProductType;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Getter
+@NoArgsConstructor
 public class ProductCreateRequest {
 
+    @NotNull(message = "상품 타입은 필수입니다.")
     private ProductType type;
+
+    @NotNull(message = "상품 판매상태는 필수입니다.")
     private ProductSellingStatus sellingStatus;
+
+    @NotBlank(message = "상품 이름은 필수입니다.")
     private String name;
+
+    @Positive(message = "상품 가격은 양수여야 합니다.")
     private int price;
 
     @Builder
