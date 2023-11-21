267594833d7a674f05d91f8b4b4c7307c36e8bb8
haerong22 <haerong22@naver.com>
2023-11-21 20:17:42 +0900

    #42 cafe kiosk: guava 학습 테스트

---
+++ tdd_practice/cafekiosk/src/test/java/com/example/cafekiosk/learning/GuavaLearningTest.java
@@ -0,0 +1,101 @@
+package com.example.cafekiosk.learning;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+public class GuavaLearningTest {
+
+    @Test
+    @DisplayName("주어진 개수만큼 List를 파티셔닝 한다.")
+    void partition() {
+        // given
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6);
+
+        // when
+        List<List<Integer>> partition = Lists.partition(integers, 3);
+
+        // then
+        assertThat(partition).hasSize(2)
+                .isEqualTo(List.of(
+                        List.of(1, 2, 3), List.of(4, 5, 6)
+                ));
+    }
+
+    @Test
+    @DisplayName("주어진 개수만큼 List를 파티셔닝 한다.")
+    void partition2() {
+        // given
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6);
+
+        // when
+        List<List<Integer>> partition = Lists.partition(integers, 4);
+
+        // then
+        assertThat(partition).hasSize(2)
+                .isEqualTo(List.of(
+                        List.of(1, 2, 3, 4), List.of(5, 6)
+                ));
+    }
+
+    @Test
+    @DisplayName("멀티맵 기능 확인")
+    void multimap() {
+        // given
+        Multimap<String, String> multimap = ArrayListMultimap.create();
+        multimap.put("커피", "아메리카노");
+        multimap.put("커피", "카페라떼");
+        multimap.put("커피", "카푸치노");
+        multimap.put("베이커리", "크루아상");
+        multimap.put("베이커리", "식빵");
+
+        // when
+        Collection<String> strings = multimap.get("커피");
+
+        // then
+        assertThat(strings).hasSize(3)
+                .isEqualTo(List.of("아메리카노", "카페라떼", "카푸치노"));
+    }
+
+    @TestFactory
+    @DisplayName("멀티맵 기능 확인")
+    Collection<DynamicTest> multimap2() {
+        // given
+        Multimap<String, String> multimap = ArrayListMultimap.create();
+        multimap.put("커피", "아메리카노");
+        multimap.put("커피", "카페라떼");
+        multimap.put("커피", "카푸치노");
+        multimap.put("베이커리", "크루아상");
+        multimap.put("베이커리", "식빵");
+
+        return List.of(
+                dynamicTest("1개 value 삭제", () -> {
+                    // when
+                    multimap.remove("커피", "카푸치노");
+
+                    // then
+                    Collection<String> strings = multimap.get("커피");
+                    assertThat(strings).hasSize(2)
+                            .isEqualTo(List.of("아메리카노", "카페라떼"));
+                }),
+                dynamicTest("전체 삭제", () -> {
+                    // when
+                    multimap.removeAll("커피");
+
+                    // then
+                    Collection<String> strings = multimap.get("커피");
+                    assertThat(strings).isEmpty();
+                })
+        );
+    }
+}
