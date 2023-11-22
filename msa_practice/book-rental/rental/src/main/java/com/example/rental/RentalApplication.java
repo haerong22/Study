d039ba9d418fcefcbb0a6fb9bfa7634dab4dc267
haerong22 <haerong22@naver.com>
2023-11-22 17:15:31 +0900

    #43 book rental: rental - init

---
+++ msa_practice/book-rental/rental/src/main/java/com/example/rental/RentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalApplication.class, args);
+    }
+
+}
---
+++ msa_practice/book-rental/rental/src/test/java/com/example/rental/RentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
