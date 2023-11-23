7711df4bc7aa6fd89cd8b5d7758591591ba0be3c
haerong22 <haerong22@naver.com>
2023-11-23 17:59:52 +0900

    #43 book rental: RentalCard 로직 및 테스트

--- msa_practice/book-rental/rental/src/main/java/com/example/rental/domain/model/RentalCard.java
+++ msa_practice/book-rental/rental/src/main/java/com/example/rental/domain/model/RentalCard.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,19 +31,92 @@ public class RentalCard {
         this.returnItems = returnItems;
     }
 
-    public void addRentalItem(RentalItem rentalItem) {
+    public static RentalCard createRentalCard(IDName creator) {
+        return RentalCard.builder()
+                .rentalCardNo(RentalCardNo.createRentalCardNo())
+                .member(creator)
+                .rentStatus(RentStatus.RENT_AVAILABLE)
+                .lateFee(LateFee.createLateFee())
+                .rentalItems(new ArrayList<>())
+                .returnItems(new ArrayList<>())
+                .build();
+    }
+
+    public RentalCard rentItem(Item item, LocalDate rentDate) {
+        checkRentalAvailable();
+        this.addRentalItem(RentalItem.createRentalItem(item, rentDate));
+        return this;
+    }
+
+    public RentalCard returnItem(Item item, LocalDate returnDate) {
+        RentalItem rentalItem = this.rentalItems.stream()
+                .filter(i -> i.getItem().equals(item))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("반납할 도서가 없습니다."));
+
+        calculateLateFee(rentalItem, returnDate);
+
+        this.addReturnItem(ReturnItem.createReturnItem(rentalItem, returnDate));
+        this.removeRentalItem(rentalItem);
+
+        return this;
+    }
+
+    public RentalCard overdueItem(Item item) {
+        RentalItem rentalItem = this.rentalItems.stream()
+                .filter(i -> i.getItem().equals(item))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 도서가 없습니다."));
+
+        rentalItem.overdue();
+        this.rentStatus = RentStatus.RENT_UNAVAILABLE;
+
+        return this;
+    }
+
+    public void makeAvailableRental(long point) {
+        if (this.rentalItems.size() != 0) {
+            throw new IllegalArgumentException("모든 도서가 반납되어야 정지를 해제할 수 있습니다.");
+        }
+
+        if (this.getLateFee().getPoint() != point) {
+            throw new IllegalArgumentException("해당 포인트로 연체를 해제할 수 없습니다.");
+        }
+
+        this.lateFee = this.getLateFee().removePoint(point);
+        this.rentStatus = RentStatus.RENT_AVAILABLE;
+    }
+
+    private void calculateLateFee(RentalItem rentalItem, LocalDate returnDate) {
+        if (returnDate.compareTo(rentalItem.getOverdueDate()) > 0) {
+            int point = Period.between(rentalItem.getOverdueDate(), returnDate).getDays() * 10;
+            this.lateFee = this.lateFee.addPoint(point);
+        }
+    }
+
+    private void checkRentalAvailable() {
+        if (this.rentStatus == RentStatus.RENT_UNAVAILABLE) {
+            throw new IllegalArgumentException("대여 불가 상태입니다.");
+        }
+
+        if (this.rentalItems.size() >= 5) {
+            throw new IllegalArgumentException("이미 5권을 대여중입니다.");
+        }
+    }
+
+    private void addRentalItem(RentalItem rentalItem) {
         this.rentalItems.add(rentalItem);
     }
 
-    public void removeRentalItem(RentalItem rentalItem) {
+    private void removeRentalItem(RentalItem rentalItem) {
         this.rentalItems.remove(rentalItem);
     }
 
-    public void addReturnItem(ReturnItem returnItem) {
+    private void addReturnItem(ReturnItem returnItem) {
         this.returnItems.add(returnItem);
     }
 
-    public void removeReturnItem(ReturnItem returnItem) {
+    private void removeReturnItem(ReturnItem returnItem) {
         this.returnItems.remove(returnItem);
     }
 }
--- msa_practice/book-rental/rental/src/test/java/com/example/rental/domain/model/RentalCardTest.java
+++ msa_practice/book-rental/rental/src/test/java/com/example/rental/domain/model/RentalCardTest.java
@@ -1,100 +1,271 @@
 package com.example.rental.domain.model;
 
+import com.example.rental.domain.model.vo.IDName;
 import com.example.rental.domain.model.vo.Item;
-import com.example.rental.domain.model.vo.ReturnItem;
+import com.example.rental.domain.model.vo.LateFee;
+import com.example.rental.domain.model.vo.RentStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import static com.example.rental.domain.model.vo.RentStatus.RENT_AVAILABLE;
+import static com.example.rental.domain.model.vo.RentStatus.RENT_UNAVAILABLE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.groups.Tuple.tuple;
 
 class RentalCardTest {
 
     @Test
-    @DisplayName("대여품목을 추가할 수 있다.")
-    void addRentalItem() {
+    @DisplayName("RentalCard를 생성할 수 있다.")
+    void createRentalCard() {
         // given
-        RentalCard rentalCard = RentalCard.builder()
+
+        // when
+        RentalCard result = RentalCard.createRentalCard(new IDName("001", "bobby"));
+
+        // then
+        assertThat(result.getMember()).isNotNull()
+                .extracting("id", "name")
+                .containsExactly("001", "bobby");
+
+        assertThat(result.getRentStatus()).isEqualTo(RENT_AVAILABLE);
+        assertThat(result.getLateFee().getPoint()).isEqualTo(0);
+        assertThat(result.getRentalItems()).isEmpty();
+        assertThat(result.getReturnItems()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("도서를 대여할 수 있다.")
+    void rentItem() {
+        // given
+        RentalCard rentalCard = testRentalCard(new IDName("001", "bobby"), RENT_AVAILABLE);
+        Item item = testItem(1L, "bobby");
+        LocalDate rentDate = LocalDate.of(2023, 11, 22);
+
+        // when
+        RentalCard result = rentalCard.rentItem(item, rentDate);
+
+        // then
+        assertThat(result.getRentalItems()).hasSize(1)
+                .extracting("item", "rentDate", "overdue", "overdueDate")
+                .containsExactly(
+                        tuple(item, rentDate, false, rentDate.plusDays(14))
+                );
+    }
+
+    @Test
+    @DisplayName("대여 불가 상태일 때 도서 대여를 할 수 없다.")
+    void rentItemWithStatus() {
+        // given
+        RentalCard rentalCard = testRentalCard(new IDName("001", "bobby"), RENT_UNAVAILABLE);
+        Item item = testItem(1L, "bobby");
+        LocalDate rentDate = LocalDate.of(2023, 11, 22);
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> rentalCard.rentItem(item, rentDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("대여 불가 상태입니다.");
+    }
+
+    @Test
+    @DisplayName("도서 대여를 최대 5권 할 수 있다.")
+    void rentItemLimit() {
+        // given
+        LocalDate rentDate = LocalDate.of(2023, 11, 22);
+        RentalCard rentalCard = testRentalCard(new IDName("001", "bobby"), RENT_AVAILABLE);
+        rentalCard.getRentalItems().add(testRentalItem(testItem(1L, "bobby"), rentDate));
+        rentalCard.getRentalItems().add(testRentalItem(testItem(2L, "bobby"), rentDate));
+        rentalCard.getRentalItems().add(testRentalItem(testItem(3L, "bobby"), rentDate));
+        rentalCard.getRentalItems().add(testRentalItem(testItem(4L, "bobby"), rentDate));
+        rentalCard.getRentalItems().add(testRentalItem(testItem(5L, "bobby"), rentDate));
+
+        Item newItem = testItem(6L, "bobby");
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> rentalCard.rentItem(newItem, rentDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미 5권을 대여중입니다.");
+    }
+
+    @Test
+    @DisplayName("도서를 반납할 수 있다.")
+    void returnItem() {
+        // given
+        LocalDate rentDate = LocalDate.of(2023, 11, 22);
+        LocalDate returnDate = LocalDate.of(2023, 11, 22);
+        RentalCard rentalCard = testRentalCard(new IDName("001", "bobby"), RENT_AVAILABLE);
+        Item item = testItem(1L, "bobby");
+        rentalCard.getRentalItems().add(testRentalItem(item, rentDate));
+
+        // when
+        RentalCard result = rentalCard.returnItem(item, returnDate);
+
+        // then
+        assertThat(result.getRentalItems()).isEmpty();
+        assertThat(result.getReturnItems()).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("대여한 도서가 없을 때 도서를 반납할 수 없다.")
+    void returnItemWithNoItem() {
+        // given
+        LocalDate rentDate = LocalDate.of(2023, 11, 22);
+        LocalDate returnDate = LocalDate.of(2023, 11, 22);
+        RentalCard rentalCard = testRentalCard(new IDName("001", "bobby"), RENT_AVAILABLE);
+        Item item = testItem(1L, "bobby");
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> rentalCard.returnItem(item, returnDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("반납할 도서가 없습니다.");
+    }
+
+    @Test
+    @DisplayName("도서를 반납 시 연체료가 추가된다.(1일당 10포인트)")
+    void returnItemLateFee() {
+        // given
+        LocalDate rentDate = LocalDate.of(2023, 1, 1);
+        LocalDate returnDate = LocalDate.of(2023, 1, 20);
+        RentalCard rentalCard = testRentalCard(new IDName("001", "bobby"), RENT_AVAILABLE);
+        Item item = testItem(1L, "bobby");
+        rentalCard.getRentalItems().add(testRentalItem(item, rentDate));
+
+        // when
+        RentalCard result = rentalCard.returnItem(item, returnDate);
+
+        // then
+        assertThat(result.getRentalItems()).isEmpty();
+        assertThat(result.getReturnItems()).hasSize(1);
+        assertThat(result.getLateFee().getPoint()).isEqualTo(5 * 10);
+    }
+
+    @Test
+    @DisplayName("연체처리되면 대여 불가 상태로 변경된다.")
+    void overdueItem() {
+        // given
+        LocalDate rentDate = LocalDate.of(2023, 1, 1);
+        RentalCard rentalCard = testRentalCard(new IDName("001", "bobby"), RENT_AVAILABLE);
+        Item item = testItem(1L, "bobby");
+        rentalCard.getRentalItems().add(testRentalItem(item, rentDate));
+
+        // when
+        RentalCard result = rentalCard.overdueItem(item);
+
+        // then
+        assertThat(result.getRentalItems().get(0).isOverdue()).isTrue();
+        assertThat(result.getRentStatus()).isEqualTo(RENT_UNAVAILABLE);
+    }
+
+    @Test
+    @DisplayName("해당 도서가 없으면 연체처리 할 수 없다.")
+    void overdueNoItem() {
+        // given
+        RentalCard rentalCard = testRentalCard(new IDName("001", "bobby"), RENT_AVAILABLE);
+        Item item = testItem(1L, "bobby");
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> rentalCard.overdueItem(item))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 도서가 없습니다.");
+    }
+
+    @Test
+    @DisplayName("대여 불가 상태를 해제할 수 있다.")
+    void makeAvailableRental() {
+        // given
+        RentalCard rentalCard = testRentalCard(new IDName("001", "bobby"), RENT_UNAVAILABLE);
+
+        // when
+        rentalCard.makeAvailableRental(0);
+
+        // then
+        assertThat(rentalCard.getRentStatus()).isEqualTo(RENT_AVAILABLE);
+    }
+
+    @Test
+    @DisplayName("반납되지 않은 도서가 있으면 대여 불가 상태를 해제할 수 없다.")
+    void makeAvailableRentalFail() {
+        // given
+        LocalDate rentDate = LocalDate.of(2023, 1, 1);
+        RentalCard rentalCard = testRentalCard(new IDName("001", "bobby"), RENT_UNAVAILABLE);
+        Item item = testItem(1L, "bobby");
+        rentalCard.getRentalItems().add(testRentalItem(item, rentDate));
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> rentalCard.makeAvailableRental(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("모든 도서가 반납되어야 정지를 해제할 수 있습니다.");
+    }
+
+    @Test
+    @DisplayName("대여 불가 상태를 해제할 때 연체료가 있으면 차감해야한다.")
+    void makeAvailableRentalWithPoint() {
+        // given
+        RentalCard rentalCard = testRentalCard(new IDName("001", "bobby"), RENT_UNAVAILABLE, new LateFee(100));
+
+        // when
+        rentalCard.makeAvailableRental(100);
+
+        // then
+        assertThat(rentalCard.getLateFee().getPoint()).isEqualTo(0);
+        assertThat(rentalCard.getRentStatus()).isEqualTo(RENT_AVAILABLE);
+    }
+
+    @Test
+    @DisplayName("대여 불가 상태를 해제할 때 연체포인트가 다르면 해제할 수 없다.")
+    void makeAvailableRentalWithNotEqualPoint() {
+        // given
+        RentalCard rentalCard = testRentalCard(new IDName("001", "bobby"), RENT_UNAVAILABLE, new LateFee(100));
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> rentalCard.makeAvailableRental(50))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 포인트로 연체를 해제할 수 없습니다.");
+    }
+
+    private RentalItem testRentalItem(Item item, LocalDate rentDate) {
+        return RentalItem.builder()
+                .item(item)
+                .rentDate(rentDate)
+                .overdue(false)
+                .overdueDate(rentDate.plusDays(14))
+                .build();
+    }
+
+    private RentalCard testRentalCard(IDName creator, RentStatus rentStatus) {
+        return testRentalCard(creator, rentStatus, new LateFee(0));
+    }
+
+    private RentalCard testRentalCard(IDName creator, RentStatus rentStatus, LateFee lateFee) {
+        return RentalCard.builder()
+                .member(creator)
+                .lateFee(lateFee)
+                .rentStatus(rentStatus)
                 .rentalItems(new ArrayList<>())
-                .build();
-        Item item = Item.builder().build();
-        LocalDate rentDate = LocalDate.of(2023, 11, 22);
-        RentalItem rentalItem = RentalItem.createRentalItem(item, rentDate);
-
-        // when
-        rentalCard.addRentalItem(rentalItem);
-
-        // then
-        assertThat(rentalCard.getRentalItems()).hasSize(1);
-    }
-
-    @Test
-    @DisplayName("대여품목을 삭제할 수 있다.")
-    void removeRentalItem() {
-        // given
-        Item item = Item.builder().build();
-        LocalDate rentDate = LocalDate.of(2023, 11, 22);
-        RentalItem rentalItem = RentalItem.createRentalItem(item, rentDate);
-
-        RentalCard rentalCard = RentalCard.builder()
-                .rentalItems(new ArrayList<>() {{
-                    add(rentalItem);
-                }})
-                .build();
-
-        // when
-        rentalCard.removeRentalItem(rentalItem);
-
-        // then
-        assertThat(rentalCard.getRentalItems()).isEmpty();
-    }
-
-    @Test
-    @DisplayName("반납품목을 추가할 수 있다.")
-    void addReturnItem() {
-        // given
-        RentalCard rentalCard = RentalCard.builder()
                 .returnItems(new ArrayList<>())
                 .build();
-
-        RentalItem rentalItem = RentalItem.createRentalItem(
-                Item.builder().build(), LocalDate.of(2023, 11, 22)
-        );
-        ReturnItem returnItem = ReturnItem.createReturnItem(
-                rentalItem, LocalDate.of(2023, 11, 22)
-        );
-
-        // when
-        rentalCard.addReturnItem(returnItem);
-
-        // then
-        assertThat(rentalCard.getReturnItems()).hasSize(1);
     }
 
-    @Test
-    @DisplayName("판납품목을 삭제할 수 있다.")
-    void removeReturnItem() {
-        // given
-        RentalItem rentalItem = RentalItem.createRentalItem(
-                Item.builder().build(), LocalDate.of(2023, 11, 22)
-        );
-        ReturnItem returnItem = ReturnItem.createReturnItem(
-                rentalItem, LocalDate.of(2023, 11, 22)
-        );
-
-        RentalCard rentalCard = RentalCard.builder()
-                .returnItems(new ArrayList<>() {{
-                    add(returnItem);
-                }})
+    private static Item testItem(Long no, String title) {
+        return Item.builder()
+                .no(no)
+                .title(title)
                 .build();
-
-        // when
-        rentalCard.removeReturnItem(returnItem);
-
-        // then
-        assertThat(rentalCard.getReturnItems()).isEmpty();
     }
 
 }
