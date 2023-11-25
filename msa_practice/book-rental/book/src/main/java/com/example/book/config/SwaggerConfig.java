0e5f81487ab48f2fc42b56dded426623a18de606
haerong22 <haerong22@naver.com>
2023-11-25 21:42:56 +0900

    #43 book rental: book - init

---
+++ msa_practice/book-rental/book/src/main/java/com/example/book/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.example.book.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(true)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.book"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
