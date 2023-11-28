ba0e1306620584337fe1b6c0a2a0f32a633d700f
haerong22 <haerong22@naver.com>
2023-11-28 14:48:35 +0900

    #43 book rental: best book - init

---
+++ msa_practice/book-rental/best/src/main/java/com/example/best/BestApplication.java
@@ -0,0 +1,13 @@
+package com.example.best;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestApplication.class, args);
+	}
+
+}
