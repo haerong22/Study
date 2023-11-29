976c3cc7fd49c4ea1b3425597c8651aa2aaedb6e
haerong22 <haerong22@naver.com>
2023-11-29 18:04:29 +0900

    #43 book rental: book - SAGA패턴 보상 트랜잭션 구현

--- msa_practice/book-rental/book/src/main/java/com/example/book/adapter/out/kafka/BookEventConsumers.java
+++ msa_practice/book-rental/book/src/main/java/com/example/book/adapter/out/kafka/BookEventConsumers.java
@@ -2,6 +2,8 @@ package com.example.book.adapter.out.kafka;
 
 import com.example.book.application.port.in.MakeAvailableUseCase;
 import com.example.book.application.port.in.MakeUnavailableUseCase;
+import com.example.book.domain.model.event.EventResult;
+import com.example.book.domain.model.event.EventType;
 import com.example.book.domain.model.event.ItemRented;
 import com.example.book.domain.model.event.ItemReturned;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -20,18 +22,49 @@ public class BookEventConsumers {
     private final ObjectMapper objectMapper;
     private final MakeAvailableUseCase makeAvailableUsecase;
     private final MakeUnavailableUseCase makeUnavailableUseCase;
+    private final BookEventProducers bookEventProducers;
 
     @KafkaListener(topics = "${consumer.topic1.name}", groupId = "${consumer.groupId.name}")
     public void consumeRental(ConsumerRecord<String, String> record) throws JsonProcessingException {
         log.info("rental_rent: {}", record.value());
         ItemRented itemRented = objectMapper.readValue(record.value(), ItemRented.class);
-        makeUnavailableUseCase.unavailable(itemRented.getItem().getNo());
+
+        EventResult eventResult = new EventResult();
+        eventResult.setEventType(EventType.RENT);
+        eventResult.setIdName(itemRented.getIdName());
+        eventResult.setItem(itemRented.getItem());
+        eventResult.setPoint(itemRented.getPoint());
+
+        try {
+            makeUnavailableUseCase.unavailable(itemRented.getItem().getNo());
+            eventResult.setSuccess(true);
+        } catch (Exception e) {
+            log.info("도서 대여 실패");
+            eventResult.setSuccess(false);
+        }
+
+        bookEventProducers.produce(eventResult);
     }
 
     @KafkaListener(topics = "${consumer.topic2.name}", groupId = "${consumer.groupId.name}")
     public void consumeReturn(ConsumerRecord<String, String> record) throws JsonProcessingException {
         log.info("rental_return: {}", record.value());
         ItemReturned itemReturned = objectMapper.readValue(record.value(), ItemReturned.class);
-        makeAvailableUsecase.available(itemReturned.getItem().getNo());
+
+        EventResult eventResult = new EventResult();
+        eventResult.setEventType(EventType.RETURN);
+        eventResult.setIdName(itemReturned.getIdName());
+        eventResult.setItem(itemReturned.getItem());
+        eventResult.setPoint(itemReturned.getPoint());
+
+        try {
+            makeAvailableUsecase.available(itemReturned.getItem().getNo());
+            eventResult.setSuccess(true);
+        } catch (Exception e) {
+            log.info("도서 반납 실패");
+            eventResult.setSuccess(false);
+        }
+
+        bookEventProducers.produce(eventResult);
     }
 }
---
+++ msa_practice/book-rental/book/src/main/java/com/example/book/domain/model/event/EventType.java
@@ -0,0 +1,8 @@
+package com.example.book.domain.model.event;
+
+public enum EventType {
+    RENT,
+    RETURN,
+    OVERDUE,
+
+}
