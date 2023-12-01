57c49abd8671fadfe9fd0424e4d5c80bb24dc985
haerong22 <haerong22@naver.com>
2023-12-01 21:59:13 +0900

    #38 hexagonal: payment - 뱅킹 서비스, 멤버 서비스 어댑터 구현

---
+++ bobby-pay/payment-service/src/main/java/org/example/payment/adapter/out/service/MembershipServiceAdapter.java
@@ -0,0 +1,43 @@
+package org.example.payment.adapter.out.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.common.CommonHttpClient;
+import org.example.payment.application.port.out.GetMembershipPort;
+import org.example.payment.application.port.out.MembershipStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MembershipServiceAdapter implements GetMembershipPort {
+
+    private final CommonHttpClient commonHttpClient;
+
+    private final String membershipServiceUrl;
+    private final ObjectMapper mapper;
+
+    public MembershipServiceAdapter(
+            CommonHttpClient commonHttpClient,
+            @Value("${service.membership.url}") String membershipServiceUrl,
+            ObjectMapper mapper) {
+        this.commonHttpClient = commonHttpClient;
+        this.membershipServiceUrl = membershipServiceUrl;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public MembershipStatus getMembership(String membershipId) {
+
+        final String url = String.join("/", membershipServiceUrl, "membership", membershipId);
+
+        try {
+            String jsonResponse = commonHttpClient.sendGetRequest(url).body();
+
+            Membership membership = mapper.readValue(jsonResponse, Membership.class);
+
+            return new MembershipStatus(membership.getMembershipId(), membership.isValid());
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
