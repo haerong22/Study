016ad546974bee31f101ea5eaf5155ee17cde1fe
haerong22 <haerong22@naver.com>
2023-12-03 19:59:32 +0900

    #38 hexagonal: money query - init

---
+++ bobby-pay/money-query-service/src/main/java/org/example/money/query/MoneyQueryConfig.java
@@ -0,0 +1,10 @@
+package org.example.money.query;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.example.common")
+public class MoneyQueryConfig {
+}
