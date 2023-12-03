1fd81c6477289a4d933171a4092e99baae6893df
haerong22 <haerong22@naver.com>
2023-12-03 16:28:21 +0900

    #38 hexagonal: money aggregation - init

---
+++ bobby-pay/money-aggregation-service/src/main/java/org/example/money/aggregation/MoneyAggregationApplication.java
@@ -0,0 +1,12 @@
+package org.example.money.aggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyAggregationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyAggregationApplication.class, args);
+    }
+}
