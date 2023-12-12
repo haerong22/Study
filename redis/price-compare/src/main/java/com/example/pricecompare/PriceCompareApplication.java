f0de9ffca036a22f55fe992218a1a02722bd5b12
haerong22 <haerong22@naver.com>
2023-12-12 15:26:11 +0900

    #44 price compare: redis - init

---
+++ redis/price-compare/src/main/java/com/example/pricecompare/PriceCompareApplication.java
@@ -0,0 +1,13 @@
+package com.example.pricecompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceCompareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceCompareApplication.class, args);
+    }
+
+}
---
+++ redis/price-compare/src/test/java/com/example/pricecompare/PriceCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pricecompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceCompareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
