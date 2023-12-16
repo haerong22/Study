7d851d62a4c5dde5084b32ffe0c0facb2c65c9ea
haerong22 <haerong22@naver.com>
2023-12-16 20:41:06 +0900

    #45 e-commerce: order - Partner 서비스 구현

---
+++ msa_practice/e-commerce/order/src/main/java/com/example/order/domain/partner/PartnerServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.order.domain.partner;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PartnerServiceImpl implements PartnerService {
+
+    private final PartnerStore partnerStore;
+    private final PartnerReader partnerReader;
+
+    @Override
+    @Transactional
+    public PartnerInfo registerPartner(PartnerCommand command) {
+        Partner initPartner = command.toEntity();
+        Partner partner = partnerStore.store(initPartner);
+        return PartnerInfo.of(partner);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public PartnerInfo getPartnerInfo(String partnerToken) {
+        Partner partner = partnerReader.getPartner(partnerToken);
+        return PartnerInfo.of(partner);
+    }
+
+    @Override
+    @Transactional
+    public PartnerInfo enablePartner(String partnerToken) {
+        Partner partner = partnerReader.getPartner(partnerToken);
+        partner.enable();
+        return PartnerInfo.of(partner);
+    }
+
+    @Override
+    @Transactional
+    public PartnerInfo disablePartner(String partnerToken) {
+        Partner partner = partnerReader.getPartner(partnerToken);
+        partner.disable();
+        return PartnerInfo.of(partner);
+    }
+}
