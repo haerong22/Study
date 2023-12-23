df4389329188a56da30f1cddd605d1d8761053dc
haerong22 <haerong22@naver.com>
2023-12-23 21:17:12 +0900

    #45 e-commerce: order - order: 배송정보 도메인 분리

---
+++ msa_practice/e-commerce/order/src/main/java/com/example/order/domain/order/fragment/DeliveryFragment.java
@@ -0,0 +1,44 @@
+package com.example.order.domain.order.fragment;
+
+import com.example.order.common.exception.InvalidParamException;
+import jakarta.persistence.Embeddable;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+@Getter
+@Embeddable
+@NoArgsConstructor
+public class DeliveryFragment {
+    private String receiverName;
+    private String receiverPhone;
+    private String receiverZipcode;
+    private String receiverAddress1;
+    private String receiverAddress2;
+    private String etcMessage;
+
+    @Builder
+    public DeliveryFragment(
+            String receiverName,
+            String receiverPhone,
+            String receiverZipcode,
+            String receiverAddress1,
+            String receiverAddress2,
+            String etcMessage
+    ) {
+        if (StringUtils.isEmpty(receiverName)) throw new InvalidParamException("DeliveryFragment receiverName");
+        if (StringUtils.isEmpty(receiverPhone)) throw new InvalidParamException("DeliveryFragment receiverPhone");
+        if (StringUtils.isEmpty(receiverZipcode)) throw new InvalidParamException("DeliveryFragment receiverZipcode");
+        if (StringUtils.isEmpty(receiverAddress1)) throw new InvalidParamException("DeliveryFragment receiverAddress1");
+        if (StringUtils.isEmpty(receiverAddress2)) throw new InvalidParamException("DeliveryFragment receiverAddress2");
+        if (StringUtils.isEmpty(etcMessage)) throw new InvalidParamException("DeliveryFragment etcMessage");
+        
+        this.receiverName = receiverName; 
+        this.receiverPhone = receiverPhone; 
+        this.receiverZipcode = receiverZipcode; 
+        this.receiverAddress1 = receiverAddress1; 
+        this.receiverAddress2 = receiverAddress2; 
+        this.etcMessage = etcMessage; 
+    }
+}
