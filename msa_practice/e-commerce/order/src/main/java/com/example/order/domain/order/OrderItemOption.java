fa3a3af3a343b5f8a5b93eb333c8b0125f0f88bf
haerong22 <haerong22@naver.com>
2023-12-23 21:13:30 +0900

    #45 e-commerce: order - order: 도메인 정의

---
+++ msa_practice/e-commerce/order/src/main/java/com/example/order/domain/order/OrderItemOption.java
@@ -0,0 +1,44 @@
+package com.example.order.domain.order;
+
+import com.example.order.common.exception.InvalidParamException;
+import com.example.order.domain.AbstractEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "order_item_options")
+public class OrderItemOption extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_item_option_group_id")
+    private OrderItemOptionGroup orderItemOptionGroup;
+    private Integer ordering;
+    private String itemOptionName;
+    private Long itemOptionPrice;
+
+    @Builder
+    public OrderItemOption(
+            OrderItemOptionGroup orderItemOptionGroup,
+            Integer ordering,
+            String itemOptionName,
+            Long itemOptionPrice) {
+        if (orderItemOptionGroup == null) throw new InvalidParamException();
+        if (ordering == null) throw new InvalidParamException();
+        if (StringUtils.isEmpty(itemOptionName)) throw new InvalidParamException();
+        if (itemOptionPrice == null) throw new InvalidParamException();
+
+        this.orderItemOptionGroup = orderItemOptionGroup;
+        this.ordering = ordering;
+        this.itemOptionName = itemOptionName;
+        this.itemOptionPrice = itemOptionPrice;
+    }
+}
---
+++ msa_practice/e-commerce/order/src/main/java/com/example/order/domain/order/OrderItemOptionGroup.java
@@ -0,0 +1,55 @@
+package com.example.order.domain.order;
+
+import com.example.order.common.exception.InvalidParamException;
+import com.example.order.domain.AbstractEntity;
+import com.google.common.collect.Lists;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+
+@Slf4j
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "order_item_option_groups")
+public class OrderItemOptionGroup extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_item_id")
+    private OrderItem orderItem;
+    private Integer ordering;
+    private String itemOptionGroupName;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "orderItemOptionGroup", cascade = CascadeType.PERSIST)
+    private List<OrderItemOption> orderItemOptionList = Lists.newArrayList();
+
+    @Builder
+    public OrderItemOptionGroup(
+            OrderItem orderItem,
+            Integer ordering,
+            String itemOptionGroupName
+    ) {
+        if (orderItem == null) throw new InvalidParamException();
+        if (ordering == null) throw new InvalidParamException();
+        if (StringUtils.isEmpty(itemOptionGroupName)) throw new InvalidParamException();
+                
+        this.orderItem = orderItem;
+        this.ordering = ordering;
+        this.itemOptionGroupName = itemOptionGroupName;
+    }
+
+    public Long calculateTotalAmount() {
+        return orderItemOptionList.stream()
+                .mapToLong(OrderItemOption::getItemOptionPrice)
+                .sum();
+    }
+}
