714d13cd2758dd3b15c630eed3b471a05e87e22e
haerong22 <haerong22@naver.com>
2023-12-28 23:15:08 +0900

    #45 e-commerce: order - order: order api 구현

---
+++ msa_practice/e-commerce/order/src/main/java/com/example/order/interfaces/order/OrderApiController.java
@@ -0,0 +1,39 @@
+package com.example.order.interfaces.order;
+
+import com.example.order.application.order.OrderFacade;
+import com.example.order.common.response.CommonResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/orders")
+public class OrderApiController {
+    private final OrderFacade orderFacade;
+    private final OrderDtoMapper orderDtoMapper;
+
+    @PostMapping("/init")
+    public CommonResponse registerOrder(@RequestBody @Valid OrderDto.RegisterOrderRequest request) {
+        var orderCommand = orderDtoMapper.of(request);
+        var orderToken = orderFacade.registerOrder(orderCommand);
+        var response = orderDtoMapper.of(orderToken);
+        return CommonResponse.success(response);
+    }
+
+    @GetMapping("/{orderToken}")
+    public CommonResponse retrieveOrder(@PathVariable String orderToken) {
+        var orderResult = orderFacade.retrieveOrder(orderToken);
+        var response = orderDtoMapper.of(orderResult);
+        return CommonResponse.success(response);
+    }
+
+    @PostMapping("/payment-order")
+    public CommonResponse paymentOrder(@RequestBody @Valid OrderDto.PaymentRequest paymentRequest) {
+        var paymentCommand = orderDtoMapper.of(paymentRequest);
+        orderFacade.paymentOrder(paymentCommand);
+        return CommonResponse.success("OK");
+    }
+}
---
+++ msa_practice/e-commerce/order/src/main/java/com/example/order/interfaces/order/OrderDtoMapper.java
@@ -0,0 +1,34 @@
+package com.example.order.interfaces.order;
+
+import com.example.order.domain.order.OrderCommand;
+import com.example.order.domain.order.OrderInfo;
+import org.mapstruct.*;
+
+@Mapper(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        unmappedTargetPolicy = ReportingPolicy.ERROR
+)
+public interface OrderDtoMapper {
+
+    @Mappings({
+            @Mapping(source = "orderedAt", target = "orderedAt", dateFormat = "yyyy-MM-dd HH:mm:ss")
+    })
+    OrderDto.Main of(OrderInfo.Main mainResult);
+
+    OrderDto.DeliveryInfo of(OrderInfo.DeliveryInfo deliveryResult);
+
+    OrderDto.OrderItem of(OrderInfo.OrderItem orderItemResult);
+
+    OrderCommand.RegisterOrder of(OrderDto.RegisterOrderRequest request);
+
+    OrderCommand.RegisterOrderItem of(OrderDto.RegisterOrderItem request);
+
+    OrderCommand.RegisterOrderItemOptionGroup of(OrderDto.RegisterOrderItemOptionGroupRequest request);
+
+    OrderCommand.RegisterOrderItemOption of(OrderDto.RegisterOrderItemOptionRequest request);
+
+    OrderDto.RegisterResponse of(String orderToken);
+
+    OrderCommand.PaymentRequest of(OrderDto.PaymentRequest request);
+}
