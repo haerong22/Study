dc4d1e94148504e8190b8b7e4c6c2fab0853c17c
haerong22 <haerong22@naver.com>
2023-12-31 20:41:23 +0900

    #45 e-commerce: gift - init

---
+++ msa_practice/e-commerce/gift/src/test/java/com/example/gift/GiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
