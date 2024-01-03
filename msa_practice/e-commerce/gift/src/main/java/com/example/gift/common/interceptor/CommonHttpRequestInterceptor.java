8f210d3fcde08fec3fc80c3fb5f3e40a01dc6a54
haerong22 <haerong22@naver.com>
2024-01-03 20:56:38 +0900

    #45 e-commerce: gift - common exception handler

---
+++ msa_practice/e-commerce/gift/src/main/java/com/example/gift/common/interceptor/CommonHttpRequestInterceptor.java
@@ -0,0 +1,29 @@
+package com.example.gift.common.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class CommonHttpRequestInterceptor implements HandlerInterceptor {
+
+    public static final String HEADER_REQUEST_UUID_KEY = "x-request-id";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String requestEventId = request.getHeader(HEADER_REQUEST_UUID_KEY);
+        if (StringUtils.isEmpty(requestEventId)) {
+            requestEventId = UUID.randomUUID().toString();
+        }
+
+        MDC.put(HEADER_REQUEST_UUID_KEY, requestEventId);
+        return true;
+    }
+}
