b513b10d15021206cc0e71865bc0e5ef7e2711a3
haerong22 <haerong22@naver.com>
2024-01-13 22:07:45 +0900

    #45 e-commerce: gift - aws sqs message listener

---
+++ msa_practice/e-commerce/gift/src/main/java/com/example/gift/interfaces/message/GiftPaymentCompleteMessage.java
@@ -0,0 +1,12 @@
+package com.example.gift.interfaces.message;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+public class GiftPaymentCompleteMessage {
+    private String orderToken;
+}
