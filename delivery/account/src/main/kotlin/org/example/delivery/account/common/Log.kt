3841c9dbe710937fe65a7b05dd61342fd75e1f4f
haerong22 <haerong22@naver.com>
2024-03-04 16:20:58 +0900

    #48 delivery: account - 설정

---
+++ delivery/account/src/main/kotlin/org/example/delivery/account/common/Log.kt
@@ -0,0 +1,9 @@
+package org.example.delivery.account.common
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+interface Log {
+
+    val log: Logger get() = LoggerFactory.getLogger(this.javaClass)
+}
