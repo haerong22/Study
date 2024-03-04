e9f1aeda762897a04a1e593199efd8ed3d41339f
haerong22 <haerong22@naver.com>
2024-03-04 14:48:57 +0900

    #48 delivery: account - init

---
+++ delivery/account/src/main/kotlin/org/example/delivery/account/AccountApplication.kt
@@ -0,0 +1,12 @@
+package org.example.delivery.account
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccountApplication {
+}
+
+fun main(args: Array<String>) {
+    runApplication<AccountApplication>(*args)
+}
