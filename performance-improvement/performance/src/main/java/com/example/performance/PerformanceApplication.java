0c03b4ae8e6c7ebbe92653b28a44550fcd18c05b
haerong22 <haerong22@naver.com>
2024-03-08 19:41:59 +0900

    #50 performance: demo app init

---
+++ performance-improvement/performance/src/main/java/com/example/performance/PerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceApplication.class, args);
+    }
+
+}
---
+++ performance-improvement/performance/src/test/java/com/example/performance/PerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.performance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
