97abfef3386b12f2fda94489b4ad2d5e310459b1
haerong22 <haerong22@naver.com>
2024-05-14 22:46:33 +0900

    #52 java 12 to 17 : Text Block

---
+++ java/java12to17/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "java12to17"
+
---
+++ java/java12to17/src/main/java/TextBlock.java
@@ -0,0 +1,71 @@
+public class TextBlock {
+
+    public static void main(String[] args) {
+
+        /*
+            A
+            BC
+            DEF
+         */
+        String str1 = "A\nBC\nDEF";
+
+        String str2 = "A\n" +
+                "BC\n" +
+                "DEF";
+
+        String textBlock = """
+                A
+                BC
+                DEF""";
+
+        System.out.println("str1 = " + str1);
+        System.out.println("str2 = " + str2);
+        System.out.println("textBlock = " + textBlock);
+        System.out.println(str1.equals(textBlock));
+
+        /**
+         * 뒷 공백 사용
+         */
+        String textBlock1 = """
+                A$$
+                BC$
+                DEF
+                """.replace('$', ' ');
+
+        String textBlock2 = """
+                A  |
+                BC |
+                DEF|
+                """.replace("|\n", "\n");
+
+        // `octal escape sequence` 사용
+        String textBlock3 = """
+                A\040\040
+                BC\040
+                DEF
+                """;
+
+        // \s 사용 - 공백을 의미
+        String textBlock4 = """
+                A \s
+                BC\s
+                DEF
+                """;
+
+        System.out.println("textBlock1 = " + textBlock1);
+        System.out.println("textBlock2 = " + textBlock2);
+        System.out.println("textBlock3 = " + textBlock3);
+        System.out.println("textBlock4 = " + textBlock4);
+
+        /**
+         * line terminator
+         */
+        String textBlock5 = """
+                A  \
+                BC \
+                DEF
+                """;
+
+        System.out.println("textBlock5 = " + textBlock5);
+    }
+}
