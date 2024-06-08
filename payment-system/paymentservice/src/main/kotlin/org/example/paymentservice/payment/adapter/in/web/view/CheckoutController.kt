3240f4dfbeb8cf0efbe76eb50f75bfb4b624fc05
haerong22 <haerong22@naver.com>
2024-06-08 18:15:43 +0900

    #53 payment system: payment checkout api

--- payment-system/paymentservice/src/main/kotlin/org/example/paymentservice/payment/adapter/in/web/view/CheckoutController.kt
+++ payment-system/paymentservice/src/main/kotlin/org/example/paymentservice/payment/adapter/in/web/view/CheckoutController.kt
@@ -1,16 +1,40 @@
 package org.example.paymentservice.payment.adapter.`in`.web.view
 
+import org.example.paymentservice.common.IdempotencyCreator
 import org.example.paymentservice.common.WebAdapter
+import org.example.paymentservice.payment.adapter.`in`.web.request.CheckoutRequest
+import org.example.paymentservice.payment.application.port.`in`.CheckoutCommand
+import org.example.paymentservice.payment.application.port.`in`.CheckoutUseCase
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import reactor.core.publisher.Mono
 
 @WebAdapter
 @Controller
-class CheckoutController {
+class CheckoutController(
+    private val checkoutUseCase: CheckoutUseCase,
+) {
 
     @GetMapping
-    fun checkoutPage(): Mono<String> {
-        return Mono.just("checkout")
+    fun checkoutPage(
+        request: CheckoutRequest,
+        model: Model,
+    ): Mono<String> {
+
+        val command = CheckoutCommand(
+            cartId = request.cartId,
+            buyerId = request.buyerId,
+            productIds = request.productIds,
+            idempotencyKey = IdempotencyCreator.create(request.seed)
+        )
+
+        return checkoutUseCase.checkout(command)
+            .map {
+                model.addAttribute("orderId", it.orderId)
+                model.addAttribute("orderName", it.orderName)
+                model.addAttribute("amount", it.amount)
+                "checkout"
+            }
     }
 }
