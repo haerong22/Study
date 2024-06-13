d1aef929d59466a1f575ce1cb8754f171d5b40be
haerong22 <haerong22@naver.com>
2024-06-13 20:56:31 +0900

    #53 payment system: payment persistent adapter

--- payment-system/paymentservice/src/main/kotlin/org/example/paymentservice/payment/adapter/out/persistent/PaymentPersistentAdapter.kt
+++ payment-system/paymentservice/src/main/kotlin/org/example/paymentservice/payment/adapter/out/persistent/PaymentPersistentAdapter.kt
@@ -2,6 +2,11 @@ package org.example.paymentservice.payment.adapter.out.persistent
 
 import org.example.paymentservice.common.PersistentAdapter
 import org.example.paymentservice.payment.adapter.out.persistent.repository.PaymentRepository
+import org.example.paymentservice.payment.adapter.out.persistent.repository.PaymentStatusUpdateRepository
+import org.example.paymentservice.payment.adapter.out.persistent.repository.PaymentValidationRepository
+import org.example.paymentservice.payment.application.port.out.PaymentStatusUpdateCommand
+import org.example.paymentservice.payment.application.port.out.PaymentStatusUpdatePort
+import org.example.paymentservice.payment.application.port.out.PaymentValidationPort
 import org.example.paymentservice.payment.application.port.out.SavePaymentPort
 import org.example.paymentservice.payment.domain.PaymentEvent
 import reactor.core.publisher.Mono
@@ -9,9 +14,22 @@ import reactor.core.publisher.Mono
 @PersistentAdapter
 class PaymentPersistentAdapter(
     private val paymentRepository: PaymentRepository,
-) : SavePaymentPort {
-
+    private val paymentStatusUpdateRepository: PaymentStatusUpdateRepository,
+    private val paymentValidationRepository: PaymentValidationRepository,
+) : SavePaymentPort, PaymentStatusUpdatePort, PaymentValidationPort {
     override fun save(paymentEvent: PaymentEvent): Mono<Void> {
         return paymentRepository.save(paymentEvent)
+    }
+
+    override fun updatePaymentStatusToExecuting(orderId: String, paymentKey: String): Mono<Boolean> {
+        return paymentStatusUpdateRepository.updatePaymentStatusToExecuting(orderId, paymentKey)
+    }
+
+    override fun isValid(orderId: String, amount: Long): Mono<Boolean> {
+        return paymentValidationRepository.isValid(orderId, amount)
+    }
+
+    override fun updatePaymentStatus(command: PaymentStatusUpdateCommand): Mono<Boolean> {
+        return paymentStatusUpdateRepository.updatePaymentStatus(command)
     }
 }
