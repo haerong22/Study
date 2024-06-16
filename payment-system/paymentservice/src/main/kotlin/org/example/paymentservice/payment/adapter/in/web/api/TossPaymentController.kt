828c048185d496433fd44b5f38346313ce2ceb74
haerong22 <haerong22@naver.com>
2024-06-16 17:30:05 +0900

    #53 payment system: payment confirm api

--- payment-system/paymentservice/src/main/kotlin/org/example/paymentservice/payment/adapter/in/web/api/TossPaymentController.kt
+++ payment-system/paymentservice/src/main/kotlin/org/example/paymentservice/payment/adapter/in/web/api/TossPaymentController.kt
@@ -3,7 +3,9 @@ package org.example.paymentservice.payment.adapter.`in`.web.api
 import org.example.paymentservice.common.WebAdapter
 import org.example.paymentservice.payment.adapter.`in`.web.request.TossPaymentConfirmRequest
 import org.example.paymentservice.payment.adapter.`in`.web.response.ApiResponse
-import org.example.paymentservice.payment.adapter.out.web.toss.executor.TossPaymentExecutor
+import org.example.paymentservice.payment.application.port.`in`.PaymentConfirmCommand
+import org.example.paymentservice.payment.application.port.`in`.PaymentConfirmUseCase
+import org.example.paymentservice.payment.domain.PaymentConfirmResult
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -16,21 +18,20 @@ import reactor.core.publisher.Mono
 @RequestMapping("/v1/toss")
 @RestController
 class TossPaymentController(
-    private val tossPaymentExecutor: TossPaymentExecutor,
+    private val paymentConfirmUseCase: PaymentConfirmUseCase,
 ) {
 
     @PostMapping("/confirm")
     fun confirm(
         @RequestBody request: TossPaymentConfirmRequest
-    ): Mono<ResponseEntity<ApiResponse<String>>> {
-        return tossPaymentExecutor.execute(
+    ): Mono<ResponseEntity<ApiResponse<PaymentConfirmResult>>> {
+        val command = PaymentConfirmCommand(
             paymentKey = request.paymentKey,
             orderId = request.orderId,
-            amount = request.amount.toString(),
-        ).map {
-            ResponseEntity.ok().body(
-                ApiResponse.with(HttpStatus.OK, "ok", it)
-            )
-        }
+            amount = request.amount,
+        )
+
+        return paymentConfirmUseCase.confirm(command)
+            .map { ResponseEntity.ok().body(ApiResponse.with(HttpStatus.OK, "", it)) }
     }
 }
