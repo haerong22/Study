96c2d796826a8423398bfdb1afc034551680cc7a
haerong22 <haerong22@naver.com>
2024-06-26 21:19:42 +0900

    #53 payment system: message consume test

--- payment-system/paymentservice/src/main/kotlin/org/example/paymentservice/PaymentserviceApplication.kt
+++ payment-system/paymentservice/src/main/kotlin/org/example/paymentservice/PaymentserviceApplication.kt
@@ -2,11 +2,27 @@ package org.example.paymentservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.messaging.Message
 import org.springframework.scheduling.annotation.EnableScheduling
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.util.function.Function
 
 @EnableScheduling
 @SpringBootApplication
-class PaymentserviceApplication
+class PaymentserviceApplication {
+
+    @Bean
+    fun consume(): Function<Flux<Message<String>>, Mono<Void>> {
+        return Function { message ->
+            message.map {
+                println(it)
+                it
+            }.then()
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<PaymentserviceApplication>(*args)
