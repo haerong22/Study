31cd8b5007eb9b265bc53a7f1a1a1ce62acd374b
haerong22 <haerong22@naver.com>
2024-06-27 22:05:07 +0900

    #53 payment system: 메시지 consume 테스트 코드 삭제

--- payment-system/paymentservice/src/main/kotlin/org/example/paymentservice/PaymentserviceApplication.kt
+++ payment-system/paymentservice/src/main/kotlin/org/example/paymentservice/PaymentserviceApplication.kt
@@ -2,27 +2,11 @@ package org.example.paymentservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.messaging.Message
 import org.springframework.scheduling.annotation.EnableScheduling
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
-import java.util.function.Function
 
 @EnableScheduling
 @SpringBootApplication
-class PaymentserviceApplication {
-
-    @Bean
-    fun consume(): Function<Flux<Message<String>>, Mono<Void>> {
-        return Function { message ->
-            message.map {
-                println(it)
-                it
-            }.then()
-        }
-    }
-}
+class PaymentserviceApplication
 
 fun main(args: Array<String>) {
     runApplication<PaymentserviceApplication>(*args)
