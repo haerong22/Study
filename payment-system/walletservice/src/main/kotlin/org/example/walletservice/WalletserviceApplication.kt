6f9fb4db8a574c625ddc0bb28a0da04038b7a37f
haerong22 <haerong22@naver.com>
2024-07-01 17:28:36 +0900

    #53 payment system: wallet service - init

---
+++ payment-system/walletservice/src/main/kotlin/org/example/walletservice/WalletserviceApplication.kt
@@ -0,0 +1,11 @@
+package org.example.walletservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WalletserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<WalletserviceApplication>(*args)
+}
---
+++ payment-system/walletservice/src/test/kotlin/org/example/walletservice/WalletserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.walletservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WalletserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
