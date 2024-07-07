6254f46192326a3534c3cdfd94d00e3671964783
haerong22 <haerong22@naver.com>
2024-07-07 19:32:30 +0900

    #53 payment system: ledger service - init(dependency)

---
+++ payment-system/ledgerservice/src/main/kotlin/org/example/ledgerservice/LedgerserviceApplication.kt
@@ -0,0 +1,11 @@
+package org.example.ledgerservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LedgerserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<LedgerserviceApplication>(*args)
+}
---
+++ payment-system/ledgerservice/src/test/kotlin/org/example/ledgerservice/LedgerserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.ledgerservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LedgerserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
