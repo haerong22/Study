0b68d004b3abe3f785a0bdf003c84837efd4331c
haerong22 <haerong22@naver.com>
2024-07-10 16:01:14 +0900

    #53 payment system: ledger service - payment event message handler(kafka)

--- payment-system/ledgerservice/src/main/kotlin/org/example/ledgerservice/ledger/adapter/in/stream/PaymentEventMessageHandler.kt
+++ payment-system/ledgerservice/src/main/kotlin/org/example/ledgerservice/ledger/adapter/in/stream/PaymentEventMessageHandler.kt
@@ -1,7 +1,9 @@
 package org.example.ledgerservice.ledger.adapter.`in`.stream
 
 import org.example.ledgerservice.common.StreamAdapter
+import org.example.ledgerservice.ledger.application.port.`in`.DoubleLedgerEntryRecordUseCase
 import org.example.ledgerservice.ledger.domain.PaymentEventMessage
+import org.springframework.cloud.stream.function.StreamBridge
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.Message
@@ -9,12 +11,17 @@ import java.util.function.Consumer
 
 @Configuration
 @StreamAdapter
-class PaymentEventMessageHandler {
+class PaymentEventMessageHandler(
+    private val doubleLedgerEntryRecordUseCase: DoubleLedgerEntryRecordUseCase,
+    private val streamBridge: StreamBridge,
+) {
 
     @Bean
     fun consume(): Consumer<Message<PaymentEventMessage>> {
         return Consumer { message ->
             println(message.payload)
+            val ledgerEventMessage = doubleLedgerEntryRecordUseCase.recordDoubleLedgerEntry(message.payload)
+            streamBridge.send("ledger", ledgerEventMessage)
         }
     }
 }
