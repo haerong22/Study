82939aa09ad2cbe4370b4de8e63f44c6384e89a7
haerong22 <haerong22@naver.com>
2024-07-15 19:49:32 +0900

    #54 gRPC: client - init

---
+++ api-protocol/grpc/demo/grpc-client/src/test/java/org/example/grpcclient/GrpcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.grpcclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
