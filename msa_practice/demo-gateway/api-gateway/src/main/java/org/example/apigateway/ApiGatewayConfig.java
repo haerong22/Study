3d90733697313c1b9bb11d57181342a01e121a13
haerong22 <haerong22@naver.com>
2024-07-24 19:52:16 +0900

    #55 api gateway: 라우팅 설정

---
+++ msa_practice/demo-gateway/api-gateway/src/main/java/org/example/apigateway/ApiGatewayConfig.java
@@ -0,0 +1,21 @@
+package org.example.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r
+                        .path("/bobby/**")
+                        .filters(f -> f.rewritePath("/bobby/(?<segment>.*)", "/"))
+                        .uri("https://google.com")
+                )
+                .build();
+    }
+}
