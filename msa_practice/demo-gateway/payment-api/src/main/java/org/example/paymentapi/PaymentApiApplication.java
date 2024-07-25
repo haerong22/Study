ab082a19a8d4cc176475e1b9124b39fb2cf58641
haerong22 <haerong22@naver.com>
2024-07-25 12:40:44 +0900

    #55 api gateway: eureka 적용

--- msa_practice/demo-gateway/payment-api/src/main/java/org/example/paymentapi/PaymentApiApplication.java
+++ msa_practice/demo-gateway/payment-api/src/main/java/org/example/paymentapi/PaymentApiApplication.java
@@ -2,7 +2,9 @@ package org.example.paymentapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PaymentApiApplication {
 
