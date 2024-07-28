6113bc177cbf3ea3ee1b567be71064ca69451d2a
haerong22 <haerong22@naver.com>
2024-07-28 19:27:31 +0900

    #56 e-leaning service: course - init

---
+++ e-learning-service/els-course/src/main/java/org/example/elscourse/ElsCourseApplication.java
@@ -0,0 +1,13 @@
+package org.example.elscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsCourseApplication.class, args);
+	}
+
+}
---
+++ e-learning-service/els-course/src/test/java/org/example/elscourse/ElsCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.elscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
