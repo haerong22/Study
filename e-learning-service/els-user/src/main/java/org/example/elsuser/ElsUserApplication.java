91dbc939310a5600b636f22c66ea034525b60407
haerong22 <haerong22@naver.com>
2024-07-29 19:06:41 +0900

    #56 e-leaning service: user - init

---
+++ e-learning-service/els-user/src/main/java/org/example/elsuser/ElsUserApplication.java
@@ -0,0 +1,13 @@
+package org.example.elsuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsUserApplication.class, args);
+	}
+
+}
---
+++ e-learning-service/els-user/src/test/java/org/example/elsuser/ElsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.elsuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
