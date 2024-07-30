2c7950e585af00d24796b659c245d604bd85b5e5
haerong22 <haerong22@naver.com>
2024-07-30 18:58:10 +0900

    #56 e-leaning service: file manage - init

---
+++ e-learning-service/els-file-manage/src/main/java/org/example/elsfilemanage/ElsFileManageApplication.java
@@ -0,0 +1,13 @@
+package org.example.elsfilemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsFileManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsFileManageApplication.class, args);
+	}
+
+}
