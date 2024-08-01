4f430befcc7d685d264762464d38f9a56607c91e
haerong22 <haerong22@naver.com>
2024-08-01 19:34:11 +0900

    #56 e-leaning service: playback - init

---
+++ e-learning-service/els-playback/src/main/java/org/example/elsplayback/ElsPlaybackApplication.java
@@ -0,0 +1,13 @@
+package org.example.elsplayback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsPlaybackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsPlaybackApplication.class, args);
+	}
+
+}
---
+++ e-learning-service/els-playback/src/test/java/org/example/elsplayback/ElsPlaybackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.elsplayback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsPlaybackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
