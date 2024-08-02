d4d2b2e2d696d5d25ff2e21b93e11e106a49b5ca
haerong22 <haerong22@naver.com>
2024-08-02 16:53:47 +0900

    #56 e-leaning service: enrollment - init

---
+++ e-learning-service/els-enrollment/src/main/java/org/example/elsenrollment/ElsEnrollmentApplication.java
@@ -0,0 +1,13 @@
+package org.example.elsenrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsEnrollmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsEnrollmentApplication.class, args);
+	}
+
+}
---
+++ e-learning-service/els-enrollment/src/test/java/org/example/elsenrollment/ElsEnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.elsenrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsEnrollmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
