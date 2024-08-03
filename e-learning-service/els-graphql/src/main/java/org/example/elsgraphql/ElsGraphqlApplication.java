c1ddffa08a49019946bc2a8ba8246cab03f9624c
haerong22 <haerong22@naver.com>
2024-08-03 16:19:24 +0900

    #56 e-leaning service: graphql - init

---
+++ e-learning-service/els-graphql/src/main/java/org/example/elsgraphql/ElsGraphqlApplication.java
@@ -0,0 +1,13 @@
+package org.example.elsgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsGraphqlApplication.class, args);
+	}
+
+}
