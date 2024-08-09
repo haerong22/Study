bd3fe66d8879cff8aeb26846f7b88c20d36ece44
haerong22 <haerong22@naver.com>
2024-08-09 16:33:30 +0900

    #56 e-leaning service: gateway - init

---
+++ e-learning-service/els-gateway/src/main/java/org/example/elsgateway/ElsGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.example.elsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsGatewayApplication.class, args);
+	}
+
+}
---
+++ e-learning-service/els-gateway/src/test/java/org/example/elsgateway/ElsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.elsgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
