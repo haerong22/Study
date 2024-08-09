c8df680b9dde20d7b25139c15ddbc17d427db624
haerong22 <haerong22@naver.com>
2024-08-09 16:00:59 +0900

    #56 e-leaning service: graphql - 최적화(쿼리 제한)

--- e-learning-service/els-graphql/src/main/java/org/example/elsgraphql/config/GraphqlConfig.java
+++ e-learning-service/els-graphql/src/main/java/org/example/elsgraphql/config/GraphqlConfig.java
@@ -1,11 +1,21 @@
 package org.example.elsgraphql.config;
 
+import graphql.analysis.FieldComplexityCalculator;
+import graphql.analysis.FieldComplexityEnvironment;
+import graphql.analysis.MaxQueryComplexityInstrumentation;
+import graphql.analysis.MaxQueryDepthInstrumentation;
+import graphql.execution.instrumentation.ChainedInstrumentation;
+import graphql.execution.instrumentation.Instrumentation;
 import graphql.scalars.ExtendedScalars;
+import graphql.schema.GraphQLFieldDefinition;
+import lombok.extern.slf4j.Slf4j;
 import org.example.elsgraphql.directive.AuthenticationDirective;
 import org.example.elsgraphql.directive.AuthorizationDirective;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class GraphqlConfig {
@@ -22,5 +32,31 @@ public class GraphqlConfig {
                 .directive("authenticate", authenticationDirective)
                 .directive("authorize", authorizationDirective)
                 ;
+    }
+
+    @Bean
+    public Instrumentation maxQueryComplexityInstrumentation() {
+        MaxQueryComplexityInstrumentation complexityInstrumentation = new MaxQueryComplexityInstrumentation(100, new CustomFieldComplexityCalculator());
+        return new ChainedInstrumentation(List.of(complexityInstrumentation));
+    }
+
+    @Bean
+    public Instrumentation maxQueryDepthInstrumentation() {
+        return new MaxQueryDepthInstrumentation(20);
+    }
+
+    @Slf4j
+    public static class CustomFieldComplexityCalculator implements FieldComplexityCalculator {
+        @Override
+        public int calculate(FieldComplexityEnvironment environment, int childComplexity) {
+            GraphQLFieldDefinition fieldDefinition = environment.getFieldDefinition();
+            String fieldName = fieldDefinition.getName();
+            log.info("fieldName: {}, childComplexity : {}", fieldName, childComplexity);
+//            if ("expensiveField".equals(fieldName)) {
+//                return childComplexity + 10; // 더 복잡한 필드에 더 높은 점수 부여
+//            }
+
+            return childComplexity + 1;
+        }
     }
 }
