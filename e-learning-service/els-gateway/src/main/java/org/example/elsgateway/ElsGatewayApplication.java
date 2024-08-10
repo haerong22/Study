25821f251cb967c743d084946a91b53412a05c35
haerong22 <haerong22@naver.com>
2024-08-10 16:13:51 +0900

    #56 e-leaning service: gateway - route setting

--- e-learning-service/els-gateway/src/main/java/org/example/elsgateway/ElsGatewayApplication.java
+++ e-learning-service/els-gateway/src/main/java/org/example/elsgateway/ElsGatewayApplication.java
@@ -2,8 +2,10 @@ package org.example.elsgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ElsGatewayApplication {
 
 	public static void main(String[] args) {
