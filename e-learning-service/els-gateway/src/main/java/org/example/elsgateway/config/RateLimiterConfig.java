0f985556352b1dec47267a93fb1a8db666029ee1
haerong22 <haerong22@naver.com>
2024-08-10 18:46:15 +0900

    #56 e-leaning service: gateway - rate limiter, time limiter

---
+++ e-learning-service/els-gateway/src/main/java/org/example/elsgateway/config/RateLimiterConfig.java
@@ -0,0 +1,19 @@
+package org.example.elsgateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Configuration
+public class RateLimiterConfig {
+
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(
+                Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress()
+        );
+    }
+}
