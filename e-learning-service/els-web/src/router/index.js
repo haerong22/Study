d3f013535ddfb9e457f9a5bc8b3f47a01874bd3b
haerong22 <haerong22@naver.com>
2024-08-21 19:45:14 +0900

    #56 e-leaning service: web - course detail page

--- e-learning-service/els-web/src/router/index.js
+++ e-learning-service/els-web/src/router/index.js
@@ -2,10 +2,12 @@ import {createRouter, createWebHistory} from 'vue-router'
 import LoginPage from "@/pages/LoginPage.vue";
 import CoursePage from "@/pages/CoursePage.vue";
 import PurchasePage from "@/pages/PurchasePage.vue";
+import CourseDetailPage from "@/pages/CourseDetailPage.vue";
 
 const routes = [
     {path: '/', component: LoginPage},
     {path: '/courses', component: CoursePage},
+    {path: '/course/:id', component: CourseDetailPage},
     {path: '/purchase/:id', component: PurchasePage},
 ]
 
