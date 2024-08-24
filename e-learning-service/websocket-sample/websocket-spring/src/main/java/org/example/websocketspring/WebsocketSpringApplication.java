43eb684d07f44acca67683b69e1366d21f8bb0d3
haerong22 <haerong22@naver.com>
2024-08-24 18:52:42 +0900

    #56 e-leaning service: websocket sample with spring - init

---
+++ e-learning-service/websocket-sample/websocket-spring/src/main/java/org/example/websocketspring/WebsocketSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.websocketspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketSpringApplication.class, args);
+	}
+
+}
---
+++ e-learning-service/websocket-sample/websocket-spring/src/test/java/org/example/websocketspring/WebsocketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.websocketspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
