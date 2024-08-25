810e4167199eb70c297a4d2209e9dd15f0d447a7
haerong22 <haerong22@naver.com>
2024-08-25 20:53:36 +0900

    #56 e-leaning service: websocket sample(graphql) - init

---
+++ e-learning-service/websocket-sample/websocket-spring-graphql/src/main/java/org/example/websocketspringgraphql/WebsocketSpringGraphqlApplication.java
@@ -0,0 +1,13 @@
+package org.example.websocketspringgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketSpringGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketSpringGraphqlApplication.class, args);
+	}
+
+}
---
+++ e-learning-service/websocket-sample/websocket-spring-graphql/src/test/java/org/example/websocketspringgraphql/WebsocketSpringGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.websocketspringgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketSpringGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
