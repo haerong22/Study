50d965cd3ca90085f0c301d2bd60e1b127302813
haerong22 <haerong22@naver.com>
2024-08-26 14:19:44 +0900

    #56 e-leaning service: chat - init

---
+++ e-learning-service/els-chat/src/main/java/org/example/elschat/ElsChatApplication.java
@@ -0,0 +1,13 @@
+package org.example.elschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElsChatApplication.class, args);
+    }
+
+}
---
+++ e-learning-service/els-chat/src/test/java/org/example/elschat/ElsChatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.elschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
