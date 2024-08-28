11a6d16025ac56a9eb8ca808fd5e9443a02a81f0
haerong22 <haerong22@naver.com>
2024-08-28 17:22:39 +0900

    #56 e-leaning service: docs - schema

---
+++ e-learning-service/docs/schema/enrollment-service-schema.sql
@@ -0,0 +1,32 @@
+-- 결제 정보를 저장하는 테이블
+CREATE TABLE payments
+(
+    id     INT AUTO_INCREMENT PRIMARY KEY,
+    user_id        INT                                 NOT NULL,
+    payment_type   ENUM ('COURSE', 'SUBSCRIPTION')     NOT NULL, -- 결제 유형 구분
+    amount         DECIMAL(10, 2)                      NOT NULL,
+    payment_method VARCHAR(50)                         NOT NULL,
+    payment_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+-- 강의 등록 정보를 저장하는 테이블
+CREATE TABLE enrollments
+(
+    id     INT AUTO_INCREMENT PRIMARY KEY,
+    user_id           INT                                 NOT NULL,
+    course_id         INT                                 NOT NULL,
+    payment_id        INT                                 NOT NULL,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (payment_id) REFERENCES payments (id)
+);
+
+-- 사용자의 구독 정보를 저장하는 테이블
+CREATE TABLE subscriptions
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id         INT                                 NOT NULL,
+    payment_id      INT                                 NOT NULL,
+    start_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    end_date        TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (payment_id) REFERENCES payments (id)
+);
