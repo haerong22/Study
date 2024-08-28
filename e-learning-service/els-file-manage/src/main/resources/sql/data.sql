ba83c86d0f41ea670195b49e0e9215848e1ce89c
haerong22 <haerong22@naver.com>
2024-08-28 17:25:17 +0900

    #56 e-leaning service: setup

--- e-learning-service/els-file-manage/src/main/resources/sql/data.sql
+++ e-learning-service/els-file-manage/src/main/resources/sql/data.sql
@@ -3,4 +3,4 @@ VALUES (100, '1714564219627_intro_to_python.mp4', 'mp4', './uploads/171456421962
        (200, 'intro_to_java.mp4', 'mp4', './uploads/1714564219627_intro_to_python.mp4', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        (200, 'java_concurrency.mp4', 'mp4', './uploads/1714564219627_intro_to_python.mp4', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        (300, 'data_structures.mp4', 'mp4', './uploads/1714564219627_intro_to_python.mp4', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       (300, 'algorithms.mp4', 'mp4', './uploads/1714564219627_intro_to_python.mp4', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+       (400, 'algorithms.mp4', 'mp4', './uploads/1714564219627_intro_to_python.mp4', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
