6d4b1450f53d776aa0b5107ad6aead02632d49d9
haerong22 <haerong22@naver.com>
2024-08-29 21:28:26 +0900

    #57 custom spring boot starter : handgame spring boot app

---
+++ spring/custom-spring-boot-starter/handgame-spring-boot-app/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+
+    implementation("org.example:handgame-spring-boot-starter:1.0-SNAPSHOT")
+}
--- spring/custom-spring-boot-starter/settings.gradle.kts
+++ spring/custom-spring-boot-starter/settings.gradle.kts
@@ -3,5 +3,6 @@ plugins {
 }
 rootProject.name = "custom-spring-boot-starter"
 include("handgame")
+include("handgame-spring-boot-app")
 include("handgame-spring-boot-autoconfigure")
 include("handgame-spring-boot-starter")
