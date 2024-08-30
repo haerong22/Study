71041954c7605924e6593ab67fea4fc43bc0dd34
haerong22 <haerong22@naver.com>
2024-08-30 17:55:36 +0900

    #58 spring refactoring(java to kotlin) : init

---
+++ spring/todo-java-to-kotlin/src/main/java/org/example/todojavatokotlin/TodoJavaToKotlinApplication.java
@@ -0,0 +1,13 @@
+package org.example.todojavatokotlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJavaToKotlinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoJavaToKotlinApplication.class, args);
+    }
+
+}
---
+++ spring/todo-java-to-kotlin/src/test/java/org/example/todojavatokotlin/TodoJavaToKotlinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todojavatokotlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoJavaToKotlinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
