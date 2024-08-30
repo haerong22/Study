420dd7b08e051210d3b2febd7d592a77b24a8dfc
haerong22 <haerong22@naver.com>
2024-08-30 21:55:16 +0900

    #58 spring refactoring(java to kotlin) : 컨트롤러 레이어 리팩토링

---
+++ spring/todo-java-to-kotlin/src/main/kotlin/org/example/todojavatokotlin/api/TodoController.kt
@@ -0,0 +1,52 @@
+package org.example.todojavatokotlin.api
+
+import org.example.todojavatokotlin.api.model.TodoListResponse
+import org.example.todojavatokotlin.api.model.TodoRequest
+import org.example.todojavatokotlin.api.model.TodoResponse
+import org.example.todojavatokotlin.service.TodoService
+import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.noContent
+import org.springframework.http.ResponseEntity.ok
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/todos")
+class TodoController(
+    private val todoService: TodoService,
+) {
+
+    @GetMapping
+    fun getAll() =
+        ok(TodoListResponse.of(todoService.findAll()))
+
+    @GetMapping("/{id}")
+    fun get(
+        @PathVariable id: Long,
+    ) = ok(TodoResponse.of(todoService.findById(id)))
+
+    @PostMapping
+    fun create(
+        @RequestBody request: TodoRequest,
+    ) = ok(TodoResponse.of(todoService.create(request)))
+
+    @PutMapping("/{id}")
+    fun update(
+        @PathVariable id: Long,
+        @RequestBody request: TodoRequest,
+    ) = ok(TodoResponse.of(todoService.update(id, request)))
+
+    @DeleteMapping("/{id}")
+    fun delete(
+        @PathVariable id: Long,
+    ): ResponseEntity<Unit> {
+        todoService.delete(id)
+        return noContent().build()
+    }
+}
