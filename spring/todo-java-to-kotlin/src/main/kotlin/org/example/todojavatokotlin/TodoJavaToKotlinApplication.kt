f5cfc999ced1985cec07a4aa22706100c0caa382
haerong22 <haerong22@naver.com>
2024-08-31 18:12:28 +0900

    #58 spring refactoring(java to kotlin) : 리팩토링

---
+++ spring/todo-java-to-kotlin/src/main/kotlin/org/example/todojavatokotlin/TodoJavaToKotlinApplication.kt
@@ -0,0 +1,11 @@
+package org.example.todojavatokotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoJavaToKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<TodoJavaToKotlinApplication>(*args)
+}
