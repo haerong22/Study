5854b65188376b25d91a6014880ea0ed6e5924c4
haerong22 <haerong22@naver.com>
2024-09-01 21:08:47 +0900

    #59 issue service: issue - 이슈 상세 조회 API

--- spring/issue-service/issue/src/main/kotlin/org/example/issueservice/service/IssueService.kt
+++ spring/issue-service/issue/src/main/kotlin/org/example/issueservice/service/IssueService.kt
@@ -3,8 +3,10 @@ package org.example.issueservice.service
 import org.example.issueservice.domain.Issue
 import org.example.issueservice.domain.IssueRepository
 import org.example.issueservice.domain.enums.IssueStatus
+import org.example.issueservice.exception.NotFoundException
 import org.example.issueservice.model.IssueRequest
 import org.example.issueservice.model.IssueResponse
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -32,4 +34,10 @@ class IssueService(
     fun getAll(status: IssueStatus) =
         issueRepository.findAllByStatusOrderByCreatedAtDesc(status)
             .map { IssueResponse(it) }
+
+    @Transactional(readOnly = true)
+    fun get(issueId: Long): IssueResponse =
+        issueRepository.findByIdOrNull(issueId)
+            ?.let { IssueResponse(it) }
+            ?: throw NotFoundException("이슈가 존재하지 않습니다.")
 }
