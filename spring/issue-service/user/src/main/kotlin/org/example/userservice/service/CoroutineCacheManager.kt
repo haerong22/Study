5119126b4bc48ddc947ec30cec5087162dac5fe3
haerong22 <haerong22@naver.com>
2024-09-04 17:28:10 +0900

    #59 issue service: user - cache manager

---
+++ spring/issue-service/user/src/main/kotlin/org/example/userservice/service/CoroutineCacheManager.kt
@@ -0,0 +1,22 @@
+package org.example.userservice.service
+
+import org.springframework.stereotype.Component
+import java.time.Duration
+import java.time.Instant
+import java.util.concurrent.ConcurrentHashMap
+
+@Component
+class CoroutineCacheManager<T> {
+
+    private val localCache = ConcurrentHashMap<String, CacheWrapper<T>>()
+
+    suspend fun awaitPut(key: String, value: T, ttl: Duration) {
+        localCache[key] = CacheWrapper(cached = value, Instant.now().plusMillis(ttl.toMillis()))
+    }
+
+    suspend fun awaitEvict(key: Any) {
+        localCache.remove(key)
+    }
+
+    data class CacheWrapper<T>(val cached: T, val ttl: Instant)
+}
