255e9e53f73d0196432a208ef82e4fc774f9132b
haerong22 <haerong22@naver.com>
2024-09-04 23:14:27 +0900

    #59 issue service: user - 리포터 조회 API

--- spring/issue-service/user/src/main/kotlin/org/example/userservice/controller/UserController.kt
+++ spring/issue-service/user/src/main/kotlin/org/example/userservice/controller/UserController.kt
@@ -9,6 +9,7 @@ import org.example.userservice.service.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -48,4 +49,10 @@ class UserController(
         @AuthToken token: String,
     ) : MeResponse =
         MeResponse(userService.getByToken(token))
+
+    @GetMapping("/{userId}/username")
+    suspend fun getUsername(
+        @PathVariable userId: Long,
+    ) : Map<String, String> =
+        mapOf("reporter" to userService.get(userId).username)
 }
