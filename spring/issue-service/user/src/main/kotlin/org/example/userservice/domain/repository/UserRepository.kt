8c853a5598a1479d2e9a7b2ab2377c0738c66b23
haerong22 <haerong22@naver.com>
2024-09-04 15:38:17 +0900

    #59 issue service: user - user 도메인 정의

---
+++ spring/issue-service/user/src/main/kotlin/org/example/userservice/domain/repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package org.example.userservice.domain.repository
+
+import org.example.userservice.domain.entity.User
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+
+interface UserRepository : CoroutineCrudRepository<User, Long> {
+
+    suspend fun findByEmail(email: String): User?
+}
