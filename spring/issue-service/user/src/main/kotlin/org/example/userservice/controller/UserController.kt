26ac589efddb510815b8f9f43ae6a5e6f2f7f983
haerong22 <haerong22@naver.com>
2024-09-04 17:28:55 +0900

    #59 issue service: user - 로그인/로그아웃 API

--- spring/issue-service/user/src/main/kotlin/org/example/userservice/controller/UserController.kt
+++ spring/issue-service/user/src/main/kotlin/org/example/userservice/controller/UserController.kt
@@ -1,10 +1,16 @@
 package org.example.userservice.controller
 
+import org.example.userservice.model.AuthToken
+import org.example.userservice.model.SignInRequest
+import org.example.userservice.model.SignInResponse
 import org.example.userservice.model.SignUpRequest
 import org.example.userservice.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -18,5 +24,20 @@ class UserController(
         @RequestBody request: SignUpRequest,
     ) {
         userService.signUp(request)
+    }
+
+    @PostMapping("/signin")
+    suspend fun signIn(
+        @RequestBody signInRequest: SignInRequest,
+    ) : SignInResponse {
+        return userService.signIn(signInRequest)
+    }
+
+    @DeleteMapping("/logout")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    suspend fun logout(
+        @AuthToken token: String,
+    ) {
+        userService.logout(token)
     }
 }
--- spring/issue-service/user/src/main/kotlin/org/example/userservice/service/UserService.kt
+++ spring/issue-service/user/src/main/kotlin/org/example/userservice/service/UserService.kt
@@ -1,16 +1,30 @@
 package org.example.userservice.service
 
+import org.example.userservice.config.JWTProperties
 import org.example.userservice.domain.entity.User
 import org.example.userservice.domain.repository.UserRepository
+import org.example.userservice.exception.PasswordNotMatchedException
 import org.example.userservice.exception.UserExistsException
+import org.example.userservice.exception.UserNotFoundException
+import org.example.userservice.model.SignInRequest
+import org.example.userservice.model.SignInResponse
 import org.example.userservice.model.SignUpRequest
 import org.example.userservice.utils.BCryptUtils
+import org.example.userservice.utils.JWTClaim
+import org.example.userservice.utils.JWTUtils
 import org.springframework.stereotype.Service
+import java.time.Duration
 
 @Service
 class UserService(
     private val userRepository: UserRepository,
+    private val jwtProperties: JWTProperties,
+    private val cacheManager: CoroutineCacheManager<User>,
 ) {
+
+    companion object {
+        private val CACHE_TTL = Duration.ofMinutes(1)
+    }
 
     suspend fun signUp(signUpRequest: SignUpRequest) {
 
@@ -27,5 +41,34 @@ class UserService(
 
             userRepository.save(user)
         }
+    }
+
+    suspend fun signIn(signInRequest: SignInRequest): SignInResponse {
+        return with(userRepository.findByEmail(signInRequest.email) ?: throw UserNotFoundException()) {
+            val verified = BCryptUtils.verify(signInRequest.password, password)
+
+            if (!verified) throw PasswordNotMatchedException()
+
+            val jwtClaim = JWTClaim(
+                userId = id!!,
+                email = email,
+                profileUrl = profileUrl,
+                username = username,
+            )
+
+            val token = JWTUtils.createToken(jwtClaim, jwtProperties)
+
+            cacheManager.awaitPut(key = token, value = this, ttl = CACHE_TTL)
+
+            SignInResponse(
+                email = email,
+                username = username,
+                token = token,
+            )
+        }
+    }
+
+    suspend fun logout(token: String) {
+        cacheManager.awaitEvict(token)
     }
 }
