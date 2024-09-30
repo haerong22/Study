e3c2ee9f09f370e1e3d7ccab95248b7b9fab9407
haerong22 <haerong22@naver.com>
2024-09-30 14:32:07 +0900

    #61 java(thread): interrupt ex - printer

---
+++ java/thread/src/thread/control/printer/MyPrinterV1.java
@@ -0,0 +1,52 @@
+package thread.control.printer;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class MyPrinterV1 {
+
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Thread printerThread = new Thread(printer, "printer");
+        printerThread.start();
+
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            log("프린터할 문서를 입력하세요. 종료 (q): ");
+            String input = userInput.nextLine();
+            if (input.equals("q")) {
+                printer.work = false;
+                break;
+            }
+            printer.addJob(input);
+        }
+    }
+
+    static class Printer implements Runnable {
+
+        volatile boolean work = true;
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+
+            while (work) {
+                if (jobQueue.isEmpty()) continue;
+
+                String job = jobQueue.poll();
+                log("출력 시작: " + job + ", 대기문서: " + jobQueue);
+                sleep(3000);
+                log("출력 완료");
+            }
+            log("프린터 종료");
+        }
+
+        public void addJob(String input) {
+            jobQueue.offer(input);
+        }
+    }
+}
---
+++ java/thread/src/thread/control/printer/MyPrinterV3.java
@@ -0,0 +1,54 @@
+package thread.control.printer;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static util.MyLogger.log;
+
+public class MyPrinterV3 {
+
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Thread printerThread = new Thread(printer, "printer");
+        printerThread.start();
+
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            log("프린터할 문서를 입력하세요. 종료 (q): ");
+            String input = userInput.nextLine();
+            if (input.equals("q")) {
+                printerThread.interrupt();
+                break;
+            }
+            printer.addJob(input);
+        }
+    }
+
+    static class Printer implements Runnable {
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+
+            while (!Thread.interrupted()) {
+                if (jobQueue.isEmpty()) continue;
+
+                try {
+                    String job = jobQueue.poll();
+                    log("출력 시작: " + job + ", 대기문서: " + jobQueue);
+                    Thread.sleep(3000);
+                    log("출력 완료");
+                } catch (InterruptedException e) {
+                    log("인터럽트!");
+                    break;
+                }
+            }
+            log("프린터 종료");
+        }
+
+        public void addJob(String input) {
+            jobQueue.offer(input);
+        }
+    }
+}
