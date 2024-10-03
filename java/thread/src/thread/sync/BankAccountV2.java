f373ca9cf826d192bab68d6f628be1e447cc92e6
haerong22 <haerong22@naver.com>
2024-10-03 16:37:33 +0900

    #61 java(thread): 동기화 - synchronized

---
+++ java/thread/src/thread/sync/BankAccountV2.java
@@ -0,0 +1,41 @@
+package thread.sync;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV2 implements BankAccount {
+
+    private int balance;
+
+    public BankAccountV2(int initBalance) {
+        this.balance = initBalance;
+    }
+
+    @Override
+    public synchronized boolean withdraw(int amount) {
+        log("거래 시작: " + getClass().getSimpleName());
+
+        // === 임계영역 시작 ===
+        // 검증
+        log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+        if (balance < amount) {
+            log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+            return false;
+        }
+
+        // 출금
+        log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+        sleep(1000);
+        balance = balance - amount;
+        log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+        // === 임계영역 종료 ===
+
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public synchronized int getBalance() {
+        return balance;
+    }
+}
--- java/thread/src/thread/sync/BankMain.java
+++ java/thread/src/thread/sync/BankMain.java
@@ -6,7 +6,9 @@ import static util.ThreadUtils.sleep;
 public class BankMain {
 
     public static void main(String[] args) throws InterruptedException {
-        BankAccount account = new BankAccountV1(1000);
+//        BankAccount account = new BankAccountV1(1000);
+//        BankAccount account = new BankAccountV2(1000);
+        BankAccount account = new BankAccountV3(1000);
 
         Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
         Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
@@ -16,7 +18,7 @@ public class BankMain {
 
         sleep(500); // 검증 까지 잠시 대기
         log("t1 state: " + t1.getState());
-        log("t2 state: " + t1.getState());
+        log("t2 state: " + t2.getState());
 
         t1.join();
         t2.join();
@@ -40,5 +42,33 @@ public class BankMain {
         2024-10-03 15:23:25.022 [       t1] 거래 종료
         2024-10-03 15:23:25.022 [       t2] 거래 종료
         2024-10-03 15:23:25.025 [     main] 최종 잔액: -600
+
+        BankAccountV2
+
+        2024-10-03 16:19:33.582 [       t2] 거래 시작: BankAccountV2
+        2024-10-03 16:19:33.587 [       t2] [검증 시작] 출금액: 800, 잔액: 1000
+        2024-10-03 16:19:33.587 [       t2] [검증 완료] 출금액: 800, 잔액: 1000
+        2024-10-03 16:19:34.071 [     main] t1 state: BLOCKED
+        2024-10-03 16:19:34.071 [     main] t2 state: TIMED_WAITING
+        2024-10-03 16:19:34.593 [       t2] [출금 완료] 출금액: 800, 잔액: 200
+        2024-10-03 16:19:34.594 [       t2] 거래 종료
+        2024-10-03 16:19:34.596 [       t1] 거래 시작: BankAccountV2
+        2024-10-03 16:19:34.597 [       t1] [검증 시작] 출금액: 800, 잔액: 200
+        2024-10-03 16:19:34.597 [       t1] [검증 실패] 출금액: 800, 잔액: 200
+        2024-10-03 16:19:34.601 [     main] 최종 잔액: 200
+
+        BankAccountV3
+
+        2024-10-03 16:26:49.337 [       t2] 거래 시작: BankAccountV3
+        2024-10-03 16:26:49.337 [       t1] 거래 시작: BankAccountV3
+        2024-10-03 16:26:49.343 [       t2] [검증 시작] 출금액: 800, 잔액: 1000
+        2024-10-03 16:26:49.343 [       t2] [검증 완료] 출금액: 800, 잔액: 1000
+        2024-10-03 16:26:49.827 [     main] t1 state: BLOCKED
+        2024-10-03 16:26:49.827 [     main] t2 state: TIMED_WAITING
+        2024-10-03 16:26:50.347 [       t2] [출금 완료] 출금액: 800, 잔액: 200
+        2024-10-03 16:26:50.350 [       t2] 거래 종료
+        2024-10-03 16:26:50.351 [       t1] [검증 시작] 출금액: 800, 잔액: 200
+        2024-10-03 16:26:50.352 [       t1] [검증 실패] 출금액: 800, 잔액: 200
+        2024-10-03 16:26:50.357 [     main] 최종 잔액: 200
      */
 }
