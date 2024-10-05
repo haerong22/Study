de11e4012f82a24fc55e4a58f8bdae4ff6dda66f
haerong22 <haerong22@naver.com>
2024-10-05 17:28:30 +0900

    #61 java(thread): 생산자/소비자 - retry(sleep)

--- java/thread/src/thread/bounded/BoundedMain.java
+++ java/thread/src/thread/bounded/BoundedMain.java
@@ -10,11 +10,12 @@ public class BoundedMain {
 
     public static void main(String[] args) {
         // 1. BoundedQueue 선택
-        BoundedQueue queue = new BoundedQueueV1(2);
+//        BoundedQueue queue = new BoundedQueueV1(2);
+        BoundedQueue queue = new BoundedQueueV2(2);
 
         // 2. 생산자, 소비자 실행 순서 선택
-        producerFirst(queue);
-//        consumerFirst(queue);
+//        producerFirst(queue);
+        consumerFirst(queue);
     }
 
     /*
@@ -87,6 +88,78 @@ public class BoundedMain {
         2024-10-05 16:56:50.129 [     main] producer2: TERMINATED
         2024-10-05 16:56:50.129 [     main] producer3: TERMINATED
         2024-10-05 16:56:50.130 [     main] == [소비자 먼저 실행] 종료, BoundedQueueV1 ==
+
+        BoundedQueueV2 - producerFirst
+
+        2024-10-05 17:11:00.253 [     main] == [생산자 먼저 실행] 시작, BoundedQueueV2 ==
+
+        2024-10-05 17:11:00.253 [     main] 생산자 시작
+        2024-10-05 17:11:00.260 [producer1] [생산 시도]data1 -> []
+        2024-10-05 17:11:00.260 [producer1] [생산 완료]data1 -> [data1]
+        2024-10-05 17:11:00.360 [producer2] [생산 시도]data2 -> [data1]
+        2024-10-05 17:11:00.360 [producer2] [생산 완료]data2 -> [data1, data2]
+        2024-10-05 17:11:00.462 [producer3] [생산 시도]data3 -> [data1, data2]
+        2024-10-05 17:11:00.462 [producer3] [put] 큐가 가득 참, 생산자 대기
+
+        2024-10-05 17:11:00.566 [     main] 현재 상태 출력, 큐 데이터: [data1, data2]
+        2024-10-05 17:11:00.566 [     main] producer1: TERMINATED
+        2024-10-05 17:11:00.567 [     main] producer2: TERMINATED
+        2024-10-05 17:11:00.567 [     main] producer3: TIMED_WAITING
+
+        2024-10-05 17:11:00.567 [     main] 소비자 시작
+        2024-10-05 17:11:00.568 [consumer1] [소비 시도]     ? <- [data1, data2]
+        2024-10-05 17:11:00.673 [consumer2] [소비 시도]     ? <- [data1, data2]
+        2024-10-05 17:11:00.777 [consumer3] [소비 시도]     ? <- [data1, data2]
+
+        2024-10-05 17:11:00.883 [     main] 현재 상태 출력, 큐 데이터: [data1, data2]
+        2024-10-05 17:11:00.883 [     main] producer1: TERMINATED
+        2024-10-05 17:11:00.884 [     main] producer2: TERMINATED
+        2024-10-05 17:11:00.884 [     main] producer3: TIMED_WAITING
+        2024-10-05 17:11:00.884 [     main] consumer1: BLOCKED
+        2024-10-05 17:11:00.884 [     main] consumer2: BLOCKED
+        2024-10-05 17:11:00.885 [     main] consumer3: BLOCKED
+        2024-10-05 17:11:00.885 [     main] == [생산자 먼저 실행] 종료, BoundedQueueV2 ==
+        2024-10-05 17:11:01.468 [producer3] [put] 큐가 가득 참, 생산자 대기
+        2024-10-05 17:11:02.472 [producer3] [put] 큐가 가득 참, 생산자 대기
+        2024-10-05 17:11:03.477 [producer3] [put] 큐가 가득 참, 생산자 대기
+
+        ... (종료 안됨 - Lock 을 가진 상태로 대기중)
+
+        BoundedQueueV2 - consumerFirst
+
+        2024-10-05 17:15:05.653 [     main] == [소비자 먼저 실행] 시작, BoundedQueueV2 ==
+
+        2024-10-05 17:15:05.653 [     main] 소비자 시작
+        2024-10-05 17:15:05.656 [consumer1] [소비 시도]     ? <- []
+        2024-10-05 17:15:05.656 [consumer1] [take] 큐에 데이터가 없음, 소비자 대기
+        2024-10-05 17:15:05.761 [consumer2] [소비 시도]     ? <- []
+        2024-10-05 17:15:05.866 [consumer3] [소비 시도]     ? <- []
+
+        2024-10-05 17:15:05.971 [     main] 현재 상태 출력, 큐 데이터: []
+        2024-10-05 17:15:05.974 [     main] consumer1: TIMED_WAITING
+        2024-10-05 17:15:05.975 [     main] consumer2: BLOCKED
+        2024-10-05 17:15:05.975 [     main] consumer3: BLOCKED
+
+        2024-10-05 17:15:05.975 [     main] 생산자 시작
+        2024-10-05 17:15:05.976 [producer1] [생산 시도]data1 -> []
+        2024-10-05 17:15:06.081 [producer2] [생산 시도]data2 -> []
+        2024-10-05 17:15:06.186 [producer3] [생산 시도]data3 -> []
+
+        2024-10-05 17:15:06.291 [     main] 현재 상태 출력, 큐 데이터: []
+        2024-10-05 17:15:06.292 [     main] consumer1: TIMED_WAITING
+        2024-10-05 17:15:06.292 [     main] consumer2: BLOCKED
+        2024-10-05 17:15:06.292 [     main] consumer3: BLOCKED
+        2024-10-05 17:15:06.292 [     main] producer1: BLOCKED
+        2024-10-05 17:15:06.293 [     main] producer2: BLOCKED
+        2024-10-05 17:15:06.293 [     main] producer3: BLOCKED
+        2024-10-05 17:15:06.293 [     main] == [소비자 먼저 실행] 종료, BoundedQueueV2 ==
+        2024-10-05 17:15:06.665 [consumer1] [take] 큐에 데이터가 없음, 소비자 대기
+        2024-10-05 17:15:07.677 [consumer1] [take] 큐에 데이터가 없음, 소비자 대기
+        2024-10-05 17:15:08.683 [consumer1] [take] 큐에 데이터가 없음, 소비자 대기
+
+        ... (종료 안됨 - Lock 을 가진 상태로 대기중)
+
+
      */
 
     private static void consumerFirst(BoundedQueue queue) {
---
+++ java/thread/src/thread/bounded/BoundedQueueV2.java
@@ -0,0 +1,41 @@
+package thread.bounded;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BoundedQueueV2 implements BoundedQueue {
+
+    private final Queue<String> queue = new ArrayDeque<>();
+    private final int max;
+
+    public BoundedQueueV2(int max) {
+        this.max = max;
+    }
+
+    @Override
+    public synchronized void put(String data) {
+
+        while (queue.size() == max) {
+            log("[put] 큐가 가득 참, 생산자 대기");
+            sleep(1000);
+        }
+        queue.offer(data);
+    }
+
+    @Override
+    public synchronized String take() {
+        while (queue.isEmpty()) {
+            log("[take] 큐에 데이터가 없음, 소비자 대기");
+            sleep(1000);
+        }
+        return queue.poll();
+    }
+
+    @Override
+    public String toString() {
+        return queue.toString();
+    }
+}
