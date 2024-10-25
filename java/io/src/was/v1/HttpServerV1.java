18c79f1e4d60ce7edb8f4a3c8f74f5e0946b9d41
haerong22 <haerong22@naver.com>
2024-10-25 20:49:30 +0900

    #62 java(I/O): http server - 서버

---
+++ java/io/src/was/v1/HttpServerV1.java
@@ -0,0 +1,82 @@
+package was.v1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static util.MyLogger.log;
+
+public class HttpServerV1 {
+
+    private final int port;
+
+    public HttpServerV1(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        log("서버 시작 port: " + port);
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            process(socket);
+        }
+    }
+
+    private void process(Socket socket) throws IOException {
+        try (
+                socket;
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), UTF_8));
+                PrintWriter writer = new PrintWriter(socket.getOutputStream(), false, UTF_8);
+        ) {
+            String requestString = requestToString(reader);
+
+            if (requestString.contains("/favicon.ico")) {
+                log("favicon 요청");
+                return;
+            }
+
+            log("HTTP 요청 정보 출력");
+            System.out.println(requestString);
+
+            log("HTTP 응답 생성 중...");
+            responseToClient(writer);
+            log("HTTP 응답 전달 완료");
+        }
+    }
+
+    private static String requestToString(BufferedReader reader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.isEmpty()) {
+                break;
+            }
+            sb.append(line).append("\n");
+        }
+        return sb.toString();
+    }
+
+    private void responseToClient(PrintWriter writer) {
+        String body = "<h1>Hello World!</h1>";
+        int length = body.getBytes(UTF_8).length;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("HTTP/1.1 200 OK\r\n");
+        sb.append("Content-Type: text/html\r\n");
+        sb.append("Content-Length: ").append(length).append("\r\n");
+        sb.append("\r\n"); // header, body 구분 라인
+        sb.append(body);
+
+        log("HTTP 응답 정보 출력");
+        System.out.println(sb);
+
+        writer.println(sb);
+        writer.flush();
+    }
+}
