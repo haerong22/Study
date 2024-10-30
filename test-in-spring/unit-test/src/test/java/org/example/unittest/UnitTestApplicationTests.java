f21695dd85abf267bfc2f1c616852246dfcb2b05
haerong22 <haerong22@naver.com>
2024-10-30 23:04:46 +0900

    #63 test in spring: unit test - init

---
+++ test-in-spring/unit-test/src/test/java/org/example/unittest/UnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.unittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
