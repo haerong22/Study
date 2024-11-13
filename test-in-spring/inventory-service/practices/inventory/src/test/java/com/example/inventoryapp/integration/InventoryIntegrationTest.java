69999368ddd05471771a84b9335e089f93def4de
haerong22 <haerong22@naver.com>
2024-11-14 00:03:36 +0900

    #63 test in spring: inventory - integration test with test binder

--- test-in-spring/inventory-service/practices/inventory/src/test/java/com/example/inventoryapp/integration/InventoryIntegrationTest.java
+++ test-in-spring/inventory-service/practices/inventory/src/test/java/com/example/inventoryapp/integration/InventoryIntegrationTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.MediaType;
+import org.springframework.messaging.Message;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -20,6 +22,8 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static com.example.inventoryapp.inventory.controller.consts.ErrorCodes.*;
+import static com.example.inventoryapp.test.assertion.Assertions.assertDecreasedEventEquals;
+import static com.example.inventoryapp.test.assertion.Assertions.assertUpdatedEventEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -45,6 +49,9 @@ public class InventoryIntegrationTest {
 
     @Autowired
     private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private OutputDestination outputDestination;
 
     private final String existingItemId = "1";
     private final String nonExistingItemId = "2";
@@ -109,6 +116,7 @@ public class InventoryIntegrationTest {
         successGetStock(existingItemId, stock);
 
         final Long quantity = 10L;
+        final Long expectedStock = stock - quantity;
         final String requestBody = "{\"quantity\":" + quantity + "}";
         mockMvc.perform(
                         post("/api/v1/inventory/{itemId}/decrease", existingItemId)
@@ -121,7 +129,10 @@ public class InventoryIntegrationTest {
                         jsonPath("$.data.stock").value(stock - quantity)
                 );
 
-        successGetStock(existingItemId, stock - quantity);
+        successGetStock(existingItemId, expectedStock);
+
+        final Message<byte[]> result = outputDestination.receive(1000, "inventory-out-0");
+        assertDecreasedEventEquals(result, existingItemId, quantity, expectedStock);
     }
 
     @DisplayName("재고 수정 실패")
@@ -145,7 +156,7 @@ public class InventoryIntegrationTest {
         successGetStock(existingItemId, stock);
     }
 
-    @DisplayName("재고 수정 실패")
+    @DisplayName("재고 수정 성공")
     @Test
     void test6() throws Exception {
         successGetStock(existingItemId, stock);
@@ -164,16 +175,18 @@ public class InventoryIntegrationTest {
                 );
 
         successGetStock(existingItemId, newStock);
+
+        final Message<byte[]> result = outputDestination.receive(1000, "inventory-out-0");
+        assertUpdatedEventEquals(result, existingItemId, newStock);
     }
 
     @DisplayName("재고 차감, 수정 종합")
     @Test
     void test7() throws Exception {
         Long expectedStock = 100L;
-        // 1. 재고를 조회하고 100개인 것을 확인한다.
+
         successGetStock(existingItemId, expectedStock);
 
-        // 2. 재고 10개 차감을 7번 반복하고 성공한다.
         final Long decreaseQuantity = 10L;
         for (int i = 0; i < 7; i++) {
             expectedStock -= decreaseQuantity;
@@ -189,10 +202,8 @@ public class InventoryIntegrationTest {
                     );
         }
 
-        // 3. 재고를 조회하고 30개인 것을 확인한다.
         successGetStock(existingItemId, expectedStock); // expectedStock = 30L
 
-        // 4. 재고를 500개로 수정하고 성공한다.
         final Long newStock = 500L;
         final String requestBody = "{\"stock\": " + newStock + "}";
         mockMvc.perform(
@@ -203,8 +214,17 @@ public class InventoryIntegrationTest {
                 .andExpect(jsonPath("$.data.item_id").value(existingItemId))
                 .andExpect(jsonPath("$.data.stock").value(newStock));
 
-        // 5. 재고를 조회하고 500개인 것을 확인한다.
         successGetStock(existingItemId, newStock);
+
+        Long prevStock = 100L;
+        for (int i = 0; i < 7; i++) {
+            prevStock -= decreaseQuantity;
+            final Message<byte[]> result = outputDestination.receive(1000, "inventory-out-0");
+            assertDecreasedEventEquals(result, existingItemId, decreaseQuantity, prevStock);
+        }
+
+        final Message<byte[]> result = outputDestination.receive(1000, "inventory-out-0");
+        assertUpdatedEventEquals(result, existingItemId, newStock);
     }
 
     private void successGetStock(String itemId, Long stock) throws Exception {
