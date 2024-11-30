c88cf760325d5f15c19c87216e458c9d95687f75
haerong22 <haerong22@naver.com>
2024-11-30 17:52:14 +0900

    #63 test in spring: batch - init

---
+++ test-in-spring/trx-batch/src/main/java/org/example/trxbatch/TrxBatchApplication.java
@@ -0,0 +1,13 @@
+package org.example.trxbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrxBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrxBatchApplication.class, args);
+    }
+
+}
---
+++ test-in-spring/trx-batch/src/test/java/org/example/trxbatch/TrxBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.trxbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrxBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
