d350eadd04a437b1ae61b6a64988355c5cf36887
haerong22 <haerong22@naver.com>
2024-12-03 23:14:39 +0900

    #63 test in spring: batch - monthly transaction report app message writer

--- test-in-spring/trx-batch/src/main/java/org/example/trxbatch/job/monthlytrxreport/MonthlyTrxReportViaAppMessengerWriter.java
+++ test-in-spring/trx-batch/src/main/java/org/example/trxbatch/job/monthlytrxreport/MonthlyTrxReportViaAppMessengerWriter.java
@@ -1,29 +1,63 @@
 package org.example.trxbatch.job.monthlytrxreport;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.trxbatch.dto.CustomerMonthlyTrxReport;
+import org.example.trxbatch.dto.CustomerMonthlyTrxReportDetail;
+import org.example.trxbatch.dto.MonthlyTrxSummary;
+import org.example.trxbatch.repository.AppMessageRepository;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.List;
+
 import static org.example.trxbatch.job.monthlytrxreport.MonthlyTrxReportJobConfig.JOB_PARAM_TARGET_YEAR_MONTH_EXPRESSION;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 @StepScope
 public class MonthlyTrxReportViaAppMessengerWriter implements ItemWriter<CustomerMonthlyTrxReport> {
 
+    private static final int DAY_10 = 10;
+    private final int year;
+    private final int month;
+    private final LocalDate sendDate;
+
+    private final AppMessageRepository appMessageRepository;
+
     public MonthlyTrxReportViaAppMessengerWriter(
-            @Value(JOB_PARAM_TARGET_YEAR_MONTH_EXPRESSION) String targetYearMonth
+            @Value(JOB_PARAM_TARGET_YEAR_MONTH_EXPRESSION) String targetYearMonthStr,
+            AppMessageRepository appMessageRepository
     ) {
+        YearMonth targetYearMonth = YearMonth.parse(targetYearMonthStr);
+        this.year = targetYearMonth.getYear();
+        this.month = targetYearMonth.getMonthValue();
+        this.sendDate = targetYearMonth.plusMonths(1).atDay(DAY_10);
+        this.appMessageRepository = appMessageRepository;
     }
 
     @Override
-    public void write(Chunk<? extends CustomerMonthlyTrxReport> chunk) throws Exception {
-        // TODO
+    public void write(Chunk<? extends CustomerMonthlyTrxReport> reports) throws Exception {
+        if (reports.isEmpty()) {
+            return;
+        }
+
+        List<MonthlyTrxSummary> summaries = reports.getItems().stream().map(report -> {
+            List<CustomerMonthlyTrxReportDetail> details = report.getCustomerMonthlyTrxReportDetails();
+            return new MonthlyTrxSummary(
+                    report.getCustomerId(),
+                    details.size(),
+                    details.stream()
+                            .map(CustomerMonthlyTrxReportDetail::getAmount)
+                            .reduce(BigInteger.ZERO, BigInteger::add)
+            );
+        }).toList();
+
+        appMessageRepository.batchInsertMonthlyTrxReport(year, month, sendDate, summaries);
     }
 }
---
+++ test-in-spring/trx-batch/src/test/java/org/example/trxbatch/job/monthlytrxreport/MonthlyTrxReportViaAppMessengerWriterTest.java
@@ -0,0 +1,84 @@
+package org.example.trxbatch.job.monthlytrxreport;
+
+import org.example.trxbatch.dto.CustomerMonthlyTrxReport;
+import org.example.trxbatch.dto.MonthlyTrxSummary;
+import org.example.trxbatch.repository.AppMessageRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.batch.item.Chunk;
+
+import java.math.BigInteger;
+import java.time.YearMonth;
+import java.util.List;
+
+import static org.example.trxbatch.job.monthlytrxreport.fixture.CustomerMonthlyTrxReportFixtures.FIXTURE_REPORT_OF_SINGLE_CUSTOMER_WITH_ID_1_THREE_TRXS;
+import static org.example.trxbatch.job.monthlytrxreport.fixture.CustomerMonthlyTrxReportFixtures.FIXTURE_REPORT_OF_SINGLE_CUSTOMER_WITH_ID_2_THREE_TRXS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class MonthlyTrxReportViaAppMessengerWriterTest {
+
+    @Mock
+    private AppMessageRepository appMessageRepository;
+
+    private MonthlyTrxReportViaAppMessengerWriter writer;
+
+    @BeforeEach
+    void setUp() {
+        writer = new MonthlyTrxReportViaAppMessengerWriter("2024-05", appMessageRepository);
+    }
+
+    @Test
+    void testSuccess() throws Exception {
+        // given
+        Chunk<CustomerMonthlyTrxReport> chunk = new Chunk<>(
+                List.of(
+                        FIXTURE_REPORT_OF_SINGLE_CUSTOMER_WITH_ID_1_THREE_TRXS(),
+                        FIXTURE_REPORT_OF_SINGLE_CUSTOMER_WITH_ID_2_THREE_TRXS()
+                )
+        );
+
+        // when
+        writer.write(chunk);
+
+        // then
+        ArgumentCaptor<List<MonthlyTrxSummary>> captor = ArgumentCaptor.forClass(List.class);
+
+        verify(appMessageRepository, times(1)).batchInsertMonthlyTrxReport(
+                eq(2024),
+                eq(5),
+                eq(YearMonth.of(2024, 6).atDay(10)),
+                captor.capture()
+        );
+        List<MonthlyTrxSummary> summaries = captor.getValue();
+        assertEquals(2, summaries.size());
+
+        MonthlyTrxSummary summary1 = summaries.get(0);
+        assertEquals(1L, summary1.customerId());
+        assertEquals(3, summary1.trxCount());
+        assertEquals(new BigInteger("140000"), summary1.trxAmountSum());
+
+        MonthlyTrxSummary summary2 = summaries.get(1);
+        assertEquals(2L, summary2.customerId());
+        assertEquals(3, summary2.trxCount());
+        assertEquals(new BigInteger("140000"), summary2.trxAmountSum());
+    }
+
+    @Test
+    void testEmpty() throws Exception {
+        // given
+        Chunk<CustomerMonthlyTrxReport> chunk = new Chunk<>(List.of());
+
+        // when
+        writer.write(chunk);
+
+        // then
+        verify(appMessageRepository, never()).batchInsertMonthlyTrxReport(anyInt(), anyInt(), any(), any());
+    }
+}
