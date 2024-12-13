7f49ac8df9874138348cee3095377ab2ca62e306
haerong22 <haerong22@naver.com>
2024-12-13 21:47:21 +0900

    #63 test in spring: batch - mock email server

---
+++ test-in-spring/mock-email-server/src/main/java/org/example/mockemailserver/MockEmailServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.mockemailserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockEmailServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockEmailServerApplication.class, args);
+    }
+
+}
