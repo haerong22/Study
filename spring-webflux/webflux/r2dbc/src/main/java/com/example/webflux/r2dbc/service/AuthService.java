ae85ce494bcbbe81ec814993590e7a197c896a16
haerong22 <haerong22@naver.com>
2024-12-22 20:50:11 +0900

    #60 r2dbc: add AuthEntity

--- spring-webflux/webflux/r2dbc/src/main/java/com/example/webflux/r2dbc/service/AuthService.java
+++ spring-webflux/webflux/r2dbc/src/main/java/com/example/webflux/r2dbc/service/AuthService.java
@@ -1,21 +1,30 @@
 package com.example.webflux.r2dbc.service;
 
+import com.example.webflux.r2dbc.common.repository.AuthEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.relational.core.query.Criteria;
+import org.springframework.data.relational.core.query.Query;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
-import java.util.Map;
-
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class AuthService {
 
     private final R2dbcEntityTemplate entityTemplate;
 
-    private static final Map<String, String> tokenUserIdMap = Map.of("abcd", "1");
-
     public Mono<String> getNameByToken(String token) {
-        return Mono.justOrEmpty(tokenUserIdMap.get(token));
+        var query = Query.query(
+                Criteria.where("token").is(token)
+        );
+
+        return entityTemplate.select(AuthEntity.class)
+                .matching(query)
+                .one()
+                .map(authEntity -> authEntity.getUserId().toString())
+                .doOnNext(name -> log.info("name: {}", name));
     }
 }
