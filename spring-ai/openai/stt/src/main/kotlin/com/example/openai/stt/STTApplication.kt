1f25545c62482a461797e3da2431d540f5c87e6d
haerong22 <haerong22@naver.com>
2025-02-05 23:05:12 +0900

    #66 OpenAI: stt - init

---
+++ spring-ai/openai/stt/src/main/kotlin/com/example/openai/stt/STTApplication.kt
@@ -0,0 +1,11 @@
+package com.example.openai.stt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class STTApplication
+
+fun main(args: Array<String>) {
+    runApplication<STTApplication>(*args)
+}
