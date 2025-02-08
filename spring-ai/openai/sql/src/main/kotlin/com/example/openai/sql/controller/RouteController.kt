7313769b70569774af6c92ce87c9dcd26e2d101b
haerong22 <haerong22@naver.com>
2025-02-08 21:11:19 +0900

    #66 OpenAI: sql - view

---
+++ spring-ai/openai/sql/src/main/kotlin/com/example/openai/sql/controller/RouteController.kt
@@ -0,0 +1,15 @@
+package com.example.openai.sql.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+
+
+@Controller
+class RouteController {
+
+    @GetMapping("/sql")
+    fun index(): String {
+        return "sql"
+    }
+}
