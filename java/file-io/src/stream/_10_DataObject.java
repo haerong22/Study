587a5a344be154f10aacfbbeecb55e5acdbdd9c2
haerong22 <haerong22@naver.com>
2025-07-09 23:21:25 +0900

    #71 I/O: object input/output stream with serializable

---
+++ java/file-io/src/stream/_10_DataObject.java
@@ -0,0 +1,87 @@
+package stream;
+
+import java.io.*;
+
+public class _10_DataObject {
+
+    private static final String targetPath = "./tmp/test.dat";
+
+    public static DataObject readObject() {
+        DataObject dto;
+        try {
+            FileInputStream fis = new FileInputStream(targetPath);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            dto = (DataObject) ois.readObject();
+        } catch (Exception e) {
+            return null;
+        }
+
+        return dto;
+    }
+
+    public static boolean writeObject(DataObject dto) {
+        try {
+            FileOutputStream fos = new FileOutputStream(targetPath);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(dto);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        if (writeObject(new DataObject(10, "TEST_DATA"))) {
+            System.out.println("writeObject(): true");
+        } else {
+            System.out.println("writeObject(): false");
+        }
+
+        DataObject dto = readObject();
+        if (dto != null) {
+            System.out.println(dto);
+        } else {
+            System.out.println("Error: Failed to read object!");
+        }
+    }
+
+    /*
+        writeObject(): true
+        Ver: 1
+        Data: 10, TEST_DATA
+     */
+
+}
+
+class DataObject implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private int type = 0;
+    private final String data;
+
+    public DataObject(int type, String data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        return "Ver: " + serialVersionUID + "\n" +
+                "Data: " + type + ", " + data;
+    }
+}
