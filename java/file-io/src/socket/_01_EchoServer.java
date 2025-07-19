9a8ade8281625e734703187e16f2d53713ec5a28
haerong22 <haerong22@naver.com>
2025-07-19 23:30:32 +0900

    #71 I/O: socket - echo server

---
+++ java/file-io/src/socket/_01_EchoServer.java
@@ -0,0 +1,54 @@
+package socket;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class _01_EchoServer {
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        BufferedReader reader = null;
+        PrintWriter writer = null;
+        int listenPort = 20000;
+
+        try {
+            System.out.println("Server start...");
+            serverSocket = new ServerSocket(listenPort);
+
+            clientSocket = serverSocket.accept();
+            System.out.println("Client connected");
+
+            InputStream inputStream = clientSocket.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            OutputStream outputStream = clientSocket.getOutputStream();
+            writer = new PrintWriter(outputStream, true);
+
+            String msg;
+            while ((msg = reader.readLine()) != null) {
+                if ("exit".equalsIgnoreCase(msg))
+                    break;
+
+                System.out.println("Received: " + msg);
+                writer.println(msg);
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+                if (clientSocket != null) clientSocket.close();
+                if (serverSocket != null) serverSocket.close();
+                if (reader != null) reader.close();
+                if (writer != null) writer.close();
+
+                System.out.println("Server stop...");
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
