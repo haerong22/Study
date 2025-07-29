0b0682a8c75380c7eb05901071dc68fee14e9346
haerong22 <haerong22@naver.com>
2025-07-29 23:15:35 +0900

    #72 splean: null safe with spring

--- splearn/build.gradle.kts
+++ splearn/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.5.4"
     id("io.spring.dependency-management") version "1.1.7"
+    id("com.github.spotbugs") version "6.2.2"
 }
 
 group = "org.example"
---
+++ splearn/src/main/java/org/example/splearn/domain/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package org.example.splearn.domain;
+
+import org.springframework.lang.NonNullApi;
