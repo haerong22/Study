27bb27b4cd46ca8b3bb51b3b9060197a9fbe6664
haerong22 <haerong22@naver.com>
2025-10-17 20:09:19 +0900

    #75 distributed tx: tcc - order api retry

--- distributed-transaction/msa-order/src/main/kotlin/org/example/order/infrastructure/point/PointApiClient.kt
+++ distributed-transaction/msa-order/src/main/kotlin/org/example/order/infrastructure/point/PointApiClient.kt
@@ -3,12 +3,35 @@ package org.example.order.infrastructure.point
 import org.example.order.infrastructure.point.dto.PointReserveApiRequest
 import org.example.order.infrastructure.point.dto.PointReserveCancelApiRequest
 import org.example.order.infrastructure.point.dto.PointReserveConfirmApiRequest
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
+import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpClientErrorException.BadRequest
+import org.springframework.web.client.HttpClientErrorException.NotFound
 import org.springframework.web.client.RestClient
+import java.time.Duration
 
+@Component
 class PointApiClient(
-    private val restClient: RestClient,
+    @param:Value("\${api-client.point}") private val pointUrl: String,
 ) {
+    private val restClient = RestClient.builder()
+        .requestFactory(
+            HttpComponentsClientHttpRequestFactory().apply {
+                setReadTimeout(Duration.ofSeconds(2))
+            }
+        )
+        .baseUrl(pointUrl)
+        .build()
 
+    @Retryable(
+        retryFor = [Exception::class],
+        noRetryFor = [BadRequest::class, NotFound::class],
+        maxAttempts = 3,
+        backoff = Backoff(delay = 500)
+    )
     fun reservePoint(request: PointReserveApiRequest) {
         restClient.post()
             .uri("/points/reserve")
--- distributed-transaction/msa-order/src/main/kotlin/org/example/order/infrastructure/product/ProductApiClient.kt
+++ distributed-transaction/msa-order/src/main/kotlin/org/example/order/infrastructure/product/ProductApiClient.kt
@@ -4,12 +4,25 @@ import org.example.order.infrastructure.product.dto.ProductReserveApiRequest
 import org.example.order.infrastructure.product.dto.ProductReserveApiResponse
 import org.example.order.infrastructure.product.dto.ProductReserveCancelApiRequest
 import org.example.order.infrastructure.product.dto.ProductReserveConfirmApiRequest
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
+import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.body
+import java.time.Duration
 
+@Component
 class ProductApiClient(
-    private val restClient: RestClient,
+    @param:Value("\${api-client.product}") private val productUrl: String,
 ) {
+    private val restClient = RestClient.builder()
+        .requestFactory(
+            HttpComponentsClientHttpRequestFactory().apply {
+                setReadTimeout(Duration.ofSeconds(2))
+            }
+        )
+        .baseUrl(productUrl)
+        .build()
 
     fun reserveProduct(request: ProductReserveApiRequest): ProductReserveApiResponse {
         return restClient.post()
