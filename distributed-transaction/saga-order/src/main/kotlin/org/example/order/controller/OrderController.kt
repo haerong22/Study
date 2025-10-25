f4a2d5164f71f4c476bfbac67ca2062a5b71b047
haerong22 <haerong22@naver.com>
2025-10-25 19:52:05 +0900

    #75 distributed tx: saga - order place order api

--- distributed-transaction/saga-order/src/main/kotlin/org/example/order/controller/OrderController.kt
+++ distributed-transaction/saga-order/src/main/kotlin/org/example/order/controller/OrderController.kt
@@ -1,8 +1,11 @@
 package org.example.order.controller
 
+import org.example.order.application.OrderCoordinator
 import org.example.order.application.OrderService
+import org.example.order.application.RedisLockService
 import org.example.order.controller.dto.CreateOrderRequest
 import org.example.order.controller.dto.CreateOrderResponse
+import org.example.order.controller.dto.PlaceOrderRequest
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class OrderController(
     private val orderService: OrderService,
+    private val redisLockService: RedisLockService,
+    private val orderCoordinator: OrderCoordinator,
 ) {
 
     @PostMapping("/orders")
@@ -18,5 +23,24 @@ class OrderController(
     ): CreateOrderResponse {
         val result = orderService.createOrder(request.toCommand())
         return CreateOrderResponse(result.orderId)
+    }
+
+    @PostMapping("/orders/place")
+    fun placeOrder(
+        @RequestBody request: PlaceOrderRequest,
+    ) {
+        val key = "order:orchestration:${request.orderId}"
+
+        val acquiredLock = redisLockService.tryLock(key, request.orderId.toString())
+
+        if (!acquiredLock) {
+            throw RuntimeException("락 획득에 실패하였습니다.")
+        }
+
+        try {
+            orderCoordinator.placeOrder(request.toCommand())
+        } finally {
+            redisLockService.releaseLock(key)
+        }
     }
 }
--- distributed-transaction/saga-order/src/main/kotlin/org/example/order/domain/Order.kt
+++ distributed-transaction/saga-order/src/main/kotlin/org/example/order/domain/Order.kt
@@ -1,6 +1,8 @@
 package org.example.order.domain
 
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -9,6 +11,7 @@ import jakarta.persistence.Table
 @Entity
 @Table(name = "orders")
 class Order(
+    @Enumerated(EnumType.STRING)
     var status: OrderStatus = OrderStatus.CREATED
 ) {
     @Id
