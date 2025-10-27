9c89bf4e02d0fc425f24d068c592c0f37a413056
haerong22 <haerong22@naver.com>
2025-10-27 23:05:56 +0900

    #75 distributed tx: saga choreography - init

--- distributed-transaction/settings.gradle.kts
+++ distributed-transaction/settings.gradle.kts
@@ -1,11 +1,15 @@
 rootProject.name = "distributed-transaction"
 
-include("monolithic")
+//include("monolithic")
 
 //include("tcc-order")
 //include("tcc-point")
 //include("tcc-product")
 
-include("saga-order")
-include("saga-point")
-include("saga-product")
+//include("saga-order")
+//include("saga-point")
+//include("saga-product")
+
+include("saga-choreography-order")
+include("saga-choreography-point")
+include("saga-choreography-product")
