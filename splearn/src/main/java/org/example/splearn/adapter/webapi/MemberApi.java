03a9f5e97c80c908e6a2d761a8e2b697fa1d8954
haerong22 <haerong22@naver.com>
2025-11-12 23:43:50 +0900

    #72 splean: member register api

---
+++ splearn/src/main/java/org/example/splearn/adapter/webapi/MemberApi.java
@@ -0,0 +1,24 @@
+package org.example.splearn.adapter.webapi;
+
+import lombok.RequiredArgsConstructor;
+import org.example.splearn.adapter.webapi.dto.MemberRegisterResponse;
+import org.example.splearn.application.member.provided.MemberRegister;
+import org.example.splearn.domain.member.Member;
+import org.example.splearn.domain.member.MemberRegisterRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApi {
+    private final MemberRegister memberRegister;
+
+    @PostMapping("/api/members")
+    public MemberRegisterResponse register(
+            @RequestBody MemberRegisterRequest request
+    ) {
+        Member member = memberRegister.register(request);
+        return MemberRegisterResponse.of(member);
+    }
+}
---
+++ splearn/src/main/java/org/example/splearn/adapter/webapi/dto/MemberRegisterResponse.java
@@ -0,0 +1,10 @@
+package org.example.splearn.adapter.webapi.dto;
+
+import org.example.splearn.domain.member.Member;
+
+public record MemberRegisterResponse(Long memberId, String emailAddress) {
+
+    public static MemberRegisterResponse of(Member member) {
+        return new MemberRegisterResponse(member.getId(), member.getEmail().address());
+    }
+}
